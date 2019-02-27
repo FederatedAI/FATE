e44aab6b353b056e154172483f9f7de35693bb26
maxhuang(黄铭毅) <maxhuang@webank.com>
2019-02-27 09:48:51 +0800

    Basics for egg node manager.

---
+++ arch/eggroll/egg/src/main/java/com/webank/ai/fate/eggroll/egg/api/grpc/server/NodeServiceImpl.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2019 The FATE Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.webank.ai.fate.eggroll.egg.api.grpc.server;
+
+import com.webank.ai.fate.api.core.BasicMeta;
+import com.webank.ai.fate.api.eggroll.egg.NodeServiceGrpc;
+import io.grpc.stub.StreamObserver;
+
+
+public class NodeServiceImpl extends NodeServiceGrpc.NodeServiceImplBase {
+
+    @Override
+    public void getProcessor(BasicMeta.Endpoint request, StreamObserver<BasicMeta.Endpoint> responseObserver) {
+        super.getProcessor(request, responseObserver);
+    }
+
+    @Override
+    public void getAllPossibleProcessors(BasicMeta.Endpoint request, StreamObserver<BasicMeta.Endpoints> responseObserver) {
+        super.getAllPossibleProcessors(request, responseObserver);
+    }
+}
