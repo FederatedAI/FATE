25ad75fb53de95ea11aa03a7a55c8c421f81b88c
zengjice <jicezeng@gmail.com>
2019-03-19 20:56:18 +0800

    delete common

--- arch/api/common/mlmodel/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# Project: FATE
-# File: __init__.py
-# Copyright (c) 2019 jarviszeng <jarviszeng@webank.com>
-# Created Time: 2019-03-14 20:54
-# Describe: 
