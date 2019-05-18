dd423fa508ab364649e2e73e904058dfd19f3ff3
zazd <zhenght5@gmail.com>
2019-05-18 20:08:36 +0800

    fit host batch size checking

--- federatedml/logistic_regression/hetero_logistic_regression/hetero_lr_host.py
+++ federatedml/logistic_regression/hetero_logistic_regression/hetero_lr_host.py
@@ -90,6 +90,9 @@ class HeteroLRHost(BaseLogisticRegression):
                     LOGGER.info("Get batch_index from Guest")
 
                     batch_size = batch_data_index.count()
+                    if batch_size != self.batch_size:
+                        raise ValueError("Accepted batch_size from guest is not equal to accepted data size from guest")
+
                     if batch_size < consts.MIN_BATCH_SIZE and batch_size != -1:
                         raise ValueError(
                             "Batch size get from guest should not less than 10, except -1, batch_size is {}".format(
