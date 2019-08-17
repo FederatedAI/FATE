350a65d22cbd59c6e0fc1af99ce40a12f7c8e2f4
zengjice <jicezeng@gmail.com>
2019-08-17 13:04:18 +0800

    fate_flow: restore db model to json function

--- fate_flow/db/db_models.py
+++ fate_flow/db/db_models.py
@@ -86,7 +86,7 @@ class DataBaseModel(Model):
         database = DB
 
     def to_json(self):
-        return self.__dict__['__data__'].copy()
+        return self.__dict__['__data__']
 
     def save(self, *args, **kwargs):
         if hasattr(self, "update_date"):
