fb2e22c988d46d271f9528ec2066a63f083010c4
zengjice <jicezeng@gmail.com>
2019-10-21 17:26:25 +0800

    delete custom config from allinone_configuration

--- cluster-deploy/scripts/allinone_configurations.sh
+++ cluster-deploy/scripts/allinone_configurations.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 user=app
-deploy_dir=/data/projects_test/fate
+deploy_dir=
 party_list=(10000 9999)
-node_list=(10.211.55.6 10.211.55.9)
+node_list=(127.0.0.1 127.0.0.1)
 db_auth=(root fate_dev)
 redis_password=fate_dev
 cxx_compile_flag=false
