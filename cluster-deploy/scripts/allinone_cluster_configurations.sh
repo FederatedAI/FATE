b25718c944e6e0187b3496efe877edd4c66a0625
zengjice <jicezeng@gmail.com>
2019-10-30 13:50:12 +0800

    deploy: 1. fix mysql grant 2. fix multinode fateflow depoly

--- cluster-deploy/scripts/allinone_cluster_configurations.sh
+++ cluster-deploy/scripts/allinone_cluster_configurations.sh
@@ -4,6 +4,6 @@ user=app
 deploy_dir=/data/projects/fate
 party_list=(10000 9999)
 node_list=(192.168.0.1 192.168.0.2)
-db_auth=(root fate_dev)
+db_auth=(fate_dev fate_dev)
 redis_password=fate_dev
 cxx_compile_flag=false
