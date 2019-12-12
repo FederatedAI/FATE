40702ee2f73b4266e0550d369dabe14ecf2ce8ab
weiwee <wbwmat@gmail.com>
2019-12-12 21:16:16 +0800

    revert fate_paillier
    Signed-off-by: weiwee <wbwmat@gmail.com>

--- federatedml/secureprotol/fate_paillier.py
+++ federatedml/secureprotol/fate_paillier.py
@@ -50,7 +50,6 @@ class PaillierKeypair(object):
 class PaillierPublicKey(object):
     """Contains a public key and associated encryption methods.
     """
-
     def __init__(self, n):
         self.g = n + 1
         self.n = n
@@ -110,7 +109,6 @@ class PaillierPublicKey(object):
 class PaillierPrivateKey(object):
     """Contains a private key and associated decryption method.
     """
-
     def __init__(self, public_key, p, q):
         if not p * q == public_key.n:
             raise ValueError("given public key does not match the given p and q")
@@ -144,7 +142,7 @@ class PaillierPrivateKey(object):
         """Computes the h-function as defined in Paillier's paper page.
         """
         return gmpy_math.invert(self.l_func(gmpy_math.powmod(self.public_key.g,
-                                                             x - 1, xsquare), x), x)
+                                                                 x - 1, xsquare), x), x)
 
     def l_func(self, x, p):
         """computes the L function as defined in Paillier's paper.
@@ -166,15 +164,15 @@ class PaillierPrivateKey(object):
         """
         if not isinstance(ciphertext, int):
             raise TypeError("ciphertext should be an int, not: %s" %
-                            type(ciphertext))
+                type(ciphertext))
 
         mp = self.l_func(gmpy_math.powmod(ciphertext,
-                                          self.p - 1, self.psquare),
-                         self.p) * self.hp % self.p
+                                              self.p-1, self.psquare),
+                                              self.p) * self.hp % self.p
 
         mq = self.l_func(gmpy_math.powmod(ciphertext,
-                                          self.q - 1, self.qsquare),
-                         self.q) * self.hq % self.q
+                                              self.q-1, self.qsquare),
+                                              self.q) * self.hq % self.q
 
         return self.crt(mp, mq)
 
@@ -190,9 +188,9 @@ class PaillierPrivateKey(object):
 
         encoded = self.raw_decrypt(encrypted_number.ciphertext(be_secure=False))
         encoded = FixedPointNumber(encoded,
-                                   encrypted_number.exponent,
-                                   self.public_key.n,
-                                   self.public_key.max_int)
+                             encrypted_number.exponent,
+                             self.public_key.n,
+                             self.public_key.max_int)
         decrypt_value = encoded.decode()
 
         return decrypt_value
@@ -201,7 +199,6 @@ class PaillierPrivateKey(object):
 class PaillierEncryptedNumber(object):
     """Represents the Paillier encryption of a float or int.
     """
-
     def __init__(self, public_key, ciphertext, exponent=0):
         self.public_key = public_key
         self.__ciphertext = ciphertext
@@ -334,7 +331,7 @@ class PaillierEncryptedNumber(object):
     def __raw_add(self, e_x, e_y, exponent):
         """return the integer E(x + y) given ints E(x) and E(y).
         """
-        ciphertext = e_x * e_y % self.public_key.nsquare
+        ciphertext =  e_x * e_y % self.public_key.nsquare
 
         return PaillierEncryptedNumber(self.public_key, ciphertext, exponent)
 
