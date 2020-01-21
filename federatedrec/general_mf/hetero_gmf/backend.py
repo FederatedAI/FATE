5d0af1f1423d35f2891a7e68e581daf3e6377c6e
tonyliu <liucongwust@gmail.com>
2020-01-21 14:46:41 +0800

    fix error of exporting prediction model

--- federatedrec/general_mf/hetero_gmf/backend.py
+++ federatedrec/general_mf/hetero_gmf/backend.py
@@ -6,11 +6,10 @@ import zipfile
 import tempfile
 
 import tensorflow as tf
-from tensorflow.python.keras.models import Model
-from tensorflow.python.keras.regularizers import l2
+from tensorflow.keras import Model
 from tensorflow.python.keras.backend import set_session
-from tensorflow.python.keras.initializers import TruncatedNormal
-from tensorflow.python.keras.layers import Input, Embedding, Multiply, Dense, Lambda, Subtract, Dot
+from tensorflow.keras.initializers import RandomNormal
+from tensorflow.keras.layers import Input, Embedding, Lambda, Subtract, Dot
 from arch.api.utils import log_utils
 from federatedrec.utils import zip_dir_as_bytes
 from federatedml.framework.weights import OrderDictWeights, Weights
@@ -19,13 +18,27 @@ from tensorflow.keras.losses import MSE as MSE
 LOGGER = log_utils.getLogger()
 
 
-class KerasModel:
-    def __init__(self):
+class GMFModel:
+    """
+    General Matrix Factorization model
+    """
+    def __init__(self, user_num=None, item_num=None, embedding_dim=10):
+        self.user_num = user_num
+        self.item_num = item_num
+        self.embedding_dim = embedding_dim
+        self._trainable_weights = None
         self._aggregate_weights = None
-        self.session = None
         self._predict_model = None
+        self._model = None
+        self._sess = None
 
     def train(self, data: tf.keras.utils.Sequence, **kwargs):
+        """
+        Train model on input data.
+        :param data: input data.
+        :param kwargs: other params.
+        :return: Training steps.
+        """
         epochs = 1
         left_kwargs = copy.deepcopy(kwargs)
         if "aggregate_every_n_epoch" in kwargs:
@@ -35,24 +48,53 @@ class KerasModel:
         return epochs * len(data)
 
     def _set_model(self, _model):
+        """
+        Set _model as trainning model.
+        :param _model: training model.
+        :return:
+        """
         self._model = _model
 
     def _set_predict_model(self, _predict_model):
+        """
+        Set _predict_model as prediction model.
+        :param _predict_model: prediction model.
+        :return:
+        """
         self._predict_model = _predict_model
 
     def modify(self, func: typing.Callable[[Weights], Weights]) -> Weights:
+        """
+        Apply func on model weights.
+        :param func: operator to apply.
+        :return: updated weights.
+        """
         weights = self.get_model_weights()
         self.set_model_weights(func(weights))
         return weights
 
     def get_model_weights(self) -> OrderDictWeights:
+        """
+        Return model's weights as OrderDictWeights.
+        :return: model's weights.
+        """
         return OrderDictWeights(self.session.run(self._aggregate_weights))
 
     def set_model_weights(self, weights: Weights):
+        """
+        Set model's weights with input weights.
+        :param weights: input weights.
+        :return: updated weights.
+        """
         unboxed = weights.unboxed
         self.session.run([tf.assign(v, unboxed[name]) for name, v in self._aggregate_weights.items()])
 
     def evaluate(self, data: tf.keras.utils.Sequence):
+        """
+        Evaluate on input data and return evaluation results.
+        :param data: input data sequence.
+        :return: evaluation results.
+        """
         names = self._model.metrics_names
         values = self._model.evaluate(x=data, verbose=1)
         if not isinstance(values, list):
@@ -60,111 +102,99 @@ class KerasModel:
         return dict(zip(names, values))
 
     def predict(self, data: tf.keras.utils.Sequence, **kwargs):
-        if self._predict_model is not None:
-            return self._predict_model.predict(data)
-        else:
-            return self._model.predict(data)
+        """
+        Predict on input data and return prediction results which used in prediction.
+        :param data: input data.
+        :return: prediction results.
+        """
+        return self._predict_model.predict(data)
 
     @classmethod
-    def restore_model(cls, model_bytes):  # todo: restore optimizer to support incremental learning
-        model = cls()
+    def restore_model(cls, model_bytes, user_num, item_num, embedding_dim):
+        """
+        Restore model from model bytes.
+        :param model_bytes: model bytes of saved model.
+        :param user_num: user num
+        :param item_num: item num
+        :param embedding_dim: embedding dimension
+        :return:restored model object.
+        """
         LOGGER.info("begin restore_model")
-        keras_model = None
         with tempfile.TemporaryDirectory() as tmp_path:
             with io.BytesIO(model_bytes) as bytes_io:
-                with zipfile.ZipFile(bytes_io, 'r', zipfile.ZIP_DEFLATED) as f:
-                    f.extractall(tmp_path)
+                with zipfile.ZipFile(bytes_io, 'r', zipfile.ZIP_DEFLATED) as file:
+                    file.extractall(tmp_path)
 
-            os.system(f"echo 'test tmp_path:' {tmp_path}; ls {tmp_path}; du -h {tmp_path}")
-            try:
-                keras_model = tf.keras.models.load_model(filepath=tmp_path)
-            except IOError:
-                import warnings
-                warnings.warn('loading the model as SavedModel is still in experimental stages. '
-                              'trying tf.keras.experimental.load_from_saved_model...')
-                keras_model = tf.keras.experimental.load_from_saved_model(saved_model_path=tmp_path)
+            os.system(f"echo 'test tmp_path:' {tmp_path}; tree {tmp_path} || true; du -h {tmp_path}")
+            # try:
+            #     keras_model = tf.keras.models.load_model(filepath=tmp_path)
+            # except IOError:
+            import warnings
+            warnings.warn(
+                'loading the model as SavedModel is still in experimental stages. '
+                'trying tf.keras.experimental.load_from_saved_model...')
+            keras_model = tf.keras.experimental.load_from_saved_model(
+                saved_model_path=tmp_path)
+        model = cls(user_num=user_num, item_num=item_num, embedding_dim=embedding_dim)
         model._set_predict_model(keras_model)
         return model
 
     def export_model(self):
+        """
+        Export model to bytes.
+        :return: bytes of saved model.
+        """
+        model_bytes = None
         with tempfile.TemporaryDirectory() as tmp_path:
             LOGGER.info(f"tmp_path: {tmp_path}")
-            try:
-                LOGGER.info(f"predict model is None: {self._predict_model == None}")
-                tf.keras.models.save_model(self._predict_model, filepath=tmp_path, save_format="tf", include_optimizer=False)
-                # tf.keras.experimental.export_saved_model(self._predict_model, saved_model_path=tmp_path)
-            except Exception as e:
-                import warnings
-                warnings.warn('Saving the model as SavedModel is still in experimental stages. '
-                              'trying tf.keras.experimental.export_saved_model...')
+            # try:
+            #     tf.keras.models.save_model(
+            #         self._predict_model, filepath=tmp_path, save_format="tf")
+            # except NotImplementedError:
+            import warnings
+            warnings.warn(
+                'Saving the model as SavedModel is still in experimental stages. '
+                'trying tf.keras.experimental.export_saved_model...')
+            tf.keras.experimental.export_saved_model(
+                self._predict_model, saved_model_path=tmp_path)
 
-                tf.keras.experimental.export_saved_model(self._predict_model, saved_model_path=tmp_path)
-            os.system(f"echo 'test tmp_path:' {tmp_path}; ls {tmp_path}; du -h {tmp_path}")
-            LOGGER.info(f"export saved model at path: {tmp_path}")
+            os.system(f"echo 'test tmp_path:' {tmp_path}; tree {tmp_path} || true; du -h {tmp_path}")
             model_bytes = zip_dir_as_bytes(tmp_path)
 
         return model_bytes
 
-
-class GMFModel(KerasModel):
-    def __init__(self, user_ids=None, item_ids=None, embedding_dim=10, l2_coef=0.01, user_num=10000):
-        super().__init__()
-        if user_ids is not None:
-            self.user_num = max(max(user_ids), max(len(user_ids), user_num)) + 100
-
-        if item_ids is not None:
-            self.item_num = len(item_ids)
-        self.embedding_dim = embedding_dim
-        self._sess = None
-        self._trainable_weights = None
-        self._aggregate_weights = None
-        self.user_ids = user_ids
-        self.item_ids = item_ids
-        self.session = self.init_session()
-        self._embedding_initializer = TruncatedNormal(stddev=0.1)
-        self._initializer = TruncatedNormal(stddev=0.1)
-        self._regularizer = l2(l2_coef)
-
-    def _build(self, optimizer='rmsprop', loss='mse', metrics='mse'):
+    def build(self, user_num, item_num, embedding_dim, optimizer='rmsprop', loss='mse', metrics='mse'):
+        """
+        build network graph of model
+        :param user_num: user num
+        :param item_num: item num
+        :param embedding_dim: embedding dimension
+        :param optimizer: optimizer method
+        :param loss:  loss methods
+        :param metrics: metric methods
+        :return:
+        """
+        sess = self.session
         users_input = Input(shape=(1,), dtype='int32', name='user_input')
         items_input = Input(shape=(1,), dtype='int32', name='item_input')
         neg_items_input = Input(shape=(1,), dtype='int32', name='neg_items_input')
 
-        users = Lambda(lambda x: tf.squeeze(x, 1))(users_input)
+        users = Lambda(
+            lambda x: tf.strings.to_hash_bucket(tf.strings.as_string(tf.squeeze(x, 1)), user_num))(users_input)
         items = Lambda(
-            lambda x: tf.strings.to_hash_bucket(tf.strings.as_string(tf.squeeze(x, 1)), self.item_num))(items_input)
+            lambda x: tf.strings.to_hash_bucket(tf.strings.as_string(tf.squeeze(x, 1)), item_num))(items_input)
         neg_items = Lambda(
-            lambda x: tf.strings.to_hash_bucket(tf.strings.as_string(tf.squeeze(x, 1)), self.item_num))(neg_items_input)
-        user_embed_layer = Embedding(self.user_num, self.embedding_dim,
-                                     embeddings_initializer=self._embedding_initializer,
+            lambda x: tf.strings.to_hash_bucket(tf.strings.as_string(tf.squeeze(x, 1)), item_num))(neg_items_input)
+        user_embed_layer = Embedding(user_num, embedding_dim,
+                                     embeddings_initializer=RandomNormal(stddev=0.1),
                                      name='user_embedding')
 
-        item_embed_layer = Embedding(self.item_num, self.embedding_dim,
-                                     embeddings_initializer=self._embedding_initializer,
+        item_embed_layer = Embedding(item_num, embedding_dim,
+                                     embeddings_initializer=RandomNormal(stddev=0.1),
                                      name='item_embedding')
-
         cur_user_embed = user_embed_layer(users)
         cur_item_embed = item_embed_layer(items)
         neg_item_embed = item_embed_layer(neg_items)
-
-        LOGGER.info(f"users shapes: {users_input.shape}")
-        LOGGER.info(f"items shapes: {items_input.shape}")
-        LOGGER.info(f"neg_items shapes: {neg_items_input.shape}")
-        LOGGER.info(f"embedding shapes, user_embedding: {cur_user_embed.shape}")
-        LOGGER.info(f"embedding shapes, item_embedding: {cur_item_embed.shape}")
-        LOGGER.info(f"embedding shapes, neg_item_embedding: {neg_item_embed.shape}")
-
-        # output_layer = Dense(units=1
-        #                      , use_bias=False
-        #                      , activation=tf.nn.relu
-        #                      , kernel_initializer=self._initializer
-        #                      , kernel_regularizer=self._regularizer
-        #                      , name='OutputVector')
-        #
-        # pos_mul = Multiply(name='pos_mul_layer')(inputs=[cur_user_embed, cur_item_embed])
-        # neg_mul = Multiply(name='neg_mul_layer')(inputs=[cur_user_embed, neg_item_embed])
-        # pos_output = output_layer(pos_mul)
-        # neg_output = output_layer(neg_mul)
         pos_output = Dot(axes=-1, name="pos_dot")([cur_user_embed, cur_item_embed])
         neg_output = Dot(axes=-1, name="neg_dot")([cur_user_embed, neg_item_embed])
         loss_inst = Subtract(name="loss_layer")(inputs=[pos_output, neg_output])
@@ -180,13 +210,16 @@ class GMFModel(KerasModel):
 
         self._model = Model(inputs=[users_input, items_input, neg_items_input],
                             outputs=[pos_output, neg_output, loss_inst])
+
         # model for prediction
         LOGGER.info(f"model output names {self._model.output_names}")
         self._predict_model = Model(inputs=[users_input, items_input],
                                     outputs=pos_output)
+        self._predict_model.summary()
 
         self._model.compile(optimizer=optimizer_instance,
-                            loss=[MSE, MSE, gmf_loss], metrics=["MSE"], loss_weights=[0.3, 0.3, 0.4])
+                            loss=[MSE, MSE, gmf_loss], metrics=["MSE", "MSE", "MSE"], loss_weights=[0.3, 0.3, 0.4])
+        LOGGER.info(f"_predict_model type: {type(self._predict_model)}")
 
         # pick user_embedding for aggregating
         self._trainable_weights = {v.name.split("/")[0]: v for v in self._model.trainable_weights}
@@ -194,19 +227,47 @@ class GMFModel(KerasModel):
         LOGGER.info(f"finish building model, in {self.__class__.__name__} _build function")
 
     @classmethod
-    def build_model(cls, user_ids, item_ids, embedding_dim, loss, optimizer, metrics, user_num):
-        model = cls(user_ids=user_ids, item_ids=item_ids, embedding_dim=embedding_dim, user_num=user_num)
-        model._build(loss=loss, optimizer=optimizer, metrics=metrics)
+    def build_model(cls, user_num, item_num, embedding_dim, loss, optimizer, metrics):
+        """
+        build model
+        :param user_num: user num
+        :param item_num:  item num
+        :param embedding_dim: embedding dimension
+        :param loss: loss func
+        :param optimizer: optimization methods
+        :param metrics: metrics
+        :return: model object
+        """
+        model = cls(user_num=user_num, item_num=item_num, embedding_dim=embedding_dim)
+        model.build(user_num=user_num, item_num=item_num, embedding_dim=embedding_dim,
+                     loss=loss, optimizer=optimizer, metrics=metrics)
         return model
 
-    def init_session(self):
-        sess = tf.Session()
-        tf.get_default_graph()
-        set_session(sess)
-        return sess
+    @property
+    def session(self):
+        """
+        If session not created, then init a tensorflow session and return.
+        :return: tensorflow session.
+        """
+        if self._sess is None:
+            sess = tf.Session()
+            tf.get_default_graph()
+            set_session(sess)
+            self._sess = sess
+        return self._sess
 
-    def set_user_ids(self, user_ids):
-        self.user_ids = user_ids
+    def set_user_num(self, user_num):
+        """
+        set user num
+        :param user_num:
+        :return:
+        """
+        self.user_num = user_num
 
-    def set_item_ids(self, item_ids):
-        self.item_ids = item_ids
+    def set_item_num(self, item_num):
+        """
+        set item num
+        :param item_num:
+        :return:
+        """
+        self.item_num = item_num
--- federatedrec/general_mf/hetero_gmf/hetero_gmf_client.py
+++ federatedrec/general_mf/hetero_gmf/hetero_gmf_client.py
@@ -39,6 +39,8 @@ class HeteroGMFClient(HeteroGMFBase):
         self._model = None
         self.feature_shape = None
         self.user_num = None
+        self.item_num = None
+        self.aggregator_iter = None
 
     def _init_model(self, param):
         super()._init_model(param)
@@ -52,6 +54,12 @@ class HeteroGMFClient(HeteroGMFBase):
         self.user_ids_sync.register_user_ids_transfer(self.transfer_variable)
 
     def _check_monitored_status(self, data, epoch_degree):
+        """
+        check the model whether is converged or not
+        :param data:
+        :param epoch_degree:
+        :return:
+        """
         metrics = self._model.evaluate(data)
         LOGGER.info(f"metrics at iter {self.aggregator_iter}: {metrics}")
         loss = metrics["loss"]
@@ -59,12 +67,6 @@ class HeteroGMFClient(HeteroGMFBase):
                                   degree=epoch_degree,
                                   suffix=self._iter_suffix())
         return self.aggregator.get_converge_status(suffix=self._iter_suffix())
-
-    def send_user_ids(self, data):
-        pass
-
-    def get_user_ids(self):
-        pass
 
     def send_user_num(self, data):
         pass
@@ -78,12 +80,17 @@ class HeteroGMFClient(HeteroGMFBase):
         return data_overview.get_features_shape(data_instances)
 
     def fit(self, data_instances, validate_data=None):
+        """
+        train model
+        :param data_instances: training data
+        :param validate_data:  validation data
+        :return:
+        """
         data = self.data_converter.convert(data_instances, batch_size=self.batch_size
                                            , neg_count=self.model_param.neg_count)
 
-        user_ids = data.unique_user_ids
-        item_ids = data.unique_items_ids
         user_num = data.user_count
+        item_num = data.item_count
 
         LOGGER.info(f'send user_num')
         self.send_user_num(user_num)
@@ -91,9 +98,10 @@ class HeteroGMFClient(HeteroGMFBase):
         remote_user_num = self.get_user_num()
         LOGGER.info(f'local user num: {user_num}, remote user num: {remote_user_num}')
         self.user_num = max(remote_user_num, user_num)
+        self.item_num = item_num
 
-        self._model = GMFModel.build_model(user_ids, item_ids, self.params.init_param.embed_dim,
-                                           self.loss, self.optimizer, self.metrics, user_num=self.user_num)
+        self._model = GMFModel.build_model(self.user_num, item_num, self.params.init_param.embed_dim,
+                                           self.loss, self.optimizer, self.metrics)
 
         epoch_degree = float(len(data))
 
@@ -122,6 +130,10 @@ class HeteroGMFClient(HeteroGMFBase):
             LOGGER.warn(f"reach max iter: {self.aggregator_iter}, not converged")
 
     def export_model(self):
+        """
+        export model
+        :return: saved model dict
+        """
         meta = self._get_meta()
         param = self._get_param()
         model_dict = {self.model_meta_name: meta, self.model_param_name: param}
@@ -129,6 +141,10 @@ class HeteroGMFClient(HeteroGMFBase):
         return model_dict
 
     def _get_meta(self):
+        """
+        get meta data for saving model
+        :return:
+        """
         from federatedrec.protobuf.generated import gmf_model_meta_pb2
         LOGGER.info(f"_get_meta")
         meta_pb = gmf_model_meta_pb2.GMFModelMeta()
@@ -137,27 +153,35 @@ class HeteroGMFClient(HeteroGMFBase):
         return meta_pb
 
     def _get_param(self):
+        """
+        get model param for saving model
+        :return:
+        """
         from federatedrec.protobuf.generated import gmf_model_param_pb2
         LOGGER.info(f"_get_param")
         param_pb = gmf_model_param_pb2.GMFModelParam()
         param_pb.saved_model_bytes = self._model.export_model()
-        param_pb.user_ids.extend(self._model.user_ids)
-        param_pb.item_ids.extend(self._model.item_ids)
-
+        param_pb.user_num = self.user_num
+        param_pb.item_num = self.item_num
         return param_pb
 
     def predict(self, data_inst):
+        """
+        predicton function. Note that: GMF model use different DataConverter in evaluation and prediction procedure.
+        :param data_inst: data instance
+        :return: the prediction results
+        """
         LOGGER.info(f"data_inst type: {type(data_inst)}, size: {data_inst.count()}, table name: {data_inst.get_name()}")
         LOGGER.info(f"current flowid: {self.flowid}")
         if self.flowid == 'validate':
-            # use GMFSequenceData
+            # use GMFSequenceData in evaluation procedure (after training procedure)
             data = self.data_converter.convert(data_inst, batch_size=self.batch_size, neg_count=self.model_param.neg_count
                                                , training=True, flow_id=self.flowid)
             keys = data.get_keys()
             labels = data.get_validate_labels()
             label_data = fate_session.parallelize(zip(keys, labels), include_key=True)
         else:
-            # use GMFSequencePredictData
+            # use GMFSequencePredictData in prediction procedure
             data = self.data_converter.convert(data_inst, batch_size=self.batch_size, training=False)
             label_data = data_inst.map(lambda k, v: (k, v.features.astype(int).tolist()[2]))
         LOGGER.info(f"label_data example: {label_data.take(10)}")
@@ -174,29 +198,33 @@ class HeteroGMFClient(HeteroGMFBase):
         return pred_data
 
     def load_model(self, model_dict):
+        """
+        load model from saved model, and initialize the model params
+        :param model_dict:
+        :return:
+        """
         model_dict = list(model_dict["model"].values())[0]
         model_obj = model_dict.get(self.model_param_name)
         meta_obj = model_dict.get(self.model_meta_name)
+        self.user_num = model_obj.user_num
+        self.item_num = model_obj.item_num
         self.model_param.restore_from_pb(meta_obj.params)
         self._init_model(self.model_param)
         self.aggregator_iter = meta_obj.aggregate_iter
-        self._model = GMFModel.restore_model(model_obj.saved_model_bytes)
-        self._model.set_user_ids(model_obj.user_ids)
-        self._model.set_item_ids(model_obj.item_ids)
+        self._model = GMFModel.restore_model(model_obj.saved_model_bytes, model_obj.user_num, model_obj.item_num, self.model_param.init_param.embed_dim)
+        self._model.set_user_num(model_obj.user_num)
+        self._model.set_item_num(model_obj.item_num)
 
 
 class HeteroGMFHost(HeteroGMFClient):
+    """
+    Host HeteroGMF Class, implement the get_user_num and send_user_num function
+    """
 
     def __init__(self):
         super().__init__()
         self.role = consts.HOST
         self.user_ids_sync = user_ids_transfer_sync.Host()
-
-    def send_user_ids(self, data):
-        self.user_ids_sync.send_host_user_ids(data)
-
-    def get_user_ids(self):
-        return self.user_ids_sync.get_guest_user_ids()
 
     def send_user_num(self, data):
         self.user_ids_sync.send_host_user_num(data)
@@ -206,17 +234,14 @@ class HeteroGMFHost(HeteroGMFClient):
 
 
 class HeteroGMFGuest(HeteroGMFClient):
+    """
+    Guest HeteroGMF Class, implement the get_user_num and send_user_num function
+    """
 
     def __init__(self):
         super().__init__()
         self.role = consts.GUEST
         self.user_ids_sync = user_ids_transfer_sync.Guest()
-
-    def send_user_ids(self, data):
-        self.user_ids_sync.send_guest_user_ids(data)
-
-    def get_user_ids(self):
-        return self.user_ids_sync.get_host_user_ids()
 
     def send_user_num(self, data):
         self.user_ids_sync.send_guest_user_num(data)
