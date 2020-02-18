95d4a39fad02377467d7beacc81038f1754fc8f4
weiwee <wbwmat@gmail.com>
2020-02-19 00:07:46 +0800

    implement old procedures using blocks
    Signed-off-by: weiwee <wbwmat@gmail.com>

--- federatedml/framework/homo/procedure/paillier_cipher.py
+++ federatedml/framework/homo/procedure/paillier_cipher.py
@@ -13,30 +13,47 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 #
+import typing
 
-from federatedml.framework.homo.sync import paillier_keygen_sync
+from deprecated import deprecated
 
 from arch.api.utils import log_utils
-from federatedml.framework.homo.sync import paillier_re_cipher_sync
+from federatedml.framework.homo.blocks import paillier_cipher
+from federatedml.secureprotol import PaillierEncrypt
+from federatedml.transfer_variable.base_transfer_variable import Variable
 
 LOGGER = log_utils.getLogger()
 
 
-class Host(paillier_keygen_sync.Host, paillier_re_cipher_sync.Host):
+class Host(paillier_cipher.Client):
 
+    @deprecated(reason="could be remove")
     def register_paillier_cipher(self, transfer_variables):
-        self._register_paillier_keygen(use_encrypt_transfer=transfer_variables.use_encrypt,
-                                       pubkey_transfer=transfer_variables.paillier_pubkey)
-        self._register_paillier_re_cipher(re_encrypt_times_transfer=transfer_variables.re_encrypt_times,
-                                          model_to_re_encrypt_transfer=transfer_variables.to_encrypt_model,
-                                          model_re_encrypted_transfer=transfer_variables.re_encrypted_model)
+        return self
 
 
-class Arbiter(paillier_keygen_sync.Arbiter, paillier_re_cipher_sync.Arbiter):
+class Arbiter(paillier_cipher.Server):
 
+    @deprecated(reason="could be remove")
     def register_paillier_cipher(self, transfer_variables):
-        self._register_paillier_keygen(use_encrypt_transfer=transfer_variables.use_encrypt,
-                                       pubkey_transfer=transfer_variables.paillier_pubkey)
-        self._register_paillier_re_cipher(re_encrypt_times_transfer=transfer_variables.re_encrypt_times,
-                                          model_to_re_encrypt_transfer=transfer_variables.to_encrypt_model,
-                                          model_re_encrypted_transfer=transfer_variables.re_encrypted_model)
+        return self
+
+    def __init__(self):
+        super().__init__()
+        re_encrypt_times_variable: Variable = self._transfer_variable.re_encrypt_times
+        self._parties = re_encrypt_times_variable.roles_to_parties(re_encrypt_times_variable.authorized_src_roles)
+        self._party_idx_map = {party: idx for idx, party in enumerate(self._parties)}
+
+    def paillier_keygen(self, key_length, suffix=tuple()) -> typing.Mapping[int, typing.Union[PaillierEncrypt, None]]:
+        ciphers = super().keygen(key_length, suffix)
+        return {self._party_idx_map[party]: cipher for party, cipher in ciphers.items()}
+
+    def set_re_cipher_time(self, ciphers: typing.Mapping[int, typing.Union[PaillierEncrypt, None]], suffix=tuple()):
+        _ciphers = {self._parties[idx]: cipher for idx, cipher in ciphers.items()}
+        recipher_times = super().set_re_cipher_time(_ciphers, suffix)
+        return {self._party_idx_map[party]: time for party, time in recipher_times.items()}
+
+    def re_cipher(self, iter_num, re_encrypt_times, ciphers, re_encrypt_batches, suffix=tuple()):
+        _ciphers = {self._parties[idx]: cipher for idx, cipher in ciphers.items()}
+        _re_encrypt_times = {self._parties[idx]: time for idx, time in re_encrypt_times.items()}
+        return super().re_cipher(iter_num, _re_encrypt_times, _ciphers, re_encrypt_batches, suffix)
