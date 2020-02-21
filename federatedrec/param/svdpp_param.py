31fc42b27155aa5d518507baa5bbe76c9e9e9717
tonyliu <liucongwust@gmail.com>
2020-02-21 14:29:21 +0800

    remove unused default paramerter
    Signed-off-by: tonyliu <liucongwust@gmail.com>

--- federatedrec/param/svdpp_param.py
+++ federatedrec/param/svdpp_param.py
@@ -35,6 +35,14 @@ class SVDppInitParam(InitParam):
         self.embed_dim = embed_dim
         self.init_method = init_method
 
+    def check(self):
+        if type(self.embed_dim).__name__ not in ["int"] or self.embed_dim < 0:
+            raise ValueError(
+                "GMFInitParam's embed_dim {} not supported, should be 'int'"
+                "and greater than 0".format(
+                    self.embed_dim))
+        return True
+
 
 class SVDppParam(BaseParam):
     """
@@ -42,7 +50,8 @@ class SVDppParam(BaseParam):
 
     Parameters
     ----------
-    optimizer : str, 'SGD', 'RMSprop', 'Adam' or 'Adagrad', default: 'SGD'
+    optimizer : dict, support optimizers in Keras such as  'SGD', 'RMSprop', 'Adam',  or 'Adagrad',
+        default: 'SGD' with learning rate 0.01
         Optimize method
 
     batch_size : int, default: -1
@@ -54,7 +63,8 @@ class SVDppParam(BaseParam):
     max_iter : int, default: 100
         The maximum iteration for training.
 
-    early_stop : str, 'diff', 'weight_diff' or 'abs', default: 'diff'
+    early_stop : dict. early_stop includes 'diff', 'weight_diff' and 'abs',
+        default: {'early_stop':'diff', 'eps': 1e-5}
         Method used to judge converge or not.
             a)	diff： Use difference of loss between two iterations to judge whether converge.
             b)  weight_diff: Use difference between weights of two consecutive iterations
@@ -70,8 +80,8 @@ class SVDppParam(BaseParam):
     def __init__(self,
                  secure_aggregate: bool = True,
                  aggregate_every_n_epoch: int = 1,
-                 early_stop: typing.Union[str, dict, SimpleNamespace] = "diff",
-                 optimizer: typing.Union[str, dict, SimpleNamespace] = 'SGD',
+                 early_stop: typing.Union[str, dict, SimpleNamespace] = {"early_stop": "diff"},
+                 optimizer: typing.Union[str, dict, SimpleNamespace] = {"optimizer": "SGD", "learning_rate": 0.01},
                  batch_size=-1,
                  init_param=SVDppInitParam(),
                  max_iter=100,
@@ -141,17 +151,13 @@ class SVDppParam(BaseParam):
     def _parse_early_stop(self, param):
         """
            Examples:
-
-               1. "early_stop": "diff"
-               2. "early_stop": {
+                "early_stop": {
                        "early_stop": "diff",
                        "eps": 0.0001
                    }
         """
         default_eps = 0.0001
-        if isinstance(param, str):
-            return SimpleNamespace(converge_func=param, eps=default_eps)
-        elif isinstance(param, dict):
+        if isinstance(param, dict):
             early_stop = param.get("early_stop", None)
             eps = param.get("eps", default_eps)
             if not early_stop:
@@ -163,12 +169,10 @@ class SVDppParam(BaseParam):
     def _parse_optimizer(self, param):
         """
         Examples:
-
-            1. "optimize": "SGD"
-            2. "optimize": {
+            "optimize": {
                     "optimizer": "SGD",
                     "learning_rate": 0.05
-                }
+            }
         """
         kwargs = {}
         if isinstance(param, str):
@@ -208,9 +212,10 @@ class HeteroSVDppParam(SVDppParam):
 
     """
 
-    def __init__(self, optimizer='SGD',
-                 batch_size=-1, init_param=SVDppInitParam(),
-                 max_iter=100, early_stop='diff',
+    def __init__(self, batch_size=-1, init_param=SVDppInitParam(),
+                 max_iter=100,
+                 early_stop: typing.Union[str, dict, SimpleNamespace] = {"early_stop": "diff"},
+                 optimizer: typing.Union[str, dict, SimpleNamespace] = {"optimizer": "SGD", "learning_rate": 0.01},
                  predict_param=PredictParam(), cv_param=CrossValidationParam(),
                  aggregate_iters=1, validation_freqs=None
                  ):
