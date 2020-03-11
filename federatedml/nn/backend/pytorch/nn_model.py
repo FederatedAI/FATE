ba10152450deb512123cbf3862794b35e96b5b5e
PossibleDream <magicamazing@163.com>
2020-03-11 19:01:23 +0800

    add some metrics
    Signed-off-by: PossibleDream <magicamazing@163.com>

--- federatedml/nn/backend/pytorch/nn_model.py
+++ federatedml/nn/backend/pytorch/nn_model.py
@@ -13,18 +13,19 @@ from torch.utils.data import DataLoader
 from arch.api.utils import log_utils
 from federatedml.framework.weights import OrderDictWeights, Weights
 from federatedml.nn.homo_nn.nn_model import NNModel, DataConverter
+from sklearn.metrics import accuracy_score, precision_score, auc, recall_score
 
 Logger = log_utils.getLogger()
 
 
 def layers(layer, config, type):
     if type == "cv":
-       if layer == "Conv2d":
-           return torch.nn.Conv2d()
-       if layer == "MaxPool2d":
-           return torch.nn.MaxPool2d()
-       if layer == "AvgPool2d":
-           return torch.nn.AvgPool2d()
+        if layer == "Conv2d":
+            return torch.nn.Conv2d()
+        if layer == "MaxPool2d":
+            return torch.nn.MaxPool2d()
+        if layer == "AvgPool2d":
+            return torch.nn.AvgPool2d()
     elif type == "nlp":
         if layer == "LSTM":
             return torch.nn.LSTM()
@@ -49,6 +50,7 @@ def layers(layer, config, type):
             return torch.nn.BatchNorm2d()
         if layer == "dropout":
             return torch.nn.Dropout()
+
 
 def build_pytorch(nn_define, optimizer, loss):
     model = torch.nn.Sequential()
@@ -75,6 +77,7 @@ def build_loss_fn(loss):
         return torch.nn.SmoothL1Loss()
     else:
         print("loss function not support!")
+
 
 def build_optimzer(optim, model):
     if optim.optimizer == "Adam":
@@ -126,60 +129,72 @@ class PytorchNNModel(NNModel):
                 loss.backward()
                 optimizer.step()
 
-
     def evaluate(self, data: data.dataset, **kwargs):
-        metircs={}
-        metircs["loss"]=0
-        metircs["auccuray"]=0
-<<<<<<< HEAD
+        metircs = {}
+        metircs["loss"] = 0
+        metircs["auccuray"] = 0
+        loss_l1 = torch.nn.L1Loss()
+        loss_mse = torch.nn.MSELoss()
         loss_fn = build_loss_fn(self._loss)
-=======
->>>>>>> e85522637330fc9cd8a252192c14e817e5f48849
+        loss_hingle = torch.nn.HingeEmbeddingLoss()
         self._model.eval()
-        evaluate_data = DataLoader(data, batch_size=1, shuffle=False)
+        evaluate_data = DataLoader(data, batch_size=data.batch_size, shuffle=False)
         result = np.zeros((len(data), data.y_shape[0]))
         eval_label = np.zeros((len(data), data.y_shape[0]))
         num_output_units = data.get_shape()[1]
         index = 0
+        batch_num = 0
         eval_loss = 0
+        mse_loss = 0
+        ab_loss = 0
+        hingle_loss = 0
         for batch_id, (feature, label) in enumerate(evaluate_data):
             feature = torch.tensor(feature, dtype=torch.float32)
             label = torch.tensor(label, dtype=torch.float32)
             y = self._model(feature)
             loss = loss_fn(y, label)
+            l1_loss = loss_l1(y, label)
+            l2_loss = loss_mse(y, label)
+            hin_loss = loss_hingle(y, label)
             eval_loss += loss
-            result[index] = y.detach().numpy()
-            eval_label[index] = label.detach().numpy()
-            index += 1
+            mse_loss += l2_loss
+            ab_loss += l1_loss
+            hingle_loss += hin_loss
+            result[index:index + feature.shape[0]] = y.detach().numpy()
+            eval_label[index:index + feature.shape[0]] = label.detach().numpy()
+            index += feature.shape[0]
+            batch_num += 1
         metircs["loss"] = eval_loss.item()
-        acc=0
+        metircs["mse"] = mse_loss.item()
+        metircs["l1"] = l1_loss.item()
+
+        acc = 0
         if num_output_units[0] == 1:
             for i in range(len(data)):
-                if(result[i]>0.5):
-                   if(eval_label[i]==1):
-                       acc+=1;
+                if (result[i] > 0.5):
+                    result[i] = 1
                 else:
-                    if(eval_label[i]==0):
-                       acc+=1;
+                    result[i] = 0
+            metircs["auccuray"] = accuracy_score(eval_label, result)
+            metircs["precision"] = precision_score(eval_label, result)
         else:
             for i in range(len(data)):
-                if(result[i].argmax()==eval_label[i].argmax()):
-                   acc+=1;
-
-        metircs["auccuray"] = acc / len(data)
+                if (result[i].argmax() == eval_label[i].argmax()):
+                    acc += 1;
+            metircs["auccuray"] = acc / len(data)
         return metircs
 
     def predict(self, data: data.dataset, **kwargs):
 
         result = np.zeros((len(data), data.y_shape[0]))
-        predict_data = DataLoader(data, batch_size=1, shuffle=False)
+        predict_data = DataLoader(data, batch_size=data.batch_size, shuffle=False)
         index = 0
         for batch_id, (feature, label) in enumerate(predict_data):
             feature = torch.tensor(feature, dtype=torch.float32)
-            label = torch.tensor(label, dtype=torch.float32)
+            # label = torch.tensor(label, dtype=torch.float32)
             y = self._model(feature)
-            result[index] = y.detach().numpy()
-            index += 1
+            result[index:index + feature.shape[0]] = y.detach().numpy()
+            index += feature.shape[0]
         return result
 
     def export_model(self):
@@ -199,6 +214,7 @@ class PytorchNNModel(NNModel):
         model = torch.load(f)
         f.close()
         return PytorchNNModel(model)
+
 
 # class PredictNN(NNModel):
 #     def __init__(self, model):
@@ -295,4 +311,3 @@ class PytorchData(data.Dataset):
 class PytorchDataConverter(DataConverter):
     def convert(self, data, *args, **kwargs):
         return PytorchData(data, *args, **kwargs)
-
