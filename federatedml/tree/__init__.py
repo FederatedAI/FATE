95a6d94ce0849203ec1fef540859035b7aa25ceb
chenweijing <talkingwallace@sohu.com>
2020-03-18 18:13:21 +0800

    merge homo
    Signed-off-by: chenweijing <talkingwallace@sohu.com>

--- federatedml/tree/__init__.py
+++ federatedml/tree/__init__.py
@@ -14,18 +14,36 @@
 #  limitations under the License.
 #
 
-from federatedml.tree.node import Node
-from federatedml.tree.node import SplitInfo
-from federatedml.tree.criterion import XgboostCriterion
-from federatedml.tree.splitter import Splitter
-from federatedml.tree.feature_histogram import FeatureHistogram
-from federatedml.tree.decision_tree import DecisionTree
-from federatedml.tree.hetero_decision_tree_guest import HeteroDecisionTreeGuest
-from federatedml.tree.hetero_decision_tree_host import HeteroDecisionTreeHost
-from federatedml.tree.boosting_tree import BoostingTree
-from federatedml.tree.hetero_secureboosting_tree_guest import HeteroSecureBoostingTreeGuest
-from federatedml.tree.hetero_secureboosting_tree_host import HeteroSecureBoostingTreeHost
+from federatedml.tree.tree_core.criterion import XgboostCriterion
+from federatedml.tree.tree_core.node import Node
+from federatedml.tree.tree_core.decision_tree import DecisionTree
+from federatedml.tree.tree_core.splitter import SplitInfo
+from federatedml.tree.tree_core.splitter import Splitter
+from federatedml.tree.tree_core.boosting_tree import BoostingTree
+from federatedml.tree.tree_core.feature_histogram import FeatureHistogram
+from federatedml.tree.tree_core.feature_histogram import HistogramBag, FeatureHistogramWeights
 
-__all__ = ["Node", "SplitInfo", "HeteroSecureBoostingTreeGuest", "HeteroSecureBoostingTreeHost",
+
+from federatedml.tree.hetero.hetero_decision_tree_host import HeteroDecisionTreeHost
+from federatedml.tree.hetero.hetero_decision_tree_guest import HeteroDecisionTreeGuest
+from federatedml.tree.hetero.hetero_secureboosting_tree_guest import HeteroSecureBoostingTreeGuest
+from federatedml.tree.hetero.hetero_secureboosting_tree_host import HeteroSecureBoostingTreeHost
+
+from federatedml.tree.homo.homo_secureboosting_aggregator import SecureBoostClientAggregator, SecureBoostArbiterAggregator,\
+    DecisionTreeClientAggregator, DecisionTreeArbiterAggregator
+from federatedml.tree.homo.homo_decision_tree_client import HomoDecisionTreeClient
+from federatedml.tree.homo.homo_decision_tree_arbiter import HomoDecisionTreeArbiter
+from federatedml.tree.homo.homo_secureboosting_client import HomoSecureBoostingTreeClient
+from federatedml.tree.homo.homo_secureboosting_arbiter import HomoSecureBoostingTreeArbiter
+
+__all__ = ["Node", "HeteroSecureBoostingTreeGuest", "HeteroSecureBoostingTreeHost",
            "HeteroDecisionTreeHost", "HeteroDecisionTreeGuest", "Splitter",
-           "FeatureHistogram", "XgboostCriterion", "DecisionTree"]
+           "FeatureHistogram", "XgboostCriterion", "DecisionTree", 'SplitInfo', "BoostingTree",
+           "HistogramBag", "FeatureHistogramWeights","HomoDecisionTreeClient", "HomoDecisionTreeArbiter",
+           "SecureBoostArbiterAggregator", "SecureBoostClientAggregator"
+           , "DecisionTreeArbiterAggregator", 'DecisionTreeClientAggregator', "HomoSecureBoostingTreeArbiter",
+           "HomoSecureBoostingTreeClient", ]
+
+"""
+
+"""
