afb68bb341ccb52373608e5e920b2d87ef24252c
wzh <15779896112@163.com>
2020-04-09 23:41:29 +0800

    add session init
    Signed-off-by: wzh <15779896112@163.com>

--- fate_flow/apps/data_access_app.py
+++ fate_flow/apps/data_access_app.py
@@ -64,6 +64,7 @@ def download_upload(access_module):
         data['table_name'] = request_config["table_name"]
         data['namespace'] = request_config["namespace"]
         data_table = session.get_data_table(name=request_config["table_name"], namespace=request_config["namespace"])
+        session.init()
         count = data_table.count()
         if count and int(request_config.get('drop', 2)) == 2:
             return get_json_result(retcode=100,
