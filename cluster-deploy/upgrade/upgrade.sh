95d4802f43bb0af6a9d763f740548b7113e58c09
wzh <15779896112@163.com>
2020-04-22 14:09:51 +0800

    add upgrade file
    Signed-off-by: wzh <15779896112@163.com>

---
+++ cluster-deploy/upgrade/upgrade.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+current_version=
+fate_dir=/data/projects/fate
+
+
+dir=$(dirname $(readlink -f "$0"))
+python_dir="$dir/$current_version/python"
+
+
+echo "start update file"
+cp -rf $python_dir $fate_dir
+
+# update settings
+python modify_settings.py "$fate_dir/python/fate_flow/settings.py"
+sed -i 's/\r$//' "$fate_dir/python/fate_flow/settings.py"
+echo "update file success"  
+
+
+sh "$fate_dir/python/fate_flow/service.sh" restart
+
+
+
+
