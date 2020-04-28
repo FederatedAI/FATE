cc2a216853492fc83f2d5186400bf4e328223856
v_wbymxu <g6xuyiming@163.com>
2020-04-28 23:52:53 +0800

    update document of standalone-deployment
    Signed-off-by: v_wbymxu <g6xuyiming@163.com>

--- standalone-deploy/docker/fateboard/docker-entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-cd /fate/fateboard
-java -Dspring.config.location=/fate/fateboard/conf/application.properties -Dssh_config_file=/fate/fateboard/ssh/ -Xmx2048m -Xms2048m -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:gc.log -XX:+HeapDumpOnOutOfMemoryError -jar fateboard.jar
-
-exec "$@"
