0427f7b5e8cd1e82d6b2046043be84f5838e89ed
weiwee <wbwmat@gmail.com>
2020-04-30 11:49:54 +0800

    embed standalone version of eggroll 1.x
    Signed-off-by: weiwee <wbwmat@gmail.com>

--- fate_flow/utils/proto_compatibility.py
+++ fate_flow/utils/proto_compatibility.py
@@ -1,10 +1,20 @@
-from arch.api import _EGGROLL_VERSION
+from arch.api.utils import log_utils
 
-if _EGGROLL_VERSION < 2:
-    from eggroll.api.proto import basic_meta_pb2
-    from eggroll.api.proto import proxy_pb2, proxy_pb2_grpc
-    from eggroll.api.proto import proxy_pb2_grpc
-else:
+LOGGER = log_utils.getLogger()
+
+try:
     from eggroll.core.proto import basic_meta_pb2
     from eggroll.core.proto import proxy_pb2, proxy_pb2_grpc
     from eggroll.core.proto import proxy_pb2_grpc
+except ImportError as e:
+    LOGGER.warning("can't import pb from eggroll 2.x, assuming eggroll 1.x used")
+
+    try:
+        from eggroll.api.proto import basic_meta_pb2
+        from eggroll.api.proto import proxy_pb2, proxy_pb2_grpc
+        from eggroll.api.proto import proxy_pb2_grpc
+    except ImportError as e:
+        LOGGER.warning("eggroll 1.x not deployed, assuming use embed standalone version")
+        from arch.standalone.proto import basic_meta_pb2
+        from arch.standalone.proto import proxy_pb2, proxy_pb2_grpc
+        from arch.standalone.proto import proxy_pb2_grpc
