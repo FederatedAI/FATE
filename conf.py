21cd71b3b8de2bc8cacbf6d71b9aa7e153820303
weiwee <wbwmat@gmail.com>
2020-05-06 20:44:48 +0800

    fix table size
    Signed-off-by: weiwee <wbwmat@gmail.com>

--- conf.py
+++ conf.py
@@ -56,7 +56,11 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['doc/readthedoc/_static']
-
+html_context = {
+    'css_files': [
+        '_static/theme_overrides.css',  # override wide tables in RTD theme
+        ],
+     }
 add_module_names = False
 master_doc = 'index'
 
