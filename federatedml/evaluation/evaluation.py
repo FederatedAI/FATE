626d7badc7ef28c66551642bcda9d0e76040ae86
weijing <talkingwallace@sohu.com>
2020-05-21 17:43:39 +0800

    fix evaluation multi accuracy metric
    Signed-off-by: chenweijing <talkingwallace@sohu.com>

--- federatedml/evaluation/evaluation.py
+++ federatedml/evaluation/evaluation.py
@@ -370,7 +370,7 @@ class Evaluation(ModelBase):
 
     def __save_accuracy_curve(self, metric, metric_res, metric_name, metric_namespace, data_name):
 
-        if self.eval_type == consts.BINARY:
+        if self.eval_type == consts.MULTY:
             return
 
         score, cuts, thresholds = metric_res[1]
