cc254b00d8e7f73409b53a58e3edcd850f4c6f05
nemirorox <yolandawu131@gmail.com>
2020-05-27 17:18:43 +0800

    add some pipeline component
    Signed-off-by: nemirorox <yolandawu131@gmail.com>

---
+++ pipeline/component/hetero_feature_binning.py
@@ -0,0 +1,36 @@
+#
+#  Copyright 2019 The FATE Authors. All Rights Reserved.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+
+from pipeline.component.component_base import Component
+from pipeline.interface.output import Output
+from federatedml.param.feature_binning_param import FeatureBinningParam
+
+
+class HeteroFeatureBinning(Component, FeatureBinningParam):
+    def __init__(self, **kwargs):
+        Component.__init__(self, **kwargs)
+
+        print (self.name)
+        new_kwargs = self.erase_component_base_param(**kwargs)
+
+        FeatureBinningParam.__init__(self, **new_kwargs)
+        self.output = Output(self.name, has_model=False)
+        self._module_name = "HeteroFeatureBinning"
+
+    def summary(self, data, metric_keyword):
+        # To be implemented
+        pass
+
