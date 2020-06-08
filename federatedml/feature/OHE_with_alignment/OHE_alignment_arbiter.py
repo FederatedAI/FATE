ffacdd178d65c84fcd1611643cf46bdad0c2fb49
cheewei <second_comet@yahoo.com>
2020-06-08 13:22:42 +0800

    ohe_alignment unit test
    Signed-off-by: cheewei <second_comet@yahoo.com>

--- federatedml/feature/OHE_with_alignment/OHE_alignment_arbiter.py
+++ federatedml/feature/OHE_with_alignment/OHE_alignment_arbiter.py
@@ -13,7 +13,6 @@ from federatedml.framework.homo.procedure import aggregator
 from federatedml.framework.homo.procedure import paillier_cipher
 from federatedml.feature.OHE_with_alignment.OHE_alignment_base import OHEAlignmentBase
 from federatedml.util import consts
-import ast
 from collections import defaultdict
 
 
@@ -29,63 +28,52 @@ class OHEAlignmentArbiter(OHEAlignmentBase):
         # self.aggregator = aggregator.Arbiter()
         self.cipher = paillier_cipher.Arbiter()
 
-    def _init_model(self, params):
-        super()._init_model(params)
-        #self.cipher.register_paillier_cipher(self.transfer_variable)
 
-    def fit(self, data_instances=None):
-
-        """This is used when there is a need for aligment within the
+    def combineAllColumnHeaders(self, guest_columns, host_columns):
+        """ This is used when there is a need for aligment within the
         federated learning. The function would align the column headers from
         guest and host and send the new aligned headers back.
 
         Args: 
             data_instances: data itself
 
-        
         Returns:
             Combine all the column headers from guest and host 
             if there is alignment is used
         """
+        all_cols_dict = defaultdict(list)
 
+        #Obtain all the guest headers
+        for guest_cols in guest_columns:
+            for k, v in guest_cols.items():
+                all_cols_dict[k] = list(set(all_cols_dict[k]+v))
         
-        # self.init_schema(data_instances)
-        # self._init_params(data_instances)
+        #Obtain all the host headers
+        for host_cols in host_columns:
+            for k, v in host_cols.items():
+                all_cols_dict[k] = list(set(all_cols_dict[k]+v))
+
+        #Align all of them together
+        combined_all_cols = {}
+        for el in all_cols_dict.keys():
+            combined_all_cols[el] = all_cols_dict[el]
+                
+        LOGGER.debug("{} combined cols: {}".format(self.role,combined_all_cols))
+
+        return combined_all_cols
+
+
+    def fit(self, data_instances=None):
         
         if self.need_alignment:
-
             self.guest_columns = self.transfer_variable.guest_columns.get(idx = -1) # getting guest column
             self.host_columns = self.transfer_variable.host_columns.get(idx = -1) # getting host column
-        
-            # let's do alignment here
-            # simple method first, we will implement Needleman–Wunsch algo in future, if needed
-            all_cols_dict = defaultdict(list)
 
-            #Obtain all the guest headers
-            for item in self.guest_columns:
-                guest_cols = ast.literal_eval(item)
-                for k, v in guest_cols.items():
-                    all_cols_dict[k] = list(set(all_cols_dict[k]+v))
+            combined_all_cols= self.combineAllColumnHeaders(self.guest_columns, self.host_columns)
             
-            #Obtain all the host headers
-            for item in self.host_columns:
-                host_cols = ast.literal_eval(item)
-                for k, v in host_cols.items():
-                    all_cols_dict[k] = list(set(all_cols_dict[k]+v))
-
-            #Align all of them together
-            combined_all_cols = {}
-            for el in all_cols_dict.keys():
-                combined_all_cols[el] = all_cols_dict[el]
-            
-            
-            LOGGER.info("**AIFEL** combined cols: {}".format(combined_all_cols))
-
             #Send the aligned headers back to guest and host
-            self.transfer_variable.aligned_columns.remote("{}".format(combined_all_cols), role = consts.HOST, idx = -1)
-            self.transfer_variable.aligned_columns.remote("{}".format(combined_all_cols), role = consts.GUEST, idx = -1)
-
-
+            self.transfer_variable.aligned_columns.remote(combined_all_cols, role = consts.HOST, idx = -1)
+            self.transfer_variable.aligned_columns.remote(combined_all_cols, role = consts.GUEST, idx = -1)
 
     def _get_meta(self):
         pass
--- federatedml/feature/OHE_with_alignment/OHE_alignment_base.py
+++ federatedml/feature/OHE_with_alignment/OHE_alignment_base.py
@@ -6,7 +6,6 @@
 #  base class for OHE alignment
 
 import functools
-
 from arch.api.utils import log_utils
 from federatedml.feature.one_hot_encoder import OneHotEncoder
 from federatedml.feature.one_hot_encoder import OneHotInnerParam
@@ -47,6 +46,7 @@ class OHEAlignmentBase(OneHotEncoder):
     def init_schema(self, data_instance):
         if data_instance is None:
             return
+
         self.schema = data_instance.schema
         self.header = self.schema.get('header')
 
@@ -54,38 +54,92 @@ class OHEAlignmentBase(OneHotEncoder):
         if data_instances is None: 
             return 
 
-        if len(self.schema) == 0:
-            self.schema = data_instances.schema
+        super(OHEAlignmentBase, self)._init_params(data_instances)
 
-        if self.inner_param is not None:
-            return
-        self.inner_param = OneHotInnerParam()
-        # self.schema = data_instances.schema
-        LOGGER.debug("In _init_params, schema is : {}".format(self.schema))
-        header = get_header(data_instances)
-        self.inner_param.set_header(header)
+    def fit(self, data_instances):
+        """This function allows for one-hot-encoding of the 
+        columns with or without alignment with the other parties
+        in the federated learning.
 
-        if self.model_param.transform_col_indexes == -1:
-            self.inner_param.set_transform_all()
-        else:
-            self.inner_param.add_transform_indexes(self.model_param.transform_col_indexes)
-            self.inner_param.add_transform_names(self.model_param.transform_col_names)
-    
-    def _transform_schema(self):
-        header = self.inner_param.header.copy()
-        LOGGER.debug("**AIFEL** [Result][OneHotEncoder]Before one-hot, "
-                     "data_instances schema is : {}".format(self.inner_param.header))
-        result_header = []
-        for col_name in header:
-            if col_name not in self.col_maps:
-                result_header.append(col_name)
-                continue
-            pair_obj = self.col_maps[col_name]
+        Args:
+            data_instances: data the guest has access to
 
-            new_headers = pair_obj.transformed_headers
-            result_header.extend(new_headers)
+        Returns:
+            if alignment is on, then the one-hot-encoding data_instances are done with
+            alignment with parties involved in federated learning else,
+            the data is one-hot-encoded independently
 
-        self.inner_param.set_result_header(result_header)
-        LOGGER.debug("**AIFEL** [Result][OneHotEncoder]After one-hot, data_instances schema is : {}".format(result_header))
+        """
 
+
+        self.init_schema(data_instances)
+        self._init_params(data_instances)
+        # keep a copy of original header
+        ori_header = self.inner_param.header.copy() 
+
+        # obtain the individual column headers with their values
+        f1 = functools.partial(self.record_new_header,
+                            inner_param=self.inner_param)
+        self.col_maps = data_instances.mapPartitions(f1).reduce(self.merge_col_maps)
+        col_maps = {}
+        for col_name, pair_obj in self.col_maps.items():
+            values = [str(x) for x in pair_obj.values]            
+            col_maps[col_name] = values
+        
+        LOGGER.debug("new col_maps is: {}".format(col_maps))
+
+        if self.need_alignment:
+
+            # Send col_maps to arbiter
+            if self.role == consts.HOST:
+                self.transfer_variable.host_columns.remote(col_maps, role = consts.ARBITER, idx = -1) 
+            elif self.role == consts.GUEST:
+                self.transfer_variable.guest_columns.remote(col_maps, role = consts.ARBITER, idx = -1) 
+            
+            # Receive aligned columns from arbiter
+            aligned_columns = self.transfer_variable.aligned_columns.get(idx=-1)
+            aligned_col_maps = aligned_columns[0]
+            LOGGER.debug("{} aligned columns received are: {}".format(self.role,aligned_col_maps))
+
+            # All the headers - original or new after alignment - are appended together
+            new_header = []
+            transform_col_names = []
+            for col in ori_header: 
+                if col not in aligned_col_maps:
+                    new_header.append(col)
+                    continue
+                transform_col_names.append(col)
+                for vv in aligned_col_maps[col]:
+                    new_header.append(col+'_'+vv)
+
+            LOGGER.debug("new transform col names after format received aligned columns: {}".format(transform_col_names))        
+            LOGGER.debug("new header after format received aligned columns: {}".format(new_header))
+
+            self.inner_param.add_transform_names = transform_col_names
+            self.inner_param.set_result_header(new_header)
+
+            LOGGER.debug("Before set_schema in fit, schema is : {}, header: {}".format(self.schema,
+                                                                                        self.inner_param.header))
+
+        else:                                                                        
+            self._transform_schema() 
+        
+        data_instances = self.transform(data_instances)
+        LOGGER.debug("[Result][OHEAlignment{}] After transform in fit, schema is : {}, header: {}".format(self.role, self.schema,
+                                                                                self.inner_param.header))
+                
+
+                
+        return data_instances
+
+class OHEAlignmentGuest(OHEAlignmentBase):
+    def __init__(self):
+        super(OHEAlignmentGuest, self).__init__()
+        self.role = consts.GUEST
+        
+
+class OHEAlignmentHost(OHEAlignmentBase):
+    def __init__(self):
+        super(OHEAlignmentHost, self).__init__()
+        self.role = consts.HOST
     
