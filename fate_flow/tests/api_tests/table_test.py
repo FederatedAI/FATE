c4e64f39e627ba07c62bebfccc2e4396cc6ca507
wzh <15779896112@163.com>
2020-06-30 09:47:44 +0800

    modify test example
    Signed-off-by: wzh <15779896112@163.com>

--- fate_flow/tests/api_tests/table_test.py
+++ fate_flow/tests/api_tests/table_test.py
@@ -11,9 +11,9 @@ from fate_flow.settings import HTTP_PORT, API_VERSION, WORK_MODE
 class TestTable(unittest.TestCase):
     def setUp(self):
         self.data_dir = os.path.join(file_utils.get_project_base_directory(), "examples", "data")
-        self.upload_config = {"file": os.path.join(self.data_dir, "breast_b.csv"), "head": 1, "partition": 10,
-                              "work_mode": WORK_MODE, "namespace": "fate_flow_test_breast",
-                              "table_name": "breast_b", "use_local_data": 0, 'drop': 0}
+        self.upload_config = {"file": os.path.join(self.data_dir, "breast_hetero_guest.csv"), "head": 1, "partition": 10,
+                              "work_mode": WORK_MODE, "namespace": "fate_flow_test_table_breast_hetero",
+                              "table_name": "breast_hetero_guest", "use_local_data": 0, 'drop': 0}
         self.server_url = "http://{}:{}/{}".format('127.0.0.1', HTTP_PORT, API_VERSION)
 
     def test_upload_guest(self):
@@ -30,14 +30,16 @@ class TestTable(unittest.TestCase):
 
     def test_table_info(self):
         response = requests.post("/".join([self.server_url, 'table', 'table_info']),
-                                 json={'table_name': 'breast_b', 'namespace': 'fate_flow_test_breast'})
+                                 json={'table_name': 'breast_hetero_guest',
+                                       'namespace': 'fate_flow_test_table_breast_hetero'})
         self.assertTrue(response.status_code in [200, 201])
         self.assertTrue(int(response.json()['retcode']) == 0)
 
     def test_table_delete(self):
         # submit
         response = requests.post("/".join([self.server_url, 'table', 'delete']),
-                                 json={'table_name': 'breast_b', 'namespace': 'fate_flow_test_breast'})
+                                 json={'table_name': 'breast_hetero_guest',
+                                       'namespace': 'fate_flow_test_table_breast_hetero'})
         self.assertTrue(response.status_code in [200, 201])
         self.assertTrue(int(response.json()['retcode']) == 0)
 
