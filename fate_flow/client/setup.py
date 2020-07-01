02cb0476c4a0e5c7b35e66a6152226012f5c7c8f
ryan.zhang <izhfeng@163.com>
2020-07-01 10:30:42 +0800

    modify requires list

--- fate_flow/client/setup.py
+++ fate_flow/client/setup.py
@@ -21,7 +21,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=['Click', 'six', 'ruamel.yaml', 'cachetools',
-                      'python-dotenv', 'kazoo', 'requests'],
+                      'python-dotenv', 'kazoo', 'requests', 'requests_toolbelt'],
     entry_points='''
         [console_scripts]
         flow=fate_flow.client.scripts.flow:flow_cli
