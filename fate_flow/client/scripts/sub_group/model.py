4ea83d0de75dd87d4f1160a81d9dc9c78d4f9d65
ryan.zhang <izhfeng@163.com>
2020-07-10 16:58:40 +0800

    merge import/restore, export/store methods

--- fate_flow/client/scripts/sub_group/model.py
+++ fate_flow/client/scripts/sub_group/model.py
@@ -108,6 +108,7 @@ def restore(ctx, **kwargs):
 # TODO Rename this method
 @model.command(short_help="Import Model Command")
 @click.argument('conf_path', type=click.Path(exists=True), metavar='<CONF_PATH>')
+@click.argument('type', type=click.Choice(['0', '1']), default='0', metavar='<TYPE>')
 @click.pass_context
 def imp(ctx, **kwargs):
     """
@@ -119,22 +120,28 @@ def imp(ctx, **kwargs):
 
     \b
     <CONF_PATH> : Configuration File Path
+    <TYPE> : Choose import type from 0(default) and 1.
+    0 means import from files, while 1 means import from database which you can specify in configuration file.
     """
-    config_data, dsl_data = preprocess(**kwargs)
-    # TODO KeyError Catchting
-    file_path = config_data["file"]
-    if not os.path.isabs(file_path):
-        file_path = os.path.join(file_utils.get_project_base_directory(), file_path)
-    if os.path.exists(file_path):
-        files = {'file': open(file_path, 'rb')}
+    if not kwargs.get('type'):
+        config_data, dsl_data = preprocess(**kwargs)
+        file_path = config_data["file"]
+        if not os.path.isabs(file_path):
+            file_path = os.path.join(file_utils.get_project_base_directory(), file_path)
+        if os.path.exists(file_path):
+            files = {'file': open(file_path, 'rb')}
+        else:
+            raise Exception('The file is obtained from the fate flow client machine, but it does not exist, '
+                            'please check the path: {}'.format(file_path))
+        access_server('post', ctx, 'model/import', config_data, files=files)
     else:
-        raise Exception('The file is obtained from the fate flow client machine, but it does not exist, '
-                        'please check the path: {}'.format(file_path))
-    access_server('post', ctx, 'model/import', config_data, files=files)
+        config_data, dsl_data = preprocess(**kwargs)
+        access_server('post', ctx, 'model/restore', config_data)
 
 
 @model.command(short_help="Export Model Command")
 @click.argument('conf_path', type=click.Path(exists=True), metavar='<CONF_PATH>')
+@click.argument('type', type=click.Choice(['0', '1']), default='0', metavar='<TYPE>')
 @click.pass_context
 def export(ctx, **kwargs):
     """
@@ -146,20 +153,26 @@ def export(ctx, **kwargs):
 
     \b
     <CONF_PATH> : Configuration File Path
+    <TYPE> : Choose export type from 0(default) and 1.
+    0 means export to files, while 1 means export to database which you can specify in configuration file.
     """
-    config_data, dsl_data = preprocess(**kwargs)
-    with closing(access_server('get', ctx, 'model/export', config_data, False, stream=True)) as response:
-        if response.status_code == 200:
-            archive_file_name = re.findall("filename=(.+)", response.headers["Content-Disposition"])[0]
-            os.makedirs(config_data["output_path"], exist_ok=True)
-            archive_file_path = os.path.join(config_data["output_path"], archive_file_name)
-            with open(archive_file_path, 'wb') as fw:
-                for chunk in response.iter_content(1024):
-                    if chunk:
-                        fw.write(chunk)
-            response = {'retcode': 0,
-                        'file': archive_file_path,
-                        'retmsg': 'download successfully, please check {}'.format(archive_file_path)}
-        else:
-            response = response.json()
-    click.echo(response.json() if isinstance(response, requests.models.Response) else response)
+    if not kwargs.get('type'):
+        config_data, dsl_data = preprocess(**kwargs)
+        with closing(access_server('get', ctx, 'model/export', config_data, False, stream=True)) as response:
+            if response.status_code == 200:
+                archive_file_name = re.findall("filename=(.+)", response.headers["Content-Disposition"])[0]
+                os.makedirs(config_data["output_path"], exist_ok=True)
+                archive_file_path = os.path.join(config_data["output_path"], archive_file_name)
+                with open(archive_file_path, 'wb') as fw:
+                    for chunk in response.iter_content(1024):
+                        if chunk:
+                            fw.write(chunk)
+                response = {'retcode': 0,
+                            'file': archive_file_path,
+                            'retmsg': 'download successfully, please check {}'.format(archive_file_path)}
+            else:
+                response = response.json()
+        click.echo(response.json() if isinstance(response, requests.models.Response) else response)
+    else:
+        config_data, dsl_data = preprocess(**kwargs)
+        access_server('post', ctx, 'model/store', config_data)
