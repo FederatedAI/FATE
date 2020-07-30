f604d19253d1f315aff8541b5e7f064286c4c3b5
nemirorox <yolandawu131@gmail.com>
2020-07-30 21:21:55 +0800

    add demo
    Signed-off-by: nemirorox <yolandawu131@gmail.com>

--- pipeline/demo/hetero_stepwise/pipeline-stepwise-lr.py
+++ pipeline/demo/hetero_stepwise/pipeline-stepwise-lr.py
@@ -11,8 +11,8 @@ guest = 9999
 host = 10000
 arbiter = 10002
 
-guest_train_data = {"name": "breast_hetero_guest", "namespace": "experiment"}
-host_train_data = {"name": "breast_hetero_host", "namespace": "experiment"}
+guest_train_data = {"name": "breast_hetero_mini_guest", "namespace": "experiment"}
+host_train_data = {"name": "breast_hetero_min_host", "namespace": "experiment"}
 
 input_0 = Input(name="train_data")
 print ("get input_0's init name {}".format(input_0.name))
@@ -24,16 +24,13 @@ dataio_0.get_party_instance(role='guest', party_id=guest).algorithm_param(with_l
 dataio_0.get_party_instance(role='host', party_id=host).algorithm_param(with_label=False)
 
 intersect_0 = Intersection(name="intersection_0")
-hetero_lr_0 = HeteroLR(name="hetero_lr_0", early_stop="weight_diff", max_iter=2,
-                       stepwise_param={"need_stepwise": True, "max_step": 1, "direction": "backward"})
+hetero_lr_0 = HeteroLR(name="hetero_lr_0", early_stop="diff", max_iter=5,
+                       stepwise_param={"need_stepwise": True, "max_step": 2, "direction": "backward"})
 
 print ("get input_0's name {}".format(input_0.name))
 pipeline.add_component(dataio_0, data=Data(data=input_0.data))
 pipeline.add_component(intersect_0, data=Data(data=dataio_0.output.data))
 pipeline.add_component(hetero_lr_0, data=Data(train_data=intersect_0.output.data))
-
-# pipeline.set_deploy_end_component([dataio_0])
-# pipeline.deploy_component([dataio_0])
 
 pipeline.compile()
 
@@ -47,26 +44,5 @@ pipeline.fit(backend=Backend.EGGROLL, work_mode=WorkMode.STANDALONE,
 
                        })
 
-# print (pipeline.get_component("intersection_0").get_output_data())
-# print (pipeline.get_component("dataio_0").get_model_param())
-# print (pipeline.get_component("hetero_lr_0").get_model_param())
+
 print (pipeline.get_component("hetero_lr_0").get_summary())
-# pipeline.get_component("intersection_0").summary("intersect_count", "intersect_rate")
-
-
-# predict
-
-pipeline.predict(backend=Backend.EGGROLL, work_mode=WorkMode.STANDALONE,
-                              feed_dict={input_0:
-                                             {"guest":
-                                                  {9999: guest_train_data},
-                                              "host": {
-                                                  10000: host_train_data
-                                              }
-                                              }
-                                         })
-
-# with open("output.pkl", "wb") as fout:
-#     fout.write(pipeline.dump())
-
-
