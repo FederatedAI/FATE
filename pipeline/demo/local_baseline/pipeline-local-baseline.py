d7c14bc5a29f00113147753a33a57a95d1a54226
nemirorox <yolandawu131@gmail.com>
2020-07-31 15:13:38 +0800

    update demo
    Signed-off-by: nemirorox <yolandawu131@gmail.com>

--- pipeline/demo/local_baseline/pipeline-local-baseline.py
+++ pipeline/demo/local_baseline/pipeline-local-baseline.py
@@ -10,38 +10,41 @@ from pipeline.component.local_baseline import LocalBaseline
 from pipeline.interface.data import Data
 
 guest = 9999
-hosts = [10000, 10001]
+host = 10000
 arbiter = 10002
 
-guest_train_data = {"name": "breast_hetero_guest", "namespace": "experiment"}
-host_train_data = [{"name": "breast_hetero_host", "namespace": "experiment"},
-                   {"name": "breast_hetero_host", "namespace": "experiment"}]
+guest_train_data = {"name": "vehicle_scale_hetero_guest", "namespace": "experiment"}
+host_train_data = {"name": "vehicle_scale_hetero_host", "namespace": "experiment"}
 
 input_0 = Input(name="train_data")
 print ("get input_0's init name {}".format(input_0.name))
 
-pipeline = PipeLine().set_initiator(role='guest', party_id=guest).set_roles(guest=guest, host=hosts, arbiter=arbiter)
+pipeline = PipeLine().set_initiator(role='guest', party_id=guest).set_roles(guest=guest, host=host, arbiter=arbiter)
 dataio_0 = DataIO(name="dataio_0")
 
-dataio_0.get_party_instance(role='guest', party_id=guest).algorithm_param(with_label=True, output_format="dense")
-dataio_0.get_party_instance(role='host', party_id=hosts).algorithm_param(with_label=False)
+dataio_0.get_party_instance(role='guest', party_id=guest).algorithm_param(with_label=True, output_format="dense",
+                                                                          label_type="int", label_name="y")
+dataio_0.get_party_instance(role='host', party_id=host).algorithm_param(with_label=False)
 
-intersect_0 = Intersection(name="intersection_0")
-hetero_lr_0 = HeteroLR(name="hetero_lr_0", early_stop="weight_diff", max_iter=20)
+intersect_0 = Intersection(name="intersection_0", intersect_method="rsa", sync_intersect_ids=True,
+                           only_output_key=False)
+hetero_lr_0 = HeteroLR(name="hetero_lr_0", penalty="L2", optimizer= "nesterov_momentum_sgd",
+                       tol=0.0001, alpha=0.0001, max_iter=30, batch_size=-1,
+                       early_stop="diff", learning_rate=0.15, init_param={"init_method": "zeros"})
 
 local_baseline_0 = LocalBaseline(name="local_baseline_0", model_name="LogisticRegression",
                                  model_opts= {"penalty": "l2", "tol": 0.0001, "C": 1.0, "fit_intercept": True,
-                                                 "solver": "lbfgs", "max_iter": 5})
+                                                 "solver": "lbfgs", "max_iter": 5, "multi_class": "ovr"})
 local_baseline_0.get_party_instance(role='guest', party_id=guest).algorithm_param(need_run=True)
-local_baseline_0.get_party_instance(role='host', party_id=hosts).algorithm_param(need_run=False)
+local_baseline_0.get_party_instance(role='host', party_id=host).algorithm_param(need_run=False)
 
-evaluation_0 = Evaluation(name="evaluation_0")
+evaluation_0 = Evaluation(name="evaluation_0", eval_type="multi", pos_label=1)
 evaluation_0.get_party_instance(role='guest', party_id=guest).algorithm_param(need_run=True)
-evaluation_0.get_party_instance(role='host', party_id=hosts).algorithm_param(need_run=False)
+evaluation_0.get_party_instance(role='host', party_id=host).algorithm_param(need_run=False)
 
 evaluation_1 = Evaluation(name="evaluation_1")
 evaluation_1.get_party_instance(role='guest', party_id=guest).algorithm_param(need_run=True)
-evaluation_1.get_party_instance(role='host', party_id=hosts).algorithm_param(need_run=False)
+evaluation_1.get_party_instance(role='host', party_id=host).algorithm_param(need_run=False)
 
 print ("get input_0's name {}".format(input_0.name))
 pipeline.add_component(dataio_0, data=Data(data=input_0.data))
@@ -59,10 +62,7 @@ pipeline.compile()
 pipeline.fit(backend=Backend.EGGROLL, work_mode=WorkMode.STANDALONE,
              feed_dict={input_0:
                            {"guest": {9999: guest_train_data},
-                            "host": {
-                              10000: host_train_data[0],
-                              10001: host_train_data[1]
-                             }
+                            "host": {10000: host_train_data}
                             }
 
                        })
