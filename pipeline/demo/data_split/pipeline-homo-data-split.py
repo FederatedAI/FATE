b28a55c91002be9ff1c0afa6a50e5d4f86fe2504
mgqa34 <mgq3374541@163.com>
2020-08-10 19:31:51 +0800

    update data-split pipeline demo
    Signed-off-by: mgqa34 <mgq3374541@163.com>

--- pipeline/demo/data_split/pipeline-homo-data-split.py
+++ pipeline/demo/data_split/pipeline-homo-data-split.py
@@ -1,9 +1,10 @@
+from pipeline.component.homo_data_split import HomoDataSplit
+from pipeline.component.reader import Reader
+
 from pipeline.backend.config import Backend
 from pipeline.backend.config import WorkMode
 from pipeline.backend.pipeline import PipeLine
 from pipeline.component.dataio import DataIO
-from pipeline.component.homo_data_split import HomoDataSplit
-from pipeline.component.input import Input
 from pipeline.interface.data import Data
 
 guest = 9999
@@ -12,7 +13,9 @@ host = 10000
 guest_train_data = {"name": "breast_homo_guest", "namespace": "experiment"}
 host_train_data = {"name": "breast_homo_host", "namespace": "experiment"}
 
-input_0 = Input(name="train_data")
+reader_0 = Reader(name="reader_0")
+reader_0.get_party_instance(role='guest', party_id=guest).algorithm_param(table=guest_train_data)
+reader_0.get_party_instance(role='host', party_id=host).algorithm_param(table=host_train_data)
 
 pipeline = PipeLine().set_initiator(role='guest', party_id=guest).set_roles(guest=guest, host=host)
 
@@ -24,21 +27,13 @@ dataio_0.get_party_instance(role='host', party_id=host).algorithm_param(with_lab
 
 homo_data_split_0 = HomoDataSplit(name="homo_data_split_0", stratified=True, test_size=0.3, validate_size=0.2)
 
-pipeline.add_component(dataio_0, data=Data(data=input_0.data))
+pipeline.add_component(reader_0)
+pipeline.add_component(dataio_0, data=Data(data=reader_0.output.data))
 pipeline.add_component(homo_data_split_0, data=Data(data=dataio_0.output.data))
 
 pipeline.compile()
 
-pipeline.fit(backend=Backend.EGGROLL, work_mode=WorkMode.STANDALONE,
-             feed_dict={input_0:
-                           {"guest": {9999: guest_train_data},
-                            "host": {
-                              10000: host_train_data
-                             }
-                            }
+pipeline.fit(backend=Backend.EGGROLL, work_mode=WorkMode.STANDALONE)
 
-                       })
-
-print (pipeline.get_component("dataio_0").get_model_param())
-print (pipeline.get_component("homo_data_split_0").get_summary())
-
+print(pipeline.get_component("dataio_0").get_model_param())
+print(pipeline.get_component("homo_data_split_0").get_summary())
