183230373348577e6d913ae8b18851a5d9eb9886
mgqa34 <mgq3374541@163.com>
2020-08-10 16:15:51 +0800

    update predict logit
    Signed-off-by: mgqa34 <mgq3374541@163.com>

--- pipeline/backend/pipeline.py
+++ pipeline/backend/pipeline.py
@@ -26,7 +26,6 @@ from pipeline.backend.config import StatusCode
 from pipeline.backend.config import VERSION
 from pipeline.backend.task_info import TaskInfo
 from pipeline.component.component_base import Component
-from pipeline.component.input import Input
 from pipeline.interface.data import Data
 from pipeline.interface.model import Model
 from pipeline.utils import tools
@@ -53,21 +52,29 @@ class PipeLine(object):
         self._train_components = {}
         self._stage = "fit"
         self._data_to_feed_in_prediction = None
+        self._predict_pipeline = []
+        self._deploy = False
 
     def set_initiator(self, role, party_id):
         self._initiator = SimpleNamespace(role=role, party_id=party_id)
 
         return self
 
+    def restore_roles(self, initiator, roles):
+        self._initiator = initiator
+        self._roles = roles
+
     def get_predict_meta(self):
-        if not self._fit_status:
+        if self._fit_status != StatusCode.SUCCESS:
             raise ValueError("To get predict meta, please fit successfully")
 
         return {"predict_dsl": self._predict_dsl,
                 "train_conf": self._train_conf,
                 "initiator": self._initiator,
+                "roles": self._roles,
                 "model_info": self._model_info,
-                "train_components": self._components
+                "components": self._components,
+                "stage": self._stage
                 }
 
     def _get_initiator_conf(self):
@@ -115,32 +122,24 @@ class PipeLine(object):
 
         return self._roles[role].index(party_id)
 
-    def set_deploy_end_component(self, components):
-        if not isinstance(components, list):
-            components = [components]
-
-        for idx in range(len(components)):
-            if isinstance(components[idx], str):
-                if components[idx] not in self._components:
-                    raise ValueError("component name {} does not exist".format(components[idx]))
-            elif isinstance(components[idx], Component):
-                component_name = components[idx].name
-                if component_name not in self._components:
-                    raise ValueError("component name {} does not exist".format(component_name))
-
-                components[idx] = component_name
-            else:
-                raise ValueError("input parameter should be component name or component object")
-
-        return self
-
     def add_component(self, component, data=None, model=None):
         if isinstance(component, PipeLine):
-            if self.model:
+            if component.is_deploy() is False:
+                raise ValueError("To use a training pipeline object as predict component, should deploy model first")
+
+            if model:
                 raise ValueError("pipeline should not have model as input!")
 
-            pipeline_components = component.get_all_components()
-            pipeline_train_dsl = component.get_train_dsl()
+            if not data:
+                raise ValueError("To use pipeline as a component, please set data input")
+
+            self._stage = "predict"
+            self._predict_pipeline.append({"pipeline": component, "data": data.predict_input})
+
+            meta = component.get_predict_meta()
+            self.restore_roles(meta.get("initiator"), meta.get("roles"))
+
+            return
 
         if not isinstance(component, Component):
             raise ValueError(
@@ -164,11 +163,7 @@ class PipeLine(object):
                 if val is None:
                     continue
 
-                print("data dep ", attr, val)
-                print("add dep ", component.name, attr)
                 data_key = attr.strip("_")
-                # if data_key == "validate_data" or data_key == "test_data":
-                #     data_key = "eval_data"
 
                 if isinstance(val, list):
                     self._components_input[component.name]["data"][data_key] = val
@@ -300,87 +295,62 @@ class PipeLine(object):
     def compile(self):
         self._construct_train_dsl()
         self._train_conf = self._construct_train_conf()
+        if self._stage == "predict":
+            predict_pipeline = self._predict_pipeline[0]["pipeline"]
+            data_info = self._predict_pipeline[0]["data"]
 
-    def _feed_input_dsl(self, predict_dsl, feed_dict):
-        for input_placeholder, reader in feed_dict.items():
-            component_dsl = {"module": reader.module,
-                             "output": {"data": reader.output.data_output}}
-            predict_dsl["components"][input_placeholder] = component_dsl
+            meta = predict_pipeline.get_predict_meta()
+            if meta["stage"] == "predict":
+                raise ValueError(
+                    "adding predict pipeline objects'stage is predict, a predict pipeline cannot be an input component")
 
-    def _feed_input_data(self, submit_conf, feed_dict, job_type=None):
-        data_source = {}
+            self._model_info = meta["model_info"]
+            predict_pipeline_dsl = meta["predict_dsl"]
+            predict_pipeline_conf = meta["train_conf"]
+            if not predict_pipeline_dsl:
+                raise ValueError(
+                    "Cannot find deploy model in predict pipeline, to use a pipeline as input component, "
+                    "it should be deploy first")
 
-        # {"reader_0": Reader}
-        if isinstance(list(feed_dict.keys())[0], str):
-            for input_placeholder, reader in feed_dict.items():
-                reader.reset_name(input_placeholder)
-                param_conf = reader.get_config(version=VERSION, roles=self._roles)
+            for cpn in self._train_dsl["components"]:
+                if cpn in predict_pipeline_dsl["components"]:
+                    raise ValueError(
+                        "component name {} exist in predict pipeline's deploy component, this is not support")
 
-                if "algorithm_parameters" in param_conf:
-                    algorithm_param_conf = param_conf["algorithm_parameters"]
-                    if "algorithm_parameters" not in self._train_conf:
-                        self._train_conf["algorithm_parameters"] = {}
-                    self._train_conf["algorithm_parameters"].update(algorithm_param_conf)
+            if "algorithm_parameters" in predict_pipeline_conf:
+                algo_param = predict_pipeline_conf["algorithm_parameters"]
+                if "algorithm_parameters" in self._train_conf:
+                    for key, value in algo_param.items():
+                        if key not in self._train_conf["algorithm_parameters"]:
+                            self._train_conf["algorithm_parameters"][key] = value
+                else:
+                    self._train_conf["algorithm_parameters"] = algo_param
 
-                if "role_parameters" in param_conf:
-                    role_param_conf = param_conf["role_parameters"]
-                    self._train_conf["role_parameters"] = tools.merge_dict(role_param_conf,
-                                                                           self._train_conf["role_parameters"])
+            if "role_parameters" in predict_pipeline_conf:
+                role_param = predict_pipeline_conf["role_parameters"]
+                for cpn in self._train_dsl["components"]:
+                    for role, param in role_param.items():
+                        for idx in param:
+                            if param[idx].get(cpn) is not None:
+                                del predict_pipeline_conf["role_parameters"][role][idx][cpn]
 
-            return submit_conf
+                if "role_parameters" not in self._train_conf:
+                    self._train_conf["role_parameters"] = {}
 
-        # {Input: {} }
-        for _input, _input_dict in feed_dict.items():
-            if not isinstance(_input, Input):
-                raise ValueError("key of feed_dict should an input object of the name of an input object")
+                self._train_conf["role_parameters"].update(predict_pipeline_conf["role_parameters"])
 
-            for role in _input_dict:
-                if role not in data_source:
-                    data_source[role] = {}
+            self._predict_dsl = tools.merge_dict(predict_pipeline_dsl, self._train_dsl)
 
-                if VERSION == 1:
-                    if "args" not in data_source[role]:
-                        data_source[role]["args"] = {"data": {}}
+            for data_field, val in data_info.items():
+                cpn = data_field.split(".", -1)[0]
+                dataset = data_field.split(".", -1)[1]
+                if not isinstance(val, list):
+                    val = [val]
 
-                    all_party = self._get_role_conf()[role]
-                    data = [{}] * len(all_party)
-                    for idx in range(len(all_party)):
-                        _party_id = all_party[idx]
-                        if _party_id not in _input_dict[role]:
-                            raise ValueError(
-                                "In Pipeline, to use dsl version-1, all party's data in role should be list, but role: {}, party: {} not found".format(
-                                    role, _party_id))
-                        data[idx] = _input_dict[role][_party_id]
+                self._predict_dsl["components"][cpn][dataset] = val
 
-                    if job_type != "predict":
-                        data_source[role]["args"]["data"][_input.name] = data
-                    else:
-                        data_source[role]["args"]["data"]["eval_data"] = data
-
-                    continue
-
-                for _party_id in _input_dict[role]:
-                    _party_index = str(self._get_party_index(role, _party_id))
-                    if _party_index not in data_source[role]:
-                        data_source[role][_party_index] = {}
-                        data_source[role][_party_index]["args"] = {}
-
-                    data_source[role][_party_index]["args"][_input.name] = _input_dict[role][_party_id]
-
-        if "role_parameters" not in submit_conf:
-            submit_conf["role_parameters"] = data_source
-        else:
-            all_roles = set(submit_conf["role_parameters"].keys()) | set(data_source.keys())
-            for role in all_roles:
-                if role not in submit_conf["role_parameters"]:
-                    submit_conf["role_parameters"][role] = data_source[role]
-                elif role in data_source:
-                    submit_conf["role_parameters"][role] = tools.merge_dict(data_source[role],
-                                                                            submit_conf["role_parameters"][role])
-
-        return submit_conf
-
-    def _feed_data_and_job_parameters(self, conf, feed_dict, backend, work_mode, job_type=None, model_info=None):
+    @staticmethod
+    def _feed_job_parameters(conf, backend, work_mode, job_type=None, model_info=None):
         submit_conf = copy.deepcopy(conf)
         print("submit conf' type {}".format(submit_conf))
         submit_conf["job_parameters"] = {
@@ -396,22 +366,15 @@ class PipeLine(object):
             submit_conf["job_parameters"]["model_id"] = model_info.model_id
             submit_conf["job_parameters"]["model_version"] = model_info.model_version
 
-        if not isinstance(feed_dict, dict):
-            return submit_conf
-
-        submit_conf = self._feed_input_data(submit_conf, feed_dict, job_type)
-
-        import pprint
-        pprint.pprint(submit_conf)
         return submit_conf
 
-    def fit(self, backend=Backend.EGGROLL, work_mode=WorkMode.STANDALONE, feed_dict=None):
+    def fit(self, backend=Backend.EGGROLL, work_mode=WorkMode.STANDALONE):
         if self._stage == "predict":
             raise ValueError("The pipeline is construct for predicting, can not use fit interface")
 
         print("_train_conf {}".format(self._train_conf))
         self._set_state("fit")
-        training_conf = self._feed_data_and_job_parameters(self._train_conf, feed_dict, backend, work_mode)
+        training_conf = self._feed_job_parameters(self._train_conf, backend, work_mode)
         pprint.pprint(training_conf)
         self._train_job_id, detail_info = self._job_invoker.submit_job(self._train_dsl, training_conf)
         self._train_board_url = detail_info["board_url"]
@@ -422,23 +385,20 @@ class PipeLine(object):
                                                                 self._initiator.role,
                                                                 self._initiator.party_id)
 
-    def predict(self, backend=Backend.EGGROLL, work_mode=WorkMode.CLUSTER, feed_dict=None):
-        if self._fit_status != StatusCode.SUCCESS:
-            print("Pipeline should be fit successfully before predict!!!")
-            return
+    def predict(self, backend=Backend.EGGROLL, work_mode=WorkMode.CLUSTER):
+        if self._stage != "predict":
+            raise ValueError(
+                "To use predict function, please use a new predict pipeline "
+                "and add data reader component and training pipeline to make a predict pipeline")
 
-        if self._data_to_feed_in_prediction:
-            feed_dict = self._data_to_feed_in_prediction
+        self.compile()
 
-        predict_conf = self._feed_data_and_job_parameters(self._train_conf,
-                                                          feed_dict,
-                                                          backend,
-                                                          work_mode,
-                                                          job_type="predict",
-                                                          model_info=self._model_info)
+        predict_conf = self._feed_job_parameters(self._train_conf,
+                                                 backend,
+                                                 work_mode,
+                                                 job_type="predict",
+                                                 model_info=self._model_info)
         predict_dsl = copy.deepcopy(self._predict_dsl)
-        if self._data_to_feed_in_prediction:
-            self._feed_input_dsl(predict_dsl, feed_dict)
 
         self._predict_job_id, _ = self._job_invoker.submit_job(dsl=predict_dsl, submit_conf=predict_conf)
         self._job_invoker.monitor_job_status(self._predict_job_id,
@@ -498,7 +458,13 @@ class PipeLine(object):
         self._predict_dsl = self._job_invoker.get_predict_dsl(train_dsl=self._train_dsl, cpn_list=deploy_cpns,
                                                               version=VERSION)
 
+        if self._predict_dsl:
+            self._deploy = True
+
         return self
+
+    def is_deploy(self):
+        return self._deploy
 
     def init_predict_config(self, config):
         if isinstance(config, PipeLine):
@@ -512,9 +478,6 @@ class PipeLine(object):
         self._train_components = config["train_components"]
 
     def get_component_input_msg(self):
-        # if self._stage != "predict":
-        #     raise ValueError("In fitting stage, reader should be add as a component")
-
         if VERSION != 2:
             raise ValueError("In DSL Version 1，only need to config data from args, no need special component")
 
