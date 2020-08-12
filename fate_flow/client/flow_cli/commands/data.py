1259f1c9eec950df7df4cfe20ca472b208cbeaaf
ryan.zhang <izhfeng@163.com>
2020-08-12 15:35:18 +0800

    fix cli_utils check_abs_path method

--- fate_flow/client/flow_cli/commands/data.py
+++ fate_flow/client/flow_cli/commands/data.py
@@ -17,7 +17,7 @@ import click
 import os
 import sys
 from fate_flow.client.flow_cli.utils import cli_args
-from fate_flow.client.flow_cli.utils.cli_utils import preprocess, access_server, check_abs_path
+from fate_flow.client.flow_cli.utils.cli_utils import preprocess, access_server, check_abs_path, prettify
 from requests_toolbelt import MultipartEncoder, MultipartEncoderMonitor
 
 
@@ -56,7 +56,7 @@ def upload(ctx, **kwargs):
     kwargs['verbose'] = int(kwargs['verbose'])
     config_data, dsl_data = preprocess(**kwargs)
     if config_data.get('use_local_data', 1):
-        file_name = check_abs_path(config_data.get('file'))
+        file_name = check_abs_path(config_data.get('file'), False)
         if os.path.exists(file_name):
             with open(file_name, 'rb') as fp:
                 data = MultipartEncoder(
@@ -77,8 +77,13 @@ def upload(ctx, **kwargs):
                 access_server('post', ctx, 'data/upload', json=None, data=data,
                               params=config_data, headers={'Content-Type': data.content_type})
         else:
-            raise Exception('The file is obtained from the fate flow client machine, but it does not exist, '
-                            'please check the path: {}'.format(file_name))
+            prettify(
+                {
+                    "retcode": 100,
+                    "retmsg": "The file is obtained from the fate flow client machine, but it does not exist, "
+                              "please check the path: {}".format(file_name)
+                }
+            )
     else:
         access_server('post', ctx, 'data/upload', config_data)
 
