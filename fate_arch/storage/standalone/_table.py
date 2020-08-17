dc5058baa2c8a5a0bae97c2d3ca126526ada6d9f
weiwee <wbwmat@gmail.com>
2020-08-17 20:21:11 +0800

    fix standalone
    Signed-off-by: weiwee <wbwmat@gmail.com>

--- fate_arch/storage/standalone/_table.py
+++ fate_arch/storage/standalone/_table.py
@@ -14,42 +14,39 @@
 #  limitations under the License.
 #
 from typing import Iterable
+
+from fate_arch.standalone import Session
 from fate_arch.storage import StorageEngine, StandaloneStorageType
 from fate_arch.storage import StorageTableBase
 
 
 class StorageTable(StorageTableBase):
     def __init__(self,
-                 context,
+                 session: Session,
                  address=None,
                  name: str = None,
                  namespace: str = None,
                  partitions: int = 1,
                  storage_type: StandaloneStorageType = None,
                  options=None):
-        self._context = context
+        super(StorageTable, self).__init__(name=name, namespace=namespace)
+        self._session = session
         self._address = address
         self._name = name
         self._namespace = namespace
         self._partitions = partitions
-        self._storage_type = storage_type
+        self._type = storage_type
         self._options = options if options else {}
         self._storage_engine = StorageEngine.STANDALONE
+        need_cleanup = self._type == StandaloneStorageType.ROLLPAIR_IN_MEMORY
+        self._table = self._session.create_table(namespace=self._namespace, name=self._name, partitions=partitions,
+                                                 need_cleanup=need_cleanup, error_if_exist=False)
 
-    def count(self):
-        return self._table.count()
+    def get_name(self):
+        return self._table.name
 
-    def collect(self, **kwargs):
-        return self._table.collect(**kwargs)
-
-    def close(self):
-        return self._session.stop()
-
-    def save_as(self, name, namespace, partitions=None, schema=None, **kwargs):
-        return self._table.save_as(name=name, namespace=namespace, partitions=partitions, need_cleanup=False)
-
-    def put_all(self, kv_list: Iterable, **kwargs):
-        return self._table.put_all(kv_list)
+    def get_namespace(self):
+        return self._table.namespace
 
     def get_address(self):
         return self._address
@@ -57,11 +54,26 @@ class StorageTable(StorageTableBase):
     def get_engine(self):
         return self._storage_engine
 
+    def get_type(self):
+        return self._type
+
     def get_partitions(self):
         return self._table.partitions
 
-    def get_name(self):
-        return self._table.name
+    def get_options(self):
+        return self._options
 
-    def get_namespace(self):
-        return self._table.namespace
+    def put_all(self, kv_list: Iterable, **kwargs):
+        return self._table.put_all(kv_list)
+
+    def collect(self, **kwargs):
+        return self._table.collect(**kwargs)
+
+    def destroy(self):
+        super().destroy()
+        return self._table.destroy()
+
+    def count(self):
+        count = self._table.count()
+        self.get_meta().update_metas(count=count)
+        return count
