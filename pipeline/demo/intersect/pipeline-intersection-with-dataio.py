b8f35f3ba7d53739219bdd48bb0aeeb31f138370
zazd <zhenght5@gmail.com>
2020-08-17 10:27:59 +0800

    add intersection pipeline
    Signed-off-by: zazd <zhenght5@gmail.com>

---
+++ pipeline/demo/intersect/pipeline-intersection-with-dataio.py
@@ -0,0 +1,91 @@
+#
+#  Copyright 2019 The FATE Authors. All Rights Reserved.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+
+import argparse
+
+from pipeline.backend.pipeline import PipeLine
+from pipeline.component.dataio import DataIO
+from pipeline.component.intersection import Intersection
+from pipeline.component.reader import Reader
+from pipeline.demo.util.demo_util import Config
+from pipeline.interface.data import Data
+
+
+def main(config="./config.yaml"):
+    # obtain config
+    config = Config(config)
+    guest = config.guest
+    host = config.host[0]
+    backend = config.backend
+    work_mode = config.work_mode
+
+    # specify input data name & namespace in database
+    guest_train_data = {"name": "breast_hetero_guest", "namespace": "experiment"}
+    host_train_data = {"name": "breast_hetero_host", "namespace": "experiment"}
+
+    # initialize pipeline
+    pipeline = PipeLine()
+    # set job initiator
+    pipeline.set_initiator(role='guest', party_id=guest)
+    # set participants information
+    pipeline.set_roles(guest=guest, host=host)
+
+    # define Reader components to read in data
+    reader_0 = Reader(name="reader_0")
+    # configure Reader for guest
+    reader_0.get_party_instance(role='guest', party_id=guest).algorithm_param(table=guest_train_data)
+    # configure Reader for host
+    reader_0.get_party_instance(role='host', party_id=host).algorithm_param(table=host_train_data)
+
+    # define DataIO components
+    dataio_0 = DataIO(name="dataio_0")
+
+    # get DataIO party instance of guest
+    dataio_0_guest_party_instance = dataio_0.get_party_instance(role='guest', party_id=guest)
+    # configure DataIO for guest
+    dataio_0_guest_party_instance.algorithm_param(with_label=True, output_format="dense")
+    # get and configure DataIO party instance of host
+    dataio_0.get_party_instance(role='host', party_id=host).algorithm_param(with_label=False)
+
+    # define Intersection components
+    intersection_0 = Intersection(name="intersection_0")
+    intersection_0.get_party_instance(role="guest", party_id=guest).algorithm_param(intersect_method="rsa",
+                                                                                    sync_intersect_ids=True,
+                                                                                    only_output_key=True)
+
+    # add components to pipeline, in order of task execution
+    pipeline.add_component(reader_0)
+    pipeline.add_component(dataio_0, data=Data(data=reader_0.output.data))
+    # set data input sources of intersection components
+    pipeline.add_component(intersection_0, data=Data(data=dataio_0.output.data))
+    # compile pipeline once finished adding modules, this step will form conf and dsl files for running job
+    pipeline.compile()
+
+    # fit model
+    pipeline.fit(backend=backend, work_mode=work_mode)
+    # query component summary
+    print(pipeline.get_component("hetero_lr_0").get_summary())
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("PIPELINE DEMO")
+    parser.add_argument("-config", type=str,
+                        help="config file")
+    args = parser.parse_args()
+    if args.config is not None:
+        main(args.config)
+    else:
+        main()
