c8a347afbb14f9937203b9e510170dc08b29387e
mgqa34 <mgq3374541@163.com>
2020-08-29 13:10:33 +0800

    fix boosting_param not import logger error
    Signed-off-by: mgqa34 <mgq3374541@163.com>

--- federatedml/param/boosting_param.py
+++ federatedml/param/boosting_param.py
@@ -22,7 +22,7 @@ from federatedml.param.encrypt_param import EncryptParam
 from federatedml.param.encrypted_mode_calculation_param import EncryptedModeCalculatorParam
 from federatedml.param.cross_validation_param import CrossValidationParam
 from federatedml.param.predict_param import PredictParam
-from federatedml.util import consts
+from federatedml.util import consts, LOGGER
 import copy
 import collections
 
