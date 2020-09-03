ea7c24135da5eade3f85a4c891fabf220c5adea7
weijing <talkingwallace@sohu.com>
2020-09-03 14:54:57 +0800

    remove module list check
    Signed-off-by: chenweijing <talkingwallace@sohu.com>

--- federatedml/protobuf/model_migrate/converter_factory.py
+++ federatedml/protobuf/model_migrate/converter_factory.py
@@ -3,17 +3,7 @@ from federatedml.protobuf.model_migrate.converter.tree_model_converter import He
 import os
 
 
-def get_module_list():
-    file_list = os.listdir('../../conf/setting_conf')
-    module_list = [s.replace('.json', '') for s in file_list]
-    return module_list
-
-
 def converter_factory(module_name: str) -> ProtoConverterBase:
-
-    module_list = get_module_list()
-    if module_name not in module_list:
-        raise ValueError('module name "{}" is illegal'.format(module_name))
 
     if module_name == 'HeteroSecureBoost':
         return HeteroSBTConverter()
