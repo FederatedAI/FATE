f1d6933f267012252c7dde9c9a50bbc40557e0da
jarvenshan <shanjiarun@gmail.com>
2020-09-04 17:24:22 +0800

    fix several evalution for clustering bugs

--- federatedml/evaluation/metrics/clustering_metric.py
+++ federatedml/evaluation/metrics/clustering_metric.py
@@ -22,7 +22,7 @@ class JaccardSimilarityScore(object):
         return jaccard_similarity_score(labels, pred_scores)
 
 
-class fowlkes_mallows_score(object):
+class FowlkesMallowsScore(object):
     """
     Compute fowlkes_mallows_score, as in FMI
     """
@@ -40,17 +40,64 @@ class AdjustedRandScore(object):
         return adjusted_rand_score(labels, pred_scores)
 
 
+class contengincy_matrix(object):
+    """
+    Compute contengincy_matrix
+    """
+
+    def compute(self, labels, pred_scores):
+        total_count = len(labels)
+        label_predict = list(zip(labels, pred_scores))
+        axis_x = len(np.unique(pred_scores))
+        axis_y = len(np.unique(labels))
+        result_array = np.zeros([axis_y, axis_x])
+        for v1, v2 in label_predict:
+            result_array[v1][v2] += 1
+        return result_array
+
+
+class distance_measure(object):
+    """
+    Compute distance_measure
+    """
+
+    def compute(self, dist_table, inter_cluster_dist):
+        max_radius_result = []
+        for i in range(0, len(dist_table)):
+            dist_list = []
+            for v in dist_list:
+                if v[1][0] == i:
+                    dist_list.append(v[1][0])
+            max_radius_result.append(max(dist_list))
+        cluster_nearest_result = []
+        for j in range(0, len(dist_table)):
+            arr = inter_cluster_dist[j * len(dist_table), j * 2 * len(dist_table)]
+            for k in range(0, len(arr)):
+                smallest = np.inf
+                if arr[i] < smallest:
+                    smallest = arr[i]
+                    smallest_index = i
+                if smallest_index >= j:
+                    smallest_index += 1
+            cluster_nearest_result.append(smallest_index)
+        distance_measure_result = dict()
+        for n in range(0, len(dist_table)):
+            distance_measure_result[n] = [max_radius_result, cluster_nearest_result]
+        return distance_measure_result
+
+
 class Davies_Bouldin_index(object):
     """
         Compute dbi，as in dbi
     """
-    def compute(self,dist_table,cluster_dist):
-        max_dij_list=[]
-        for i in range(0,len(dist_table)):
-            dij_list=[]
-            for j in range (0,len(dist_table)):
-                if j!=i:
-                    dij_list.append((dist_table[i]+dist_table[j])/(cluster_dist[i+j] ** 0.5))
-            max_dij=max(dij_list)
+
+    def compute(self, dist_table, cluster_dist):
+        max_dij_list = []
+        for i in range(0, len(dist_table)):
+            dij_list = []
+            for j in range(0, len(dist_table)):
+                if j != i:
+                    dij_list.append((dist_table[i] + dist_table[j]) / (cluster_dist[i + j] ** 0.5))
+            max_dij = max(dij_list)
         max_dij_list.append(max_dij)
-        return np.sum(max_dij_list)/len(dist_table)
+        return np.sum(max_dij_list) / len(dist_table)
--- federatedml/unsupervise/kmeans/hetero_kmeans/hetero_kmeans_arbiter.py
+++ federatedml/unsupervise/kmeans/hetero_kmeans/hetero_kmeans_arbiter.py
@@ -67,7 +67,7 @@ class HeteroKmeansArbiter(BaseKmeansModel):
             dist_cluster_dtable = dist_sum.join(cluster_result, lambda v1, v2: [v1, v2])
             dist_table = self.cal_ave_dist(dist_cluster_dtable, cluster_result, self.k)  # ave dist in each cluster
             cluster_dist = self.cluster_dist_aggregator.sum_model(suffix=(self.n_iter_,))
-            # self.DBI=clustering_metric.Davies_Bouldin_index.compute(dist_table, cluster_dist)
+            self.DBI=clustering_metric.Davies_Bouldin_index.compute(self, dist_table, cluster_dist._weights)
 
             tol1 = self.transfer_variable.guest_tol.get(idx=0, suffix=(self.n_iter_,))
             tol2 = self.transfer_variable.host_tol.get(idx=0, suffix=(self.n_iter_,))
@@ -86,15 +86,17 @@ class HeteroKmeansArbiter(BaseKmeansModel):
         secure_dist_all_2 = self.transfer_variable.host_dist.get(idx=0, suffix='predict')
         dist_sum = secure_dist_all_1.join(secure_dist_all_2, lambda v1, v2: v1 + v2)
         cluster_result = dist_sum.mapValues(lambda v: np.argmin(v))
+        cluster_dist_result = dist_sum.mapValues(lambda v: np.argmin(v))
         self.transfer_variable.cluster_result.remote(cluster_result, role=consts.GUEST, idx=0, suffix='predict')
         self.transfer_variable.cluster_result.remote(cluster_result, role=consts.HOST, idx=0, suffix='predict')
 
-        dist_cluster_dtable = dist_sum.join(cluster_result, lambda v1, v2: [v2, v1])
+        dist_cluster_dtable = dist_sum.join(cluster_result, lambda v1, v2: [v1, v2])
         dist_table = self.cal_ave_dist(dist_cluster_dtable, cluster_result, self.k)  # ave dist in each cluster
         cluster_dist = self.cluster_dist_aggregator.sum_model(suffix='predict')
         result = []
         for v in dist_table:
             result.append(tuple([v[0], [v[1], v[2], cluster_dist._weights]]))
+        dist_cluster_dtable_out = cluster_result.join(cluster_dist_result, lambda v1, v2: [v2, v1])
         predict_result1 = session.parallelize(result, partition=dist_sum.partitions, include_key=True)
-        predict_result2 = dist_cluster_dtable
+        predict_result2 = dist_cluster_dtable_out
         return predict_result1, predict_result2
