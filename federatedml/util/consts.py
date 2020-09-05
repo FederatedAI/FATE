937e76733dc6e8dedc35b386b91fd357b39ffc07
nemirorox <yolandawu131@gmail.com>
2020-09-05 13:24:37 +0800

    column expand first commit
    Signed-off-by: nemirorox <yolandawu131@gmail.com>

--- federatedml/util/consts.py
+++ federatedml/util/consts.py
@@ -245,3 +245,6 @@ HOMO_SBT = 'homo_sbt'
 HETERO_SBT = 'hetero_sbt'
 HETERO_FAST_SBT_MIX = 'hetero_fast_sbt_mix'
 HETERO_FAST_SBT_LAYERED = 'hetero_fast_sbt_layered'
+
+# column_expand
+MANUAL = 'manual'
