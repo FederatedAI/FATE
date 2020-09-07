3e81d7865ccea1211872f3102f59336e9efd70a7
weiwee <wbwmat@gmail.com>
2020-09-07 20:01:28 +0800

    impl model_migrate for pearson
    Signed-off-by: weiwee <wbwmat@gmail.com>

--- federatedml/protobuf/model_migrate/converter_factory.py
+++ federatedml/protobuf/model_migrate/converter_factory.py
@@ -1,14 +1,14 @@
 from federatedml.protobuf.model_migrate.converter.converter_base import ProtoConverterBase
+from federatedml.protobuf.model_migrate.converter.pearson_model_converter import HeteroPearsonConverter
 from federatedml.protobuf.model_migrate.converter.tree_model_converter import HeteroSBTConverter
-import os
 
 
 def converter_factory(module_name: str) -> ProtoConverterBase:
 
     if module_name == 'HeteroSecureBoost':
         return HeteroSBTConverter()
-    elif module_name == 'HeteroFastSecureBoost':
-        return HeteroSBTConverter()
+    if module_name == "HeteroPearson":
+        return HeteroPearsonConverter()
     else:
-        return None
+        raise ValueError('this module has no converter')
 
