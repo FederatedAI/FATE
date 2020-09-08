6e60aa9ff6f5ce0fae3148f7cc51e3a4fc38ab09
tanmingchao <mingchaotan@outlook.com>
2020-09-08 19:18:55 +0800

    Add binning converter
    Signed-off-by: tanmingchao <mingchaotan@outlook.com>

--- federatedml/protobuf/model_migrate/converter_factory.py
+++ federatedml/protobuf/model_migrate/converter_factory.py
@@ -1,14 +1,18 @@
 from federatedml.protobuf.model_migrate.converter.converter_base import ProtoConverterBase
 from federatedml.protobuf.model_migrate.converter.pearson_model_converter import HeteroPearsonConverter
 from federatedml.protobuf.model_migrate.converter.tree_model_converter import HeteroSBTConverter
+from federatedml.protobuf.model_migrate.converter.binning_model_converter import FeatureBinningConverter
 
 
 def converter_factory(module_name: str) -> ProtoConverterBase:
 
     if module_name == 'HeteroSecureBoost':
         return HeteroSBTConverter()
+    elif module_name == 'HeteroFastSecureBoost':
+        return HeteroSBTConverter()
+    elif module_name == 'HeteroFeatureBinning':
+        return FeatureBinningConverter()
     if module_name == "HeteroPearson":
         return HeteroPearsonConverter()
     else:
-        raise ValueError('this module has no converter')
-
+        return None
