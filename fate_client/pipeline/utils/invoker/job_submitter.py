6e71aab7c90502cead0e6f93c846f95ba1c8388d
nemirorox <yolandawu131@gmail.com>
2020-09-09 17:35:07 +0800

    add version info
    Signed-off-by: nemirorox <yolandawu131@gmail.com>

--- fate_client/pipeline/utils/invoker/job_submitter.py
+++ fate_client/pipeline/utils/invoker/job_submitter.py
@@ -45,7 +45,7 @@ class JobFunc:
 
 class JobInvoker(object):
     def __init__(self):
-        self.client = FlowClient(ip=conf.FlowConfig.IP, port=conf.FlowConfig.PORT)
+        self.client = FlowClient(ip=conf.FlowConfig.IP, port=conf.FlowConfig.PORT, version=2)
 
     @classmethod
     def _run_cmd(cls, cmd, output_while_running=False):
