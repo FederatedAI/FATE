69f84bda8953a5d2e5c726e9a86e4faed5f060c9
weiwee <wbwmat@gmail.com>
2020-09-10 11:11:13 +0800

    rename mapPartitions2 to mapPartitions in pearson add preserves partitions parameter to mapPartitions
    Signed-off-by: weiwee <wbwmat@gmail.com>

--- fate_arch/abc/_computing.py
+++ fate_arch/abc/_computing.py
@@ -64,7 +64,7 @@ class CTableABC(metaclass=ABCMeta):
         ...
 
     @abc.abstractmethod
-    def mapPartitions(self, func):
+    def mapPartitions(self, func, use_previous_behavior=True, preserves_partitioning=False):
         ...
 
     @abc.abstractmethod
