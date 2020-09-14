0e1176c820e84571eb61a42eebc5f0b0e5d3f39e
weijing <talkingwallace@sohu.com>
2020-09-14 14:29:52 +0800

    fix empty table problem
    Signed-off-by: chenweijing <talkingwallace@sohu.com>

--- federatedml/ensemble/basic_algorithms/decision_tree/tree_core/feature_histogram.py
+++ federatedml/ensemble/basic_algorithms/decision_tree/tree_core/feature_histogram.py
@@ -26,7 +26,7 @@
 # =============================================================================
 import copy
 import functools
-import uuid
+import numpy as np
 from operator import add, sub
 from typing import List
 
@@ -156,7 +156,7 @@ class FeatureHistogram(object):
         pass
 
     @staticmethod
-    def guest_accumulate_histogram(histograms):
+    def tensor_histogram_cumsum(histograms):
 
         for i in range(1, len(histograms)):
             for j in range(len(histograms[i])):
@@ -164,15 +164,7 @@ class FeatureHistogram(object):
         return histograms
 
     @staticmethod
-    def host_accumulate_histogram_0(histograms):
-        new_hist = copy.deepcopy(histograms)
-        for i in range(1, len(new_hist)):
-            for j in range(len(new_hist[i])):
-                new_hist[i][j] += new_hist[i - 1][j]
-        return new_hist
-
-    @staticmethod
-    def host_accumulate_histogram_1(histograms):
+    def dtable_histogram_cumsum(histograms):
 
         new_hist = [[0, 0, 0] for i in range(len(histograms))]
         new_hist[0][0] = copy.deepcopy(histograms[0][0])
@@ -188,7 +180,7 @@ class FeatureHistogram(object):
     def host_accumulate_histogram_map_func(v):
 
         fid, histograms = v
-        new_value = (fid, FeatureHistogram.host_accumulate_histogram_0(histograms))
+        new_value = (fid, FeatureHistogram.dtable_histogram_cumsum(histograms))
         return new_value
 
     @staticmethod
@@ -207,14 +199,30 @@ class FeatureHistogram(object):
 
         agg_histogram = functools.partial(FeatureHistogram.aggregate_histogram, node_map=node_map)
         batch_histogram_intermediate_rs = data_bin.join(grad_and_hess, lambda data_inst, g_h: (data_inst, g_h))
-        histograms_table = batch_histogram_intermediate_rs.mapReducePartitions(batch_histogram_cal, agg_histogram)
 
-        if ret == "tensor":
-            feature_num = bin_split_points.shape[0]
-            rs = FeatureHistogram.recombine_histograms(histograms_table, node_map, feature_num)
-            return rs
+        if batch_histogram_intermediate_rs.count() == 0:
+
+            # if input sample number is 0, return empty histograms
+            node_histograms = FeatureHistogram.generate_histogram_template(node_map, bin_split_points, valid_features,
+                                                                           1 if use_missing else 0)
+            hist_list = FeatureHistogram.generate_histogram_key_value_list(node_histograms, node_map, bin_split_points)
+
+            if ret == 'tensor':
+                feature_num = bin_split_points.shape[0]
+                return FeatureHistogram.recombine_histograms(hist_list, node_map, feature_num)
+            else:
+                histograms_table = session.parallelize(hist_list, partition=data_bin.partitions, include_key=True)
+                return FeatureHistogram.construct_table(histograms_table)
+
         else:
-            return FeatureHistogram.construct_table(histograms_table)
+            histograms_table = batch_histogram_intermediate_rs.mapReducePartitions(batch_histogram_cal, agg_histogram)
+            if ret == "tensor":
+                feature_num = bin_split_points.shape[0]
+                histogram_list = list(histograms_table.collect())
+                rs = FeatureHistogram.recombine_histograms(histogram_list, node_map, feature_num)
+                return rs
+            else:
+                return FeatureHistogram.construct_table(histograms_table)
 
     @staticmethod
     def aggregate_histogram(fid_histogram1, fid_histogram2, node_map):
@@ -226,6 +234,44 @@ class FeatureHistogram(object):
                 histogram1[i][j] += histogram2[i][j]
 
         return fid_1, histogram1
+
+    @staticmethod
+    def generate_histogram_template(node_map: dict, bin_split_points: np.ndarray, valid_features: dict,
+                                    missing_bin):
+
+        # for every feature, generate histograms containers (initialized val are 0s)
+
+        node_num = len(node_map)
+        node_histograms = []
+        for k in range(node_num):
+            feature_histogram_template = []
+            for fid in range(bin_split_points.shape[0]):
+                # if is not valid features, skip generating
+                if valid_features is not None and valid_features[fid] is False:
+                    feature_histogram_template.append([])
+                    continue
+                else:
+                    # 0, 0, 0 -> grad, hess, sample number
+                    feature_histogram_template.append([[0, 0, 0]
+                                                       for j in
+                                                       range(bin_split_points[fid].shape[0] + 1 + missing_bin)])
+
+            node_histograms.append(feature_histogram_template)
+            # check feature num
+            assert len(feature_histogram_template) == bin_split_points.shape[0]
+
+        return node_histograms
+
+    @staticmethod
+    def generate_histogram_key_value_list(node_histograms, node_map, bin_split_points):
+
+        # generate key_value hist list for DTable parallelization
+        ret = []
+        for nid in range(len(node_map)):
+            for fid in range(bin_split_points.shape[0]):
+                # key: (nid, fid), value: (fid, hist)
+                ret.append(((nid, fid), (fid, node_histograms[nid][fid])))
+        return ret
 
     @staticmethod
     def batch_calculate_histogram(kv_iterator, bin_split_points=None,
@@ -261,21 +307,8 @@ class FeatureHistogram(object):
         zero_opt_node_sum = [[0 for i in range(3)]
                              for j in range(node_num)]
 
-        node_histograms = []
-        for k in range(node_num):
-            feature_histogram_template = []
-            for fid in range(bin_split_points.shape[0]):
-                if valid_features is not None and valid_features[fid] is False:
-                    feature_histogram_template.append([])
-                    continue
-                else:
-                    feature_histogram_template.append([[0 for i in range(3)]
-                                                       for j in
-                                                       range(bin_split_points[fid].shape[0] + 1 + missing_bin)])
-
-            node_histograms.append(feature_histogram_template)
-
-        assert len(feature_histogram_template) == bin_split_points.shape[0]
+        node_histograms = FeatureHistogram.generate_histogram_template(node_map, bin_split_points, valid_features,
+                                                                       missing_bin)
 
         for rid in range(data_record):
             nid = node_map.get(node_ids[rid])
@@ -313,21 +346,16 @@ class FeatureHistogram(object):
                         node_histograms[nid][fid][-1][1] += zero_opt_node_sum[nid][1] - zero_optim[nid][fid][1]
                         node_histograms[nid][fid][-1][2] += zero_opt_node_sum[nid][2] - zero_optim[nid][fid][2]
 
-        ret = []
-        for nid in range(node_num):
-            for fid in range(bin_split_points.shape[0]):
-                ret.append(((nid, fid), (fid, node_histograms[nid][fid])))
-
+        ret = FeatureHistogram.generate_histogram_key_value_list(node_histograms, node_map, bin_split_points)
         return ret
 
     @staticmethod
-    def recombine_histograms(histograms_table, node_map, feature_num):
+    def recombine_histograms(histograms_list: list, node_map, feature_num):
 
         histograms = [[[] for j in range(feature_num)] for k in range(len(node_map))]
-        histogram_list = list(histograms_table.collect())
-        for tuple_ in histogram_list:
+        for tuple_ in histograms_list:
             nid, fid = tuple_[0]
-            histograms[int(nid)][int(fid)] = FeatureHistogram.guest_accumulate_histogram(tuple_[1][1])
+            histograms[int(nid)][int(fid)] = FeatureHistogram.tensor_histogram_cumsum(tuple_[1][1])
         return histograms
 
     @staticmethod
