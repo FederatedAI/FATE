7a9b9802c7d0f2b5c120ce650c11682c37921d15
nemirorox <yolandawu131@gmail.com>
2020-09-19 18:14:15 +0800

    edit pipeline consts
    Signed-off-by: nemirorox <yolandawu131@gmail.com>

--- fate_client/pipeline/param/consts.py
+++ fate_client/pipeline/param/consts.py
@@ -32,6 +32,7 @@ RANDOM_PADS = "RandomPads"
 NONE = "None"
 AFFINE = 'Affine'
 ITERATIVEAFFINE = 'IterativeAffine'
+RANDOM_ITERATIVEAFFINE = 'Random_IterativeAffine'
 L1_PENALTY = 'L1'
 L2_PENALTY = 'L2'
 
