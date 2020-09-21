85f09e52f42288acbefd4390c5142187487a502b
weijing <talkingwallace@sohu.com>
2020-09-21 21:04:08 +0800

    recover
    Signed-off-by: chenweijing <talkingwallace@sohu.com>

--- python/federatedml/ensemble/boosting/hetero/hetero_secureboost_guest.py
+++ python/federatedml/ensemble/boosting/hetero/hetero_secureboost_guest.py
@@ -15,7 +15,7 @@ from federatedml.util import consts
 from federatedml.transfer_variable.transfer_class.hetero_secure_boosting_predict_transfer_variable import \
     HeteroSecureBoostTransferVariable
 from federatedml.util.io_check import assert_io_num_rows_equal
-from federatedml.util.fate_operator import generate_anonymous
+from federatedml.util.anonymous_generator import generate_anonymous
 
 
 
