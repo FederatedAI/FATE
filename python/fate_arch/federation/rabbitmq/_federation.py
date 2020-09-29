dfd2e8de347f1bc63f9a25f332ae049a609b4f91
dylanfan <289765648@qq.com>
2020-09-29 17:32:07 +0800

    fix rabbitmq/federation receive bugs

--- python/fate_arch/federation/rabbitmq/_federation.py
+++ python/fate_arch/federation/rabbitmq/_federation.py
@@ -278,9 +278,7 @@ def _receive(channel_info, name, tag):
     for method, properties, body in channel_info.consume():
         LOGGER.debug(f"[rabbitmq._receive]count: {count}, method: {method}, properties: {properties}.")
         if properties.message_id != name or properties.correlation_id != tag:
-            raise RuntimeError(f"rabbitmq got unexpected message, "
-                               f"require {name}.{tag}, got {properties.message_id}.{properties.correlation_id}")
-
+            continue
         # object
         if properties.content_type == 'text/plain':
             obj = p_loads(body)
