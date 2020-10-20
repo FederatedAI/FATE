c37a4e3ce680acb2e6ebf8cc9d1805cd4db201da
zengjice <jicezeng@gmail.com>
2020-10-20 18:17:52 +0800

    fix rabbitmq new conf bug

--- python/fate_arch/federation/rabbitmq/_federation.py
+++ python/fate_arch/federation/rabbitmq/_federation.py
@@ -187,8 +187,8 @@ class Federation(FederationABC):
         return names
 
     def _upstream_uri(self, party_id):
-        host = self._mq.route_table.get(str(party_id)).get("host")
-        port = self._mq.route_table.get(str(party_id)).get("port")
+        host = self._mq.route_table.get(int(party_id)).get("host")
+        port = self._mq.route_table.get(int(party_id)).get("port")
         upstream_uri = f"amqp://{self._mq.union_name}:{self._mq.policy_id}@{host}:{port}"
         return upstream_uri
 
