5256452bb0e05ddad7e193dac74c438ceb8ab960
weiwee <wbwmat@gmail.com>
2020-11-03 13:54:49 +0800

    fix mnist demo in dsl v2
    Signed-off-by: weiwee <wbwmat@gmail.com>

--- examples/dsl/v2/homo_nn/mnist_demo/build_mnist_testsuite.py
+++ examples/dsl/v2/homo_nn/mnist_demo/build_mnist_testsuite.py
@@ -10,14 +10,14 @@ def build_model():
 
     model = Sequential()
     model.add(Reshape((28, 28, 1)))
-    model.add(Conv2D(32, kernel_size=(3, 3), activation='relu'))
-    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(Conv2D(32, kernel_size=(3, 3), activation="relu"))
+    model.add(Conv2D(64, (3, 3), activation="relu"))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
     model.add(Flatten())
-    model.add(Dense(128, activation='relu'))
+    model.add(Dense(128, activation="relu"))
     model.add(Dropout(0.5))
-    model.add(Dense(10, activation='softmax'))
+    model.add(Dense(10, activation="softmax"))
     return model
 
 
@@ -36,58 +36,52 @@ def build_conf(nn_define):
     conf_dict = {
         "initiator": {"role": "guest", "party_id": 9999},
         "role": {"guest": [10000], "host": [10000], "arbiter": [10000]},
-        "job_parameters": {"work_mode": 1, "backend": 0, "dsl_version": 2},
-        "role_parameters": {
-            "host": {
-                "0": {
-                    "reader_0": {
-                        "table": {
-                            "name": "mnist_train",
-                            "namespace": "experiment"
+        "job_parameters": {"common": {"work_mode": 1, "backend": 0, "dsl_version": 2}},
+        "component_parameters": {
+            "role": {
+                "host": {
+                    "0": {
+                        "reader_0": {
+                            "table": {"name": "mnist_train", "namespace": "experiment"},
                         },
-                    },
-                    "dataio_0": {
-                        "with_label": True,
-                        "label_name": "label",
-                        "label_type": "int",
-                        "output_format": "dense"
+                        "dataio_0": {
+                            "with_label": True,
+                            "label_name": "label",
+                            "label_type": "int",
+                            "output_format": "dense",
+                        },
                     }
-                }
+                },
+                "guest": {
+                    "0": {
+                        "reader_0": {
+                            "table": {"name": "mnist_train", "namespace": "experiment"}
+                        },
+                        "dataio_0": {
+                            "with_label": True,
+                            "label_name": "label",
+                            "label_type": "int",
+                            "output_format": "dense",
+                        },
+                    }
+                },
             },
-            "guest": {
-                "0": {
-                    "reader_0": {
-                        "table": {
-                            "name": "mnist_train",
-                            "namespace": "experiment"
-                        }
-                    },
-                    "dataio_0": {
-                        "with_label": True,
-                        "label_name": "label",
-                        "label_type": "int",
-                        "output_format": "dense"
-                    }
-                }
-            }
+            "common": {
+                "dataio_0": {"with_label": True},
+                "homo_nn_0": {
+                    "encode_label": True,
+                    "max_iter": 3,
+                    "batch_size": 128,
+                    "early_stop": {"early_stop": "diff", "eps": 0.0001},
+                    "optimizer": {"learning_rate": 0.001, "optimizer": "Adam"},
+                    "loss": "categorical_crossentropy",
+                    "metrics": ["accuracy"],
+                    "nn_define": nn_define_dict,
+                    "config_type": "keras",
+                },
+                "evaluation_0": {"eval_type": "multi"},
+            },
         },
-        "algorithm_parameters": {
-            "dataio_0": {"with_label": True},
-            "homo_nn_0": {
-                "encode_label": True,
-                "max_iter": 3,
-                "batch_size": 128,
-                "early_stop": {"early_stop": "diff", "eps": 0.0001},
-                "optimizer": {"learning_rate": 0.001, "optimizer": "Adam"},
-                "loss": "categorical_crossentropy",
-                "metrics": ["accuracy"],
-                "nn_define": nn_define_dict,
-                "config_type": "keras"
-            },
-            "evaluation_0": {
-                "eval_type": "multi"
-            }
-        }
     }
     with open("mnist_conf.json", "w") as f:
         json.dump(conf_dict, f, indent=indent)
@@ -96,28 +90,22 @@ def build_conf(nn_define):
 def build_dsl():
     dsl_dict = {
         "components": {
-            "reader_0": {
-                "module": "Reader",
-                "output": {"data": ["data"]}
-            },
+            "reader_0": {"module": "Reader", "output": {"data": ["data"]}},
             "dataio_0": {
                 "module": "DataIO",
                 "input": {"data": {"data": ["reader_0.data"]}},
-                "output": {
-                    "data": ["data"],
-                    "model": ["model"]
-                }
+                "output": {"data": ["data"], "model": ["model"]},
             },
             "homo_nn_0": {
                 "module": "HomoNN",
                 "input": {"data": {"train_data": ["dataio_0.data"]}},
-                "output": {"data": ["data"], "model": ["model"]}
+                "output": {"data": ["data"], "model": ["model"]},
             },
             "evaluation_0": {
                 "module": "Evaluation",
                 "input": {"data": {"data": ["homo_nn_0.data"]}},
-                "output": {"data": ["data"]}
-            }
+                "output": {"data": ["data"]},
+            },
         }
     }
     with open("mnist_dsl.json", "w") as f:
@@ -134,30 +122,24 @@ def build_predict_conf():
             "dsl_version": 2,
             "job_type": "predict",
             "model_id": "arbiter-10000#guest-9999#host-10000#model",
-            "model_version": ""
+            "model_version": "",
         },
         "role_parameters": {
             "guest": {
                 "0": {
                     "reader_0": {
-                        "table": {
-                            "name": "mnist_test",
-                            "namespace": "experiment"
-                        }
+                        "table": {"name": "mnist_test", "namespace": "experiment"}
                     }
                 }
             },
             "host": {
                 "0": {
                     "reader_0": {
-                        "table": {
-                            "name": "mnist_test",
-                            "namespace": "experiment"
-                        }
+                        "table": {"name": "mnist_test", "namespace": "experiment"}
                     }
                 }
-            }
-        }
+            },
+        },
     }
     with open("mnist_predict_conf.json", "w") as f:
         json.dump(conf_dict, f, indent=indent)
@@ -166,25 +148,28 @@ def build_predict_conf():
 def build_predict_dsl():
     dsl_dict = {
         "components": {
-            "reader_0": {
-                "module": "Reader",
-                "output": {"data": ["data"]}
-            },
+            "reader_0": {"module": "Reader", "output": {"data": ["data"]}},
             "dataio_0": {
-                "input": {"data": {"data": ["reader_0.data"]}, "model": ["pipeline.dataio_0.model"]},
+                "input": {
+                    "data": {"data": ["reader_0.data"]},
+                    "model": ["pipeline.dataio_0.model"],
+                },
                 "module": "DataIO",
-                "output": {"data": ["data"]}
+                "output": {"data": ["data"]},
             },
             "homo_nn_0": {
-                "input": {"data": {"test_data": ["dataio_0.data"]}, "model": ["pipeline.homo_nn_0.model"]},
+                "input": {
+                    "data": {"test_data": ["dataio_0.data"]},
+                    "model": ["pipeline.homo_nn_0.model"],
+                },
                 "module": "HomoNN",
-                "output": {"data": ["data"]}
+                "output": {"data": ["data"]},
             },
             "evaluation_0": {
                 "input": {"data": {"data": ["homo_nn_0.data"]}},
                 "module": "Evaluation",
-                "output": {"data": ["data"]}
-            }
+                "output": {"data": ["data"]},
+            },
         }
     }
     with open("mnist_predict_dsl.json", "w") as f:
@@ -200,7 +185,7 @@ def build_testsuite():
                 "partition": 16,
                 "table_name": "mnist_test",
                 "namespace": "experiment",
-                "role": "guest_0"
+                "role": "guest_0",
             },
             {
                 "file": "examples/data/mnist_test.csv",
@@ -208,7 +193,7 @@ def build_testsuite():
                 "partition": 16,
                 "table_name": "mnist_test",
                 "namespace": "experiment",
-                "role": "host_0"
+                "role": "host_0",
             },
             {
                 "file": "examples/data/mnist_train.csv",
@@ -216,7 +201,7 @@ def build_testsuite():
                 "partition": 16,
                 "table_name": "mnist_train",
                 "namespace": "experiment",
-                "role": "guest_0"
+                "role": "guest_0",
             },
             {
                 "file": "examples/data/mnist_train.csv",
@@ -224,24 +209,21 @@ def build_testsuite():
                 "partition": 16,
                 "table_name": "mnist_train",
                 "namespace": "experiment",
-                "role": "host_0"
-            }
+                "role": "host_0",
+            },
         ],
         "tasks": {
-            "mnist_conv": {
-                "conf": "./mnist_conf.json",
-                "dsl": "./mnist_dsl.json"
-            },
+            "mnist_conv": {"conf": "./mnist_conf.json", "dsl": "./mnist_dsl.json"},
             "mnist_predict": {
                 "deps": "mnist_conv",
                 "conf": "./mnist_predict_conf.json",
-                "dsl": "./mnist_predict_dsl.json"
-            }
-        }
+                "dsl": "./mnist_predict_dsl.json",
+            },
+        },
     }
     with open("mnist_testsuite.json", "w") as f:
         json.dump(testsuite_dict, f, indent=indent)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
