836828fefe907aa5aa549f28d0b479c8af728621
李钰 <liyu@ebchinatech.com>
2020-11-27 17:15:21 +0800

    init sss(secret sharing sum)
    Fixes ISSUE #2068
    
    Changes:
    
    add param
    
    add transfer_variable
    
    add sss model
    
    Signed-off-by: 李钰 <liyu@ebchinatech.com>

---
+++ python/federatedml/transfer_variable/transfer_class/secret_sharing_sum_transfer_variable.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+#  Copyright 2019 The FATE Authors. All Rights Reserved.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+
+################################################################################
+#
+# AUTO GENERATED TRANSFER VARIABLE CLASS. DO NOT MODIFY
+#
+################################################################################
+
+from python.federatedml.transfer_variable.base_transfer_variable import BaseTransferVariables
+
+
+# noinspection PyAttributeOutsideInit
+class SecretSharingSumTransferVariables(BaseTransferVariables):
+    def __init__(self, flowid=0):
+        super().__init__(flowid)
+        self.guest_share_primes = self._create_variable(name='guest_share_primes', src=['guest'], dst=['host'])
+        self.guest_share_secret = self._create_variable(name='guest_share_secret', src=['guest'], dst=['host'])
+        self.host_share_to_guest = self._create_variable(name='host_share_to_guest', src=['host'], dst=['guest'])
+        self.host_share_to_host = self._create_variable(name='host_share_to_host', src=['host'], dst=['host'])
+        self.host_sum = self._create_variable(name='host_sum', src=['host'], dst=['guest'])
+        self.primes_share = self._create_variable(name='primes_share', src=['guest'], dst=['host'])
+        self.guest_commitments = self._create_variable(name='guest_commitments', src=['guest'], dst=['host'])
+        self.host_commitments = self._create_variable(name='host_commitments', src=['host'], dst=['guest', 'host'])
