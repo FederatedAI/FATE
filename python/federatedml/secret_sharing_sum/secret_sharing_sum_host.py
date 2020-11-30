c285d9869bae70089f3d71ffee757a4c71a008c0
李钰 <liyu@ebchinatech.com>
2020-11-30 19:37:37 +0800

    Update secret_sharing_sum_host.py
    Cancel overloading run() save_data()
    
    Signed-off-by: 李钰 <liyu@ebchinatech.com>

--- python/federatedml/secret_sharing_sum/secret_sharing_sum_host.py
+++ python/federatedml/secret_sharing_sum/secret_sharing_sum_host.py
@@ -17,15 +17,12 @@
 #  limitations under the License.
 #
 
-import numpy as np
 
-from python.fate_arch.session import computing_session
-from python.federatedml.util import LOGGER
-from python.federatedml.param.secure_sharing_sum_param import SecureSharingSumParam
-from python.federatedml.transfer_variable.transfer_class.secret_sharing_sum_transfer_variable import \
+from federatedml.util import LOGGER
+from federatedml.param.secure_sharing_sum_param import SecureSharingSumParam
+from federatedml.transfer_variable.transfer_class.secret_sharing_sum_transfer_variable import \
     SecretSharingSumTransferVariables
-from python.federatedml.util.param_extract import ParamExtract
-from python.federatedml.secret_sharing_sum.bash_secret_sharing_sum import BaseSecretSharingSum
+from federatedml.secret_sharing_sum.base_secret_sharing_sum import BaseSecretSharingSum
 
 
 class SecretSharingSumHost(BaseSecretSharingSum):
@@ -33,33 +30,23 @@ class SecretSharingSumHost(BaseSecretSharingSum):
         super(SecretSharingSumHost, self).__init__()
         self.transfer_inst = SecretSharingSumTransferVariables()
         self.model_param = SecureSharingSumParam()
-        self.data_output = None
-        self.model_output = None
         self.host_party_idlist = []
         self.local_partyid = -1
 
-    def _init_runtime_parameters(self, component_parameters):
-        param_extracter = ParamExtract()
-        param = param_extracter.parse_param_from_config(self.model_param, component_parameters)
-        self.role = self.component_properties.parse_component_param(component_parameters, param).role
-        self.local_partyid = self.component_properties.parse_component_param(component_parameters, param).local_partyid
-        self.host_party_idlist = self.component_properties.parse_component_param(component_parameters,
-                                                                                 param).host_party_idlist
-        self.share_amount = len(self.host_party_idlist)+1
-        self._init_model(param)
-        return param
-
-    def _init_model(self, model_param):
+    def _init_model(self, model_param: SecureSharingSumParam):
         self.need_verify = model_param.need_verify
         self.partition = model_param.partition
 
-    def _init_data(self, arg=None):
-        _, _, data = self.component_properties.extract_input_data(arg)
-        self.data_set = [(k, int(v)) for k, v in data['args'].get_all()]
-        self.x = computing_session.parallelize(self.data_set, include_key=True, partition=self.partition)
+    def _init_data(self, data_inst):
+        self.local_partyid = self.component_properties.local_partyid
+        self.host_party_idlist = self.component_properties.host_party_idlist
+        self.share_amount = len(self.host_party_idlist)+1
+        self.vss.set_share_amount(self.share_amount)
+        self.x = data_inst
 
     def recv_primes_from_guest(self):
-        self.prime = self.transfer_inst.guest_share_primes.get(idx=0)
+        prime = self.transfer_inst.guest_share_primes.get(idx=0)
+        self.vss.set_prime(prime)
 
     def sync_share_to_parties(self):
         for idx, party_id in enumerate(self.host_party_idlist):
@@ -67,38 +54,28 @@ class SecretSharingSumHost(BaseSecretSharingSum):
                 self.transfer_inst.host_share_to_host.remote(self.secret_sharing[idx],
                                                              role="host",
                                                              idx=idx)
-                self.transfer_inst.host_commitments.remote(self.commitments,
-                                                           role="host",
-                                                           idx=idx)
             else:
                 self.x_plus_y = self.secret_sharing[idx]
                 self.transfer_inst.host_share_to_guest.remote(self.secret_sharing[-1],
                                                               role="guest",
                                                               idx=0)
-                self.transfer_inst.host_commitments.remote(self.commitments,
-                                                           role="guest",
-                                                           idx=idx)
 
     def recv_share_from_parties(self):
         for idx, party_id in enumerate(self.host_party_idlist):
             if self.local_partyid != party_id:
                 self.y_recv.append(self.transfer_inst.host_share_to_host.get(idx=idx))
-                self.commitments_recv.append(self.transfer_inst.host_commitments.get(idx=idx))
             else:
                 self.y_recv.append(self.transfer_inst.guest_share_secret.get(idx=0))
-                self.commitments_recv.append(self.transfer_inst.guest_commitments.get(idx=idx))
 
     def sync_host_sum_to_guest(self):
         self.transfer_inst.host_sum.remote(self.x_plus_y,
                                            role="guest",
                                            idx=-1)
 
-    def run(self, component_parameters=None, args=None):
-        LOGGER.info("begin to init parameters of multi-party privacy summation host")
-        self._init_runtime_parameters(component_parameters)
+    def fit(self, data_inst):
 
         LOGGER.info("begin to make host data")
-        self._init_data(args)
+        self._init_data(data_inst)
 
         LOGGER.info("get primes from host")
         self.recv_primes_from_guest()
