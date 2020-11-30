3739889c059cb4e7ef4d6cc0120d6726c70bfed4
李钰 <liyu@ebchinatech.com>
2020-11-30 19:23:46 +0800

    Update secure_sharing_sum_param.py
    fix python path
    
    Signed-off-by: 李钰 <liyu@ebchinatech.com>

--- python/federatedml/param/secure_sharing_sum_param.py
+++ python/federatedml/param/secure_sharing_sum_param.py
@@ -16,7 +16,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 #
-from python.federatedml.param.base_param import BaseParam
+from federatedml.param.base_param import BaseParam
 
 
 class SecureSharingSumParam(BaseParam):
