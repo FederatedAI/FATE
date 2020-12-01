06a9f45f6c5d757ab4538231c4cf77b8ab947ea9
v_wbymxu <g6xuyiming@163.com>
2020-12-01 11:13:50 +0800

    update standalone deploy script
    Signed-off-by: v_wbymxu <g6xuyiming@163.com>

--- standalone-deploy/docker/fate/docker-entrypoint.sh
+++ standalone-deploy/docker/fate/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 cd /fate/python/fate_flow
-python fate_flow_server.py
+nohup python fate_flow_server.py &
 
 cd /fate/fateboard
 ${JAVA_PATH}/java -Dspring.config.location=/fate/fateboard/conf/application.properties -Dssh_config_file=/fate/fateboard/ssh/ -DFATE_DEPLOY_PREFIX=/fate/logs/ -Xmx2048m -Xms2048m -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:gc.log -XX:+HeapDumpOnOutOfMemoryError -jar fateboard.jar
