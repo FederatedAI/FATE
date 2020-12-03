5f29f597412d4991d3f35e04b6818a015d52e88e
ryan.zhang <izhfeng@163.com>
2020-12-03 17:01:37 +0800

    add model deploy related methods for flow_sdk, update docs

--- python/fate_client/flow_sdk/client/api/model.py
+++ python/fate_client/flow_sdk/client/api/model.py
@@ -111,3 +111,18 @@ class Model(BaseFlowAPI):
         config_data, dsl_data = preprocess(**kwargs)
         return self._post(url='model/model_tag/retrieve', json=config_data)
 
+    def deploy(self, model_id, model_version, cpn_list=None, predict_dsl=None):
+        kwargs = locals()
+        config_data, dsl_data = preprocess(**kwargs)
+        return self._post(url='model/deploy', json=config_data)
+
+    def get_predict_dsl(self, model_id, model_version):
+        kwargs = locals()
+        config_data, dsl_data = preprocess(**kwargs)
+        return self._post(url='model/get/predict/dsl', json=config_data)
+
+    def get_predict_conf(self, model_id, model_version):
+        kwargs = locals()
+        config_data, dsl_data = preprocess(**kwargs)
+        return self._post(url='model/get/predict/conf', json=config_data)
+
