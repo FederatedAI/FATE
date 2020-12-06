28b9822a834e2cc5a5fbf9bbc7cc5ed011007702
李钰 <liyu@ebchinatech.com>
2020-12-06 22:22:20 +0800

    Update vss.py
    Signed-off-by: 李钰 <liyu@ebchinatech.com>

--- python/federatedml/secureprotol/secret_sharing/vss.py
+++ python/federatedml/secureprotol/secret_sharing/vss.py
@@ -1,6 +1,5 @@
 import random
 from federatedml.secureprotol import gmpy_math
-import functools
 
 
 class Vss(object):
@@ -37,48 +36,35 @@ class Vss(object):
     def decrypt(self, x_values, y_values):
         k = len(x_values)
         assert k == len(set(x_values)), 'x_values points must be distinct'
-        nums = []
-        dens = []
+        secret = 0
         for i in range(k):
-            others = list(x_values)
-            cur = others.pop(i)
-            nums.append(functools.reduce(lambda a, b: a * b, [0 - o for o in others], 1))
-            dens.append(functools.reduce(lambda a, b: a * b, [cur - o for o in others], 1))
-        den = functools.reduce(lambda a, b: a * b, dens, 1)
-
-        num = []
-        for i in range(k):
-            term = nums[i] * den * y_values[i] % self.prime
-            inv = self.extended_gcd(dens[i], self.prime)
-            num.append(term * inv)
-
-        secret = ((sum(num) * self.extended_gcd(den, self.prime)) + self.prime) % self.prime
+            numerator, denominator = 1, 1
+            for j in range(k):
+                if i == j:
+                    continue
+                # compute a fraction & update the existing numerator + denominator
+                numerator = (numerator * (0 - x_values[j])) % self.prime
+                denominator = (denominator * (x_values[i] - x_values[j])) % self.prime
+            # get the polynomial from the numerator + denominator mod inverse
+            lagrange_polynomial = numerator * gmpy_math.invert(denominator, self.prime)
+            # multiply the current y & the evaluated polynomial & add it to f(x)
+            secret = (self.prime + secret + (y_values[i] * lagrange_polynomial)) % self.prime
 
         return secret
 
-    def calculate_commitments(self, coefficient):
-        return gmpy_math.powmod(self.g, coefficient, self.prime)
+    def egcd(self, a, b):
+        if a == 0:
+            res = (b, 0, 1)
+        else:
+            g, y, x = self.egcd(b % a, a)
+            res = (g, x - (b // a) * y, y)
+        return res
 
-    def verify(self, xy, commitments):
-        x, y = xy[0], xy[1]
-        v1 = gmpy_math.powmod(self.g, y, self.prime)
-        v2 = 1
-        for i in range(len(commitments)):
-            v2 *= gmpy_math.powmod(commitments[i], (x**i), self.prime)
-            v2 = v2 % self.prime
-        return v1 == v2
-
-    @staticmethod
-    def extended_gcd(a: int, b: int):
-        if not isinstance(a, int) or not isinstance(b, int):
-            raise TypeError('a and b must be integers')
-        x = 0
-        last_x = 1
-        y = 1
-        last_y = 0
-        while b != 0:
-            quot = a // b
-            a, b = b, a % b
-            x, last_x = last_x - quot * x, x
-            y, last_y = last_y - quot * y, y
-        return last_x
+    def mod_inverse(self, k):
+        k = k % self.prime
+        if k < 0:
+            r = self.egcd(self.prime, -k)[2]
+        else:
+            r = self.egcd(self.prime, k)[2]
+        res = (self.prime + r) % self.prime
+        return res
