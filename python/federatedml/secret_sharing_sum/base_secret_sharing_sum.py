51e7cf0a00d0417d9ce78847a2a7a924bc95c9c8
李钰 <liyu@ebchinatech.com>
2020-12-14 16:04:01 +0800

    Update base_secret_sharing_sum.py
    1. fix module paramter naming error
    2. add error message
    3. use join for joining tables of the same id
    
    Signed-off-by: 李钰 <liyu@ebchinatech.com>

--- python/federatedml/secret_sharing_sum/base_secret_sharing_sum.py
+++ python/federatedml/secret_sharing_sum/base_secret_sharing_sum.py
@@ -18,8 +18,8 @@
 import numpy as np
 
 from federatedml.model_base import ModelBase
-from federatedml.param.secure_sharing_sum_param import SecureSharingSumParam
-from federatedml.secureprotol.secret_sharing.vss import Vss
+from federatedml.param.secret_sharing_sum_param import SecretSharingSumParam
+from federatedml.secureprotol.secret_sharing.VerifiableSecretSharing import Vss
 
 
 class BaseSecretSharingSum(ModelBase):
@@ -27,7 +27,7 @@ class BaseSecretSharingSum(ModelBase):
         super(BaseSecretSharingSum, self).__init__()
         self.vss = Vss()
         self.host_count = None
-        self.model_param = SecureSharingSumParam()
+        self.model_param = SecretSharingSumParam()
         self.sum_cols = None
         self.x = None
         self.sub_key = []  # (x,f(x))
@@ -43,7 +43,7 @@ class BaseSecretSharingSum(ModelBase):
         sub_key_table = encrypt_result.mapValues(lambda x: x[0])
         self.commitments = encrypt_result.mapValues(lambda x: x[1])
         for i in range(self.host_count+1):
-            sub_key = sub_key_table.mapValues(lambda y: np.array(y)[:, i])
+            sub_key = sub_key_table.mapValues(lambda y: y[:, i])
             self.sub_key.append(sub_key)
 
     def generate_shares(self, values):
@@ -53,16 +53,16 @@ class BaseSecretSharingSum(ModelBase):
             sub_key, commitment = self.vss.encrypt(s)
             keys.append(sub_key)
             commitments.append(commitment)
-        res = (keys, commitments)
+        res = (np.array(keys), np.array(commitments))
         return res
 
     def sub_key_sum(self):
         for recv in self.y_recv:
-            self.x_plus_y = self.x_plus_y.union(recv, lambda x, y: np.column_stack((x[:, 0], np.add(x[:, 1], y[:, 1]))))
+            self.x_plus_y = self.x_plus_y.join(recv, lambda x, y: np.column_stack((x[:, 0], np.add(x[:, 1], y[:, 1]))))
 
     def reconstruct(self):
         for recv in self.host_sum_recv:
-            self.x_plus_y = self.x_plus_y.union(recv, lambda x, y: np.column_stack((x, y)))
+            self.x_plus_y = self.x_plus_y.join(recv, lambda x, y: np.column_stack((x, y)))
         self.secret_sum = self.x_plus_y.mapValues(self.decrypt)
 
     def decrypt(self, values):
@@ -73,16 +73,18 @@ class BaseSecretSharingSum(ModelBase):
             secret_sum.append(self.vss.decrypt(x_values, y_values))
         return secret_sum
 
-    def verify_sumkey(self, sum_key, commitment):
+    def verify_sumkey(self, sum_key, commitment, party_id):
         for recv in self.commitments_recv:
-            commitment = commitment.union(recv, lambda x, y: np.array(x) * np.array(y))
-        sum_key.union(commitment, lambda x, y: self.verify(x, y))
+            commitment = commitment.join(recv, lambda x, y: (x * y) % self.vss.prime)
+        sum_key.join(commitment, lambda x, y: self.verify(x, y, party_id, "sum_key"))
 
-    def verify_subkey(self, sub_key, commitment):
-        sub_key.union(commitment, lambda x, y: self.verify(x, y))
+    def verify_subkey(self, sub_key, commitment, party_id):
+        sub_key.join(commitment, lambda x, y: self.verify(x, y, party_id, "sub_key"))
 
-    def verify(self, sub_key, commitment):
-        for idx, key in enumerate(sub_key):
-            self.vss.verify(key, commitment[idx])
+    def verify(self, key, commitment, party_id, key_type):
+        for idx, key in enumerate(key):
+            res = self.vss.verify(key, commitment[idx])
+            if not res:
+                raise ValueError(f"Get wrong {key_type} from {party_id}")
         return True
 
