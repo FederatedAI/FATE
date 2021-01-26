76da992843fbc586b58623d73f08f82afce9bb74
ryan.zhang <izhfeng@163.com>
2021-01-26 15:57:55 +0800

    fix predict dsl generator

--- python/fate_flow/apps/job_app.py
+++ python/fate_flow/apps/job_app.py
@@ -211,7 +211,7 @@ def dsl_generator():
             cpn_str = cpn_str.replace(" ", "").replace("\n", "").strip(",[]")
             cpn_list = cpn_str.split(",")
         train_dsl = json_loads(data.get("train_dsl"))
-        parser = schedule_utils.get_dsl_parser_by_version(data.get("version", "1"))
+        parser = schedule_utils.get_dsl_parser_by_version(data.get("version", "2"))
         predict_dsl = parser.deploy_component(cpn_list, train_dsl)
 
         if data.get("filename"):
