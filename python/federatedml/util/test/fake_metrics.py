42c156e17fe24bcdf518dae8526e557cb2191cbc
weijingchen <talkingwallace@sohu.com>
2021-02-03 10:41:11 +0800

    add fake metrics for early stop testing
    Signed-off-by: chenweijing <talkingwallace@sohu.com>

---
+++ python/federatedml/util/test/fake_metrics.py
@@ -0,0 +1,37 @@
+# fake metrics are for testing early stopping function
+
+multi_fake = [
+    {'precision': 0.8, 'recall': 0.81, 'accuracy': 0.83},
+    {'precision': 0.81, 'recall': 0.82, 'accuracy': 0.82},
+    {'precision': 0.82, 'recall': 0.83, 'accuracy': 0.81},
+    {'precision': 0.83, 'recall': 0.84, 'accuracy': 0.84},
+    {'precision': 0.84, 'recall': 0.83, 'accuracy': 0.87},
+    {'precision': 0.85, 'recall': 0.82, 'accuracy': 0.86},
+    {'precision': 0.86, 'recall': 0.81, 'accuracy': 0.88},
+    {'precision': 0.87, 'recall': 0.89, 'accuracy': 0.81},
+    {'precision': 0.88, 'recall': 0.899, 'accuracy': 0.89},
+    {'precision': 0.89, 'recall': 0.91, 'accuracy': 0.90},
+    {'precision': 0.88, 'recall': 0.93, 'accuracy': 0.91},
+    {'precision': 0.87, 'recall': 0.94, 'accuracy': 0.92},
+    {'precision': 0.86, 'recall': 0.95, 'accuracy': 0.93},
+    {'precision': 0.85, 'recall': 0.96, 'accuracy': 0.94},
+    {'precision': 0.84, 'recall': 0.97, 'accuracy': 0.95},
+    {'precision': 0.83, 'recall': 0.98, 'accuracy': 0.96},
+    {'precision': 0.82, 'recall': 0.99, 'accuracy': 0.97},
+    {'precision': 0.81, 'recall': 0.999, 'accuracy': 0.98},
+    {'precision': 0.8, 'recall': 0.9999, 'accuracy': 0.99},
+    {'precision': 0.8, 'recall': 1.0, 'accuracy': 0.8},
+    {'precision': 0.8, 'recall': 1.0, 'accuracy': 0.8},
+]
+
+binary_fake = [
+    {'auc': 0.6, 'ks': 0.21},
+    {'auc': 0.7, 'ks': 0.23},
+    {'auc': 0.71, 'ks': 0.22},
+    {'auc': 0.78, 'ks': 0.22},
+    {'auc': 0.65, 'ks': 0.23},
+    {'auc': 0.64, 'ks': 0.21},
+    {'auc': 0.81, 'ks': 0.24},
+    {'auc': 0.74, 'ks': 0.25},
+    {'auc': 0.78, 'ks': 0.25}
+]
