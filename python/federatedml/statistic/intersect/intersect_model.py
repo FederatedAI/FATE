ef07711746f34fab9358e18da0e6ed08f5baa320
zazd <zhenght5@gmail.com>
2021-02-05 17:06:35 +0800

    modify order for repeated id process add sample id proc for 1.6.1

--- python/federatedml/statistic/intersect/intersect_model.py
+++ python/federatedml/statistic/intersect/intersect_model.py
@@ -13,7 +13,8 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 #
-
+from fate_arch.session import computing_session as session
+from fate_arch.common import versions
 from fate_flow.entity.metric import Metric, MetricMeta
 from federatedml.feature.instance import Instance
 from federatedml.model_base import ModelBase
@@ -23,7 +24,8 @@ from federatedml.statistic.intersect import RawIntersectionHost, RawIntersection
 from federatedml.statistic.intersect.repeat_id_process import RepeatedIDIntersect
 from federatedml.transfer_variable.transfer_class.intersection_func_transfer_variable import \
     IntersectionFuncTransferVariable
-from federatedml.util import consts, LOGGER
+from federatedml.util import consts
+from federatedml.util import LOGGER
 
 
 class IntersectModelBase(ModelBase):
@@ -45,13 +47,40 @@ class IntersectModelBase(ModelBase):
 
         self.transfer_variable = IntersectionFuncTransferVariable()
 
-    def init_intersect_method(self):
+    def __init_intersect_method(self):
         LOGGER.info("Using {} intersection, role is {}".format(self.model_param.intersect_method, self.role))
         self.host_party_id_list = self.component_properties.host_party_idlist
         self.guest_party_id = self.component_properties.guest_partyid
+        if self.role == consts.HOST:
+            self.host_party_id = self.component_properties.local_partyid
 
-        if self.role not in [consts.HOST, consts.GUEST]:
-            raise ValueError("role {} is not support".format(self.role))
+        if self.model_param.intersect_method == "rsa":
+            if self.role == consts.HOST:
+                self.intersection_obj = RsaIntersectionHost(self.model_param)
+                self.intersection_obj.host_party_id = self.host_party_id
+            elif self.role == consts.GUEST:
+                self.intersection_obj = RsaIntersectionGuest(self.model_param)
+            else:
+                raise ValueError("role {} is not support".format(self.role))
+
+            self.intersection_obj.guest_party_id = self.guest_party_id
+
+        elif self.model_param.intersect_method == "raw":
+            if self.role == consts.HOST:
+                self.intersection_obj = RawIntersectionHost(self.model_param)
+            elif self.role == consts.GUEST:
+                self.intersection_obj = RawIntersectionGuest(self.model_param)
+            else:
+                raise ValueError("role {} is not support".format(self.role))
+            self.intersection_obj.tracker = self.tracker
+            self.intersection_obj.task_version_id = self.task_version_id
+        else:
+            raise ValueError("intersect_method {} is not support yet".format(self.model_param.intersect_method))
+
+        if self.role == consts.HOST:
+            self.intersection_obj.host_party_id = self.host_party_id
+        self.intersection_obj.guest_party_id = self.guest_party_id
+        self.intersection_obj.host_party_id_list = self.host_party_id_list
 
     def get_model_summary(self):
         return {"intersect_num": self.intersect_num, "intersect_rate": self.intersect_rate}
@@ -97,7 +126,9 @@ class IntersectModelBase(ModelBase):
         return self.intersect_ids
 
     def fit(self, data):
-        self.init_intersect_method()
+        self.__init_intersect_method()
+
+        LOGGER.info("fate version:{}".format(versions.get_fate_version()))
 
         if self.model_param.repeated_id_process:
             if self.model_param.intersect_cache_param.use_cache is True and self.model_param.intersect_method == consts.RSA:
@@ -107,17 +138,24 @@ class IntersectModelBase(ModelBase):
                 raise ValueError("While multi-host, repeated_id_owner should be guest.")
 
             proc_obj = RepeatedIDIntersect(repeated_id_owner=self.model_param.repeated_id_owner, role=self.role)
-            data = proc_obj.map_id(data=data)
+            proc_obj.set_version(versions.get_fate_version())
+            data = proc_obj.recover(data=data)
+
+        self.intersect_ids = self.intersection_obj.run(data)
+
+        if self.model_param.repeated_id_process:
+            if not self.model_param.sync_intersect_ids:
+                self.intersect_ids = data
+
+            self.intersect_ids = proc_obj.expand(self.intersect_ids)
+            LOGGER.info("repeated_id process:{}".format(self.intersect_ids.count()))
 
         if self.model_param.allow_info_share:
             if self.model_param.intersect_method == consts.RSA and self.model_param.info_owner == consts.GUEST \
                     or self.model_param.intersect_method == consts.RAW and self.model_param.join_role == self.model_param.info_owner:
                 self.model_param.sync_intersect_ids = False
 
-        self.intersect_ids = self.intersection_obj.run_intersect(data)
-
-        if self.model_param.allow_info_share:
-            self.intersect_ids = self.__share_info(data)
+            self.intersect_ids = self.__share_info(self.intersect_ids)
 
         LOGGER.info("Finish intersection")
 
@@ -150,45 +188,8 @@ class IntersectHost(IntersectModelBase):
         super().__init__()
         self.role = consts.HOST
 
-    def init_intersect_method(self):
-        super().init_intersect_method()
-        self.host_party_id = self.component_properties.local_partyid
-
-        if self.model_param.intersect_method == "rsa":
-            self.intersection_obj = RsaIntersectionHost()
-
-        elif self.model_param.intersect_method == "raw":
-            self.intersection_obj = RawIntersectionHost()
-            self.intersection_obj.tracker = self.tracker
-            self.intersection_obj.task_version_id = self.task_version_id
-        else:
-            raise ValueError("intersect_method {} is not support yet".format(self.model_param.intersect_method))
-
-        self.intersection_obj.host_party_id = self.host_party_id
-        self.intersection_obj.guest_party_id = self.guest_party_id
-        self.intersection_obj.host_party_id_list = self.host_party_id_list
-        self.intersection_obj.load_params(self.model_param)
-
 
 class IntersectGuest(IntersectModelBase):
     def __init__(self):
         super().__init__()
         self.role = consts.GUEST
-
-    def init_intersect_method(self):
-        super().init_intersect_method()
-
-        if self.model_param.intersect_method == "rsa":
-            self.intersection_obj = RsaIntersectionGuest()
-            self.intersection_obj.guest_party_id = self.guest_party_id
-
-        elif self.model_param.intersect_method == "raw":
-            self.intersection_obj = RawIntersectionGuest()
-            self.intersection_obj.tracker = self.tracker
-            self.intersection_obj.task_version_id = self.task_version_id
-        else:
-            raise ValueError("intersect_method {} is not support yet".format(self.model_param.intersect_method))
-
-        self.intersection_obj.guest_party_id = self.guest_party_id
-        self.intersection_obj.host_party_id_list = self.host_party_id_list
-        self.intersection_obj.load_params(self.model_param)
