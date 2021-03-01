8449c6e5696d93415e6d6f00c56cfaad0eb37d88
weiwee <wbwmat@gmail.com>
2021-03-01 11:23:24 +0800

    remove logs
    Signed-off-by: weiwee <wbwmat@gmail.com>

--- python/federatedml/statistic/correlation/hetero_pearson.py
+++ python/federatedml/statistic/correlation/hetero_pearson.py
@@ -22,7 +22,10 @@ from fate_flow.entity.metric import MetricMeta
 from federatedml.model_base import ModelBase
 from federatedml.param.pearson_param import PearsonParam
 from federatedml.secureprotol.spdz import SPDZ
-from federatedml.secureprotol.spdz.tensor.fixedpoint_table import FixedPointTensor, table_dot
+from federatedml.secureprotol.spdz.tensor.fixedpoint_table import (
+    FixedPointTensor,
+    table_dot,
+)
 from federatedml.util import LOGGER
 from federatedml.util.anonymous_generator import generate_anonymous
 
@@ -31,7 +34,6 @@ MODEL_PARAM_NAME = "HeteroPearsonModelParam"
 
 
 class HeteroPearson(ModelBase):
-
     def __init__(self):
         super().__init__()
         self.model_param = PearsonParam()
@@ -56,8 +58,10 @@ class HeteroPearson(ModelBase):
         guest_parties = session.get_latest_opened().parties.roles_to_parties(["guest"])
         host_parties = session.get_latest_opened().parties.roles_to_parties(["host"])
         if len(guest_parties) != 1 or len(host_parties) != 1:
-            raise ValueError(f"one guest and one host required, "
-                             f"while {len(guest_parties)} guest and {len(host_parties)} host provided")
+            raise ValueError(
+                f"one guest and one host required, "
+                f"while {len(guest_parties)} guest and {len(host_parties)} host provided"
+            )
         parties.extend(guest_parties)
         parties.extend(host_parties)
 
@@ -105,8 +109,9 @@ class HeteroPearson(ModelBase):
     @staticmethod
     def _standardized(data):
         n = data.count()
-        sum_x, sum_square_x = data.mapValues(lambda x: (x, x ** 2)) \
-            .reduce(lambda pair1, pair2: (pair1[0] + pair2[0], pair1[1] + pair2[1]))
+        sum_x, sum_square_x = data.mapValues(lambda x: (x, x ** 2)).reduce(
+            lambda pair1, pair2: (pair1[0] + pair2[0], pair1[1] + pair2[1])
+        )
         mu = sum_x / n
         sigma = np.sqrt(sum_square_x / n - mu ** 2)
         if (sigma <= 0).any():
@@ -116,7 +121,6 @@ class HeteroPearson(ModelBase):
     @staticmethod
     def _vif_from_pearson_matrix(mat: np.ndarray):
         shape = mat.shape
-        LOGGER.error(f"shape: {shape}")
         if shape[0] != shape[1]:
             raise RuntimeError("accept square matrix only")
         dim = shape[0]
@@ -130,7 +134,9 @@ class HeteroPearson(ModelBase):
 
     @staticmethod
     def _generate_determinant_one_matrix(n: int):
-        k = math.exp((math.sqrt(2 * math.log(n)) / 2 + math.log(math.factorial(n - 1))) / (2 * n))
+        k = math.exp(
+            (math.sqrt(2 * math.log(n)) / 2 + math.log(math.factorial(n - 1))) / (2 * n)
+        )
         a = np.random.randn(n, n) / k
         d = np.linalg.det(a)
         return a / d ** (1 / n)
@@ -147,13 +153,23 @@ class HeteroPearson(ModelBase):
         self._summary["num_local_features"] = n
 
         if self.model_param.cross_parties:
-            with SPDZ("pearson", local_party=self.local_party, all_parties=self.parties,
-                      use_mix_rand=self.model_param.use_mix_rand) as spdz:
+            with SPDZ(
+                "pearson",
+                local_party=self.local_party,
+                all_parties=self.parties,
+                use_mix_rand=self.model_param.use_mix_rand,
+            ) as spdz:
                 source = [normed, self.other_party]
                 if self.local_party.role == "guest":
-                    x, y = FixedPointTensor.from_source("x", source[0]), FixedPointTensor.from_source("y", source[1])
+                    x, y = (
+                        FixedPointTensor.from_source("x", source[0]),
+                        FixedPointTensor.from_source("y", source[1]),
+                    )
                 else:
-                    y, x = FixedPointTensor.from_source("y", source[0]), FixedPointTensor.from_source("x", source[1])
+                    y, x = (
+                        FixedPointTensor.from_source("y", source[0]),
+                        FixedPointTensor.from_source("x", source[1]),
+                    )
                 m1 = len(x.value.first()[1])
                 m2 = len(y.value.first()[1])
                 self.shapes.append(m1)
@@ -161,7 +177,9 @@ class HeteroPearson(ModelBase):
 
                 self.corr = spdz.dot(x, y, "corr").get() / n
                 self._summary["corr"] = self.corr.tolist()
-                self._summary["num_remote_features"] = m2 if self.local_party.role == "guest" else m1
+                self._summary["num_remote_features"] = (
+                    m2 if self.local_party.role == "guest" else m1
+                )
 
         else:
             self.shapes.append(self.local_corr.shape[0])
@@ -176,6 +194,7 @@ class HeteroPearson(ModelBase):
 
     def _get_meta(self):
         from federatedml.protobuf.generated import pearson_model_meta_pb2
+
         meta_pb = pearson_model_meta_pb2.PearsonModelMeta()
         for shape in self.shapes:
             meta_pb.shapes.append(shape)
@@ -183,6 +202,7 @@ class HeteroPearson(ModelBase):
 
     def _get_param(self):
         from federatedml.protobuf.generated import pearson_model_param_pb2
+
         param_pb = pearson_model_param_pb2.PearsonModelParam()
 
         # local
@@ -194,8 +214,9 @@ class HeteroPearson(ModelBase):
             param_pb.names.append(name)
             anonymous = param_pb.anonymous_map.add()
             anonymous.name = name
-            anonymous.anonymous = generate_anonymous(fid=idx, party_id=self.local_party.party_id,
-                                                     role=self.local_party.role)
+            anonymous.anonymous = generate_anonymous(
+                fid=idx, party_id=self.local_party.party_id, role=self.local_party.role
+            )
 
         if self.model_param.calc_local_vaf:
             for vif_value in self.local_vif:
@@ -224,12 +245,15 @@ class HeteroPearson(ModelBase):
 
     def export_model(self):
         if self.model_param.need_run:
-            return self._build_model_dict(meta=self._get_meta(), param=self._get_param())
+            return self._build_model_dict(
+                meta=self._get_meta(), param=self._get_param()
+            )
 
     # noinspection PyTypeChecker
     def _callback(self):
 
-        self.tracker.set_metric_meta(metric_namespace="statistic",
-                                     metric_name="correlation",
-                                     metric_meta=MetricMeta(name="pearson",
-                                                            metric_type="CORRELATION_GRAPH"))
+        self.tracker.set_metric_meta(
+            metric_namespace="statistic",
+            metric_name="correlation",
+            metric_meta=MetricMeta(name="pearson", metric_type="CORRELATION_GRAPH"),
+        )
