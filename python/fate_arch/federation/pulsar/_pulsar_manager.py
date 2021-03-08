692b5da97178746a03d82378d82c973b00a03bcf
jiahaoc <jiahaoc@vmware.com>
2021-03-08 17:27:49 +0800

    remove sensitive IP address
    Signed-off-by: jiahaoc <jiahaoc@vmware.com>

--- python/fate_arch/federation/pulsar/_pulsar_manager.py
+++ python/fate_arch/federation/pulsar/_pulsar_manager.py
@@ -183,34 +183,3 @@ class PulsarManager():
                                               namespace, str(force).lower())
         )
         return response
-
-
-'''
-if __name__ == '__main__':
-    pulsar_manager_a = PulsarManager('localhost', '8080')
-    pulsar_manager_b = PulsarManager('localhost', '8081')
-
-    pulsar_manager_a.create_cluster(
-        'cluster2', 'http://10.78.172.42:8081', 'pulsar://10.78.172.42:6651')
-    print(pulsar_manager_a.get_cluster().text)
-
-    pulsar_manager_b.create_cluster(
-        'cluster1', 'http://10.78.172.42:8080', 'pulsar://10.78.172.42:6650')
-    print(pulsar_manager_a.get_cluster().text)
-    # pulsar_manager_a.delete_cluster('cluster1')
-
-    # tenant could be guest_id-host_id
-    response = pulsar_manager_a.create_tenant(
-        tenant='geo-tenant', admins=[], clusters=['standalone', 'cluster2'])
-    print(response.text)
-    response = pulsar_manager_a.create_namespace(
-        'geo-tenant', 'geo-namespace', {'replication_clusters': ['standalone', 'cluster2']})
-    print(response.text)
-
-    response = pulsar_manager_b.create_tenant(
-        tenant='geo-tenant', admins=[], clusters=['cluster1', 'standalone'])
-    print(response.text)
-    response = pulsar_manager_b.create_namespace(
-        'geo-tenant', 'geo-namespace', {'replication_clusters': ['cluster1', 'standalone']})
-    print(response.text)
-'''
