924b10533cb0b80e1ac50a224f848422c70f0882
常伟杰 <17800372351@163.com>
2021-03-15 12:34:34 +0800

    fate test update

--- examples/benchmark_quality/parse_result.py
+++
@@ -1,7 +0,0 @@
-def parse_summary_result(rs_dict):
-    for model_key in rs_dict:
-        rs_content = rs_dict[model_key]
-        if 'validate' in rs_content:
-            return rs_content['validate']
-        else:
-            return rs_content['train']
