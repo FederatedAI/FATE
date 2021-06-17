2411e441c145f778619f37d65d85886082c7cd72
Jat <jat@sinosky.org>
2021-06-17 20:08:57 +0800

    unregister models at python exits
    Signed-off-by: Jat <jat@sinosky.org>

--- python/fate_flow/fate_flow_server.py
+++ python/fate_flow/fate_flow_server.py
@@ -18,6 +18,7 @@ import signal
 import sys
 import time
 import traceback
+import atexit
 
 import grpc
 from flask import Flask
@@ -103,6 +104,7 @@ if __name__ == '__main__':
     RuntimeConfig.set_process_role(ProcessRole.DRIVER)
     PrivilegeAuth.init()
     service_db().register_models()
+    atexit.register(service_db().unregister_models)
     ResourceManager.initialize()
     Detector(interval=5 * 1000).start()
     DAGScheduler(interval=2 * 1000).start()
