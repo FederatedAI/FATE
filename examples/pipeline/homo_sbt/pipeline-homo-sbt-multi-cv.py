acafa5dbed9f34c36dd44a88175511958d18f4bc
weijingchen <talkingwallace@sohu.com>
2021-10-13 16:58:34 +0800

    pipeline udpdate dataio -> data transform
    Signed-off-by: chenweijing <talkingwallace@sohu.com>

--- examples/pipeline/homo_sbt/pipeline-homo-sbt-multi-cv.py
+++ examples/pipeline/homo_sbt/pipeline-homo-sbt-multi-cv.py
@@ -17,7 +17,7 @@
 import argparse
 
 from pipeline.backend.pipeline import PipeLine
-from pipeline.component.dataio import DataIO
+from pipeline.component.data_transform import DataTransform
 from pipeline.component.homo_secureboost import HomoSecureBoost
 from pipeline.component.reader import Reader
 from pipeline.interface.data import Data
@@ -45,13 +45,13 @@ def main(config="../../config.yaml", namespace=""):
 
     pipeline = PipeLine().set_initiator(role='guest', party_id=guest).set_roles(guest=guest, host=host, arbiter=arbiter)
 
-    dataio_0 = DataIO(name="dataio_0")
+    data_transform_0 = DataTransform(name="data_transform_0")
     reader_0 = Reader(name="reader_0")
 
     reader_0.get_party_instance(role='guest', party_id=guest).component_param(table=guest_train_data)
     reader_0.get_party_instance(role='host', party_id=host).component_param(table=host_train_data)
-    dataio_0.get_party_instance(role='guest', party_id=guest).component_param(with_label=True, output_format="dense")
-    dataio_0.get_party_instance(role='host', party_id=host).component_param(with_label=True, output_format="dense")
+    data_transform_0.get_party_instance(role='guest', party_id=guest).component_param(with_label=True, output_format="dense")
+    data_transform_0.get_party_instance(role='host', party_id=host).component_param(with_label=True, output_format="dense")
 
     homo_secureboost_0 = HomoSecureBoost(name="homo_secureboost_0",
                                          num_trees=3,
@@ -68,8 +68,8 @@ def main(config="../../config.yaml", namespace=""):
                                          )
 
     pipeline.add_component(reader_0)
-    pipeline.add_component(dataio_0, data=Data(data=reader_0.output.data))
-    pipeline.add_component(homo_secureboost_0, data=Data(train_data=dataio_0.output.data))
+    pipeline.add_component(data_transform_0, data=Data(data=reader_0.output.data))
+    pipeline.add_component(homo_secureboost_0, data=Data(train_data=data_transform_0.output.data))
 
     pipeline.compile()
     job_parameters = JobParameters(backend=backend, work_mode=work_mode)
