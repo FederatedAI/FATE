fixed sshlr gradient bugs

python/federatedml/linear_model/logistic_regression/hetero_sshe_logistic_regression/hetero_lr_base.py
@@ -74,12 +74,16 @@ class HeteroLRBase(BaseLinearModel, ABC):
         self.cal_loss = self.model_param.compute_loss
         self.converge_func_name = params.early_stop
         self.review_every_iter = params.reveal_every_iter
-        self.random_field = params.random_field
+
         # self.fixpoint_filed = self.random_field ** 2
-        self.fixpoint_filed = 293973345475167247070445277780365744413 ** 2
+        self.fixpoint_filed = 293973345475167247070445277780365744413
+        # self.random_field = params.random_field
+
+        self.random_field = self.fixpoint_filed
+
         self.batch_generator = batch_generator.Guest() if self.role == consts.GUEST else batch_generator.Host()
         self.batch_generator.register_batch_generator(BatchGeneratorTransferVariable(), has_arbiter=False)
-        self.fixpoint_encoder = FixedPointEndec(n=self.fixpoint_filed)
+        self.fixedpoint_encoder = FixedPointEndec(n=self.fixpoint_filed)
         self.converge_transfer_variable = ConvergeCheckerTransferVariable()
         self.secure_matrix_obj = SecureMatrix(party=self.local_party,
                                               q_field=self.fixpoint_filed,
@@ -122,33 +126,33 @@ class HeteroLRBase(BaseLinearModel, ABC):
     def is_respectively_reveal(self):
         return self.model_param.reveal_strategy == "respectively"
 
-    def share_table(self, fix_point_encoder, value=None, tensor_name=''):
+    def share_table(self, value=None, tensor_name=''):
         if value is None:
             value = self.other_party
 
         return fixedpoint_table.FixedPointTensor.from_source(tensor_name, value,
-                                                             encoder=fix_point_encoder,
+                                                             encoder=self.fixedpoint_encoder,
                                                              q_field=self.random_field)
 
-    def share_model(self, w, fix_point_encoder, suffix):
+    def share_model(self, w, suffix):
         source = [w, self.other_party]
         if self.local_party.role == consts.GUEST:
             wb, wa = (
                 fixedpoint_numpy.FixedPointTensor.from_source(f"wb_{suffix}", source[0],
-                                                              encoder=fix_point_encoder,
+                                                              encoder=self.fixedpoint_encoder,
                                                               q_field=self.random_field),
                 fixedpoint_numpy.FixedPointTensor.from_source(f"wa_{suffix}", source[1],
-                                                              encoder=fix_point_encoder,
+                                                              encoder=self.fixedpoint_encoder,
                                                               q_field=self.random_field),
             )
             return wb, wa
         else:
             wa, wb = (
                 fixedpoint_numpy.FixedPointTensor.from_source(f"wa_{suffix}", source[0],
-                                                              encoder=fix_point_encoder,
+                                                              encoder=self.fixedpoint_encoder,
                                                               q_field=self.random_field),
                 fixedpoint_numpy.FixedPointTensor.from_source(f"wb_{suffix}", source[1],
-                                                              encoder=fix_point_encoder,
+                                                              encoder=self.fixedpoint_encoder,
                                                               q_field=self.random_field),
             )
             return wa, wb
@@ -218,31 +222,33 @@ class HeteroLRBase(BaseLinearModel, ABC):
                 q_field=self.random_field,
                 use_mix_rand=self.model_param.use_mix_rand,
         ) as spdz:
-            # self.fixpoint_encoder = self.create_fixpoint_encoder(remote_pubkey.n)
             if self.role == consts.GUEST:
                 self.labels = data_instances.mapValues(lambda x: np.array([x.label], dtype=int))
-                self.label_tensor = fixedpoint_table.FixedPointTensor(self.labels,
-                                                                      q_field=self.fixpoint_encoder.n,
-                                                                      endec=self.fixpoint_encoder)
+
+                # todo : dylan
+                label_encoder = self.fixedpoint_encoder.encode(self.labels)
+
+                self.label_tensor = fixedpoint_table.FixedPointTensor(label_encoder,
+                                                                      q_field=self.fixedpoint_encoder.n,
+                                                                      endec=self.fixedpoint_encoder)
             if self.cal_loss:
-                value = self.label_tensor.value if self.role == consts.GUEST else None
-                self.shared_y = self.share_table(self.fixpoint_encoder, value=value, tensor_name="label")
+                value = self.labels if self.role == consts.GUEST else None
+                self.shared_y = self.share_table(value=value, tensor_name="label")
                 LOGGER.debug(f"shared_y: {self.shared_y}, type: {type(self.shared_y)}")
 
-            w_self, w_remote = self.share_model(w, self.fixpoint_encoder, suffix="init")
+            w_self, w_remote = self.share_model(w, suffix="init")
             LOGGER.debug(f"first_w_self shape: {w_self.shape}, w_remote_shape: {w_remote.shape}")
 
             batch_data_generator = self.batch_generator.generate_batch_data()
             encoded_batch_data = []
             for batch_data in batch_data_generator:
                 batch_features = batch_data.mapValues(lambda x: x.features)
-                # self.encoded_batch_num.append(self.fixpoint_encoder.encode(1 / batch_data.count()))
                 self.encoded_batch_num.append(1 / batch_data.count())
 
                 encoded_batch_data.append(
-                    fixedpoint_table.FixedPointTensor(self.fixpoint_encoder.encode(batch_features),
-                                                      q_field=self.fixpoint_encoder.n,
-                                                      endec=self.fixpoint_encoder))
+                    fixedpoint_table.FixedPointTensor(self.fixedpoint_encoder.encode(batch_features),
+                                                      q_field=self.fixedpoint_encoder.n,
+                                                      endec=self.fixedpoint_encoder))
 
             while self.n_iter_ < self.max_iter:
                 self.callback_list.on_epoch_begin(self.n_iter_)
@@ -255,14 +261,9 @@ class HeteroLRBase(BaseLinearModel, ABC):
                     y = self.cal_prediction(w_self, w_remote, features=batch_data, spdz=spdz, suffix=current_suffix)
 
                     if self.role == consts.GUEST:
-                        error = y.value.join(self.labels, operator.sub)
-                        error = error.mapValues(lambda x: self.fixpoint_encoder.encode(x))
-                        LOGGER.debug(f"tmc, error: {error.first()}")
+                        error = y - self.labels
 
-                        error = fixedpoint_table.FixedPointTensor(error,
-                                                                  q_field=self.fixpoint_encoder.n,
-                                                                  endec=self.fixpoint_encoder)
-                        remote_g, self_g = self.compute_gradient(wa=w_remote, wb=w_self, error=error,
+                        self_g, remote_g = self.compute_gradient(wa=w_remote, wb=w_self, error=error,
                                                                  features=batch_data,
                                                                  suffix=current_suffix)
                     else:
@@ -278,7 +279,6 @@ class HeteroLRBase(BaseLinearModel, ABC):
                                      f"self.model_param.reveal_strategy: {self.model_param.reveal_strategy}")
 
                         if new_g is not None:
-                            new_g = np.array([x.decode() for x in new_g])
                             LOGGER.debug(f"before review, {self.model_weights.unboxed}, {new_g}")
                             self.model_weights = self.optimizer.update_model(self.model_weights, new_g,
                                                                              has_applied=False)
@@ -291,7 +291,7 @@ class HeteroLRBase(BaseLinearModel, ABC):
                             self.host_model_weights = LinearModelWeights(
                                 l=host_g,
                                 fit_intercept=False)
-                        w_self, w_remote = self.share_model(self.model_weights.unboxed, self.fixpoint_encoder,
+                        w_self, w_remote = self.share_model(self.model_weights.unboxed,
                                                             suffix=(self.n_iter_, batch_idx))
                     else:
                         w_self -= self_g * self.optimizer.decay_learning_rate()
@@ -355,29 +355,23 @@ class HeteroLRBase(BaseLinearModel, ABC):
         if self.model_param.reveal_strategy == "respectively":
 
             if self.role == consts.GUEST:
-                new_w = w_self.reconstruct(tensor_name=f"wb_{suffix}",
+                new_w = w_self.get(tensor_name=f"wb_{suffix}",
                                            broadcast=False)
                 w_remote.broadcast_reconstruct_share(tensor_name=f"wa_{suffix}")
 
-                # new_w = w_self.reconstruct_unilateral(tensor_name=f"wb_{suffix}")
-                # w_remote.broadcast_reconstruct_share(tensor_name=f"wa_{suffix}")
             else:
                 w_remote.broadcast_reconstruct_share(tensor_name=f"wb_{suffix}")
-                new_w = w_self.reconstruct(tensor_name=f"wa_{suffix}",
+                new_w = w_self.get(tensor_name=f"wa_{suffix}",
                                            broadcast=False)
-            #     w_remote.broadcast_reconstruct_share(tensor_name=f"wb_{suffix}")
-            #     new_w = w_self.reconstruct_unilateral(tensor_name=f"wa_{suffix}")
 
         elif self.model_param.reveal_strategy == "all_reveal_in_guest":
 
             if self.role == consts.GUEST:
-                new_w = w_self.reconstruct(tensor_name=f"wb_{suffix}",
+                new_w = w_self.get(tensor_name=f"wb_{suffix}",
                                            broadcast=False)
-                host_weights = w_remote.reconstruct(tensor_name=f"wa_{suffix}",
+                host_weights = w_remote.get(tensor_name=f"wa_{suffix}",
                                                     broadcast=False)
-                # new_w = w_self.reconstruct_unilateral(tensor_name=f"wb_{suffix}")
-                # host_weights = w_remote.reconstruct_unilateral(tensor_name=f"wa_{suffix}")
-                # self.host_model_weights = [LinearModelWeights(l=hosted_weights, fit_intercept=False)]
+
             else:
                 if w_remote.shape[0] > 2:
                     raise ValueError("Too many features in Guest. Review strategy: 'all_reveal_in_guest' "
@@ -385,7 +379,7 @@ class HeteroLRBase(BaseLinearModel, ABC):
                 w_remote.broadcast_reconstruct_share(tensor_name=f"wb_{suffix}")
 
                 w_self.broadcast_reconstruct_share(tensor_name=f"wa_{suffix}")
-                # new_w = np.zeros(w_self.shape)
+
                 new_w = None
         else:
             raise NotImplementedError(f"review strategy: {self.model_param.reveal_strategy} has not been implemented.")

python/federatedml/linear_model/logistic_regression/hetero_sshe_logistic_regression/hetero_lr_guest.py
@@ -88,43 +88,32 @@ class HeteroLRGuest(HeteroLRBase):
         else:
             LOGGER.debug(f"Calculate z directly.")
             z = features.dot_local(self.model_weights.coef_)
+
             if self.model_weights.fit_intercept:
-                z.value = z.value.mapValues(lambda x: x + self.model_weights.intercept_)
+                z = z + self.model_weights.intercept_
 
         remote_z = self.share_encrypted_value(suffix=suffix, is_remote=False, z=None)[0]
+
         sigmoid_z = self._compute_sigmoid(z, remote_z)
 
-        # sigmoid_z = complete_z * 0.2 + 0.5
         self.encrypted_wx = sigmoid_z
-        encrypted_error = sigmoid_z.value.join(self.labels, operator.sub)
-        self.encrypted_error = fixedpoint_table.PaillierFixedPointTensor(
-            encrypted_error
-        )
-        # shared_sigmoid_z = self.share_matrix(sigmoid_z, suffix=("sigmoid_z",) + suffix)
+
+        self.encrypted_error = sigmoid_z - self.labels
+
         tensor_name = ".".join(("sigmoid_z",) + suffix)
         shared_sigmoid_z = SecureMatrix.from_source(tensor_name,
                                                     sigmoid_z,
                                                     self.cipher,
-                                                    self.fixpoint_encoder.n,
-                                                    self.fixpoint_encoder)
+                                                    self.fixedpoint_encoder.n,
+                                                    self.fixedpoint_encoder)
         return shared_sigmoid_z
 
     def compute_gradient(self, wa, wb, error, features, suffix):
         encoded_1_n = self.encoded_batch_num[int(suffix[1])]
+
         error_1_n = error * encoded_1_n
 
-        encrypt_g = self.encrypted_error.value.join(features.value, operator.mul).reduce(operator.add) * encoded_1_n
-        if self.fit_intercept:
-            bias = self.encrypted_error.value.reduce(operator.add) * encoded_1_n
-            encrypt_g = np.array(list(encrypt_g) + list(bias))
-        encrypt_g = fixedpoint_numpy.PaillierFixedPointTensor(encrypt_g)
-        # gb2 = self.share_matrix(encrypt_g, suffix=("encrypt_g",) + suffix)
-        tensor_name = ".".join(("encrypt_g",) + suffix)
-        gb2 = SecureMatrix.from_source(tensor_name,
-                                       encrypt_g,
-                                       self.cipher,
-                                       self.fixpoint_encoder.n,
-                                       self.fixpoint_encoder)
+        LOGGER.debug(f"error_1_n: {error_1_n}")
 
         ga2_suffix = ("ga2",) + suffix
         ga2_2 = self.secure_matrix_obj.secure_matrix_mul(error_1_n,
@@ -133,18 +122,26 @@ class HeteroLRGuest(HeteroLRBase):
                                                          suffix=ga2_suffix,
                                                          is_fixedpoint_table=False)
 
-        # self.secure_matrix_mul_active(error_1_n, cipher=self.cipher,
-        #                               suffix=ga2_suffix)
-        # ga2_2 = self.received_share_matrix(self.cipher, q_field=self.fixpoint_encoder.n,
-        #                                    encoder=self.fixpoint_encoder, suffix=ga2_suffix)
+        LOGGER.debug(f"ga2_2: {ga2_2}")
 
-        # wb = wb - gb2 * self.model_param.learning_rate
-        ga2_2 = ga2_2.reshape(ga2_2.shape[0])
-        LOGGER.debug(f"wa shape: {wa.shape}, ga_shape: {ga2_2.shape}, {ga2_2.value[0].__dict__}")
-        # wa = wa - ga2_2 * self.model_param.learning_rate
-        # wa = wa.reshape(wa.shape[-1])
+        encrypt_g = self.encrypted_error.dot(features) * encoded_1_n
+        if self.fit_intercept:
+            bias = self.encrypted_error.reduce(operator.add) * encoded_1_n
+            encrypt_g = np.array(list(encrypt_g.value) + list(bias.value))
+            encrypt_g = fixedpoint_numpy.PaillierFixedPointTensor(encrypt_g)
 
-        return ga2_2, gb2
+        LOGGER.debug(f"encrypt_g: {encrypt_g}")
+
+        tensor_name = ".".join(("encrypt_g",) + suffix)
+        gb2 = SecureMatrix.from_source(tensor_name,
+                                       encrypt_g,
+                                       self.cipher,
+                                       self.fixedpoint_encoder.n,
+                                       self.fixedpoint_encoder)
+
+        LOGGER.debug(f"gb2: {gb2}")
+
+        return gb2, ga2_2
 
     def compute_loss(self, spdz, suffix):
         """
@@ -159,8 +156,8 @@ class HeteroLRGuest(HeteroLRBase):
         shared_wx = SecureMatrix.from_source(tensor_name,
                                              self.encrypted_wx,
                                              self.cipher,
-                                             self.fixpoint_encoder.n,
-                                             self.fixpoint_encoder)
+                                             self.fixedpoint_encoder.n,
+                                             self.fixedpoint_encoder)
 
         wxy = spdz.dot(shared_wx, self.shared_y, ("wxy",) + suffix).get()
         LOGGER.debug(f"wxy_value: {wxy}, shared_wx: {shared_wx.value.first()}")

python/federatedml/secureprotol/spdz/secure_matrix/secure_matrix.py
@@ -40,39 +40,6 @@ class SecureMatrix(object):
             self.encoder = FixedPointEndec(q_field)
         return self.encoder
 
-    @classmethod
-    def table_dot(cls, a_table, b_table):
-        def _table_dot_func(it):
-            ret = None
-            for _, (x, y) in it:
-                if ret is None:
-                    ret = np.tensordot(x, y, [[], []])
-                else:
-                    ret += np.tensordot(x, y, [[], []])
-            return ret
-
-        return a_table.join(b_table, lambda x, y: [x, y]) \
-            .applyPartitions(lambda it: _table_dot_func(it)) \
-            .reduce(lambda x, y: x + y)
-
-    @classmethod
-    def dot(cls, matrix, y):
-        def _vec_dot(x, y):
-            ret = np.dot(x, y)
-            if not isinstance(ret, np.ndarray):
-                ret = np.array([ret])
-            return ret
-
-        if isinstance(y, np.ndarray):
-            ret = matrix.mapValues(lambda x: _vec_dot(x, y))
-            return ret
-
-        elif is_table(y):
-            ret = cls.table_dot(matrix, y)
-            return ret
-        else:
-            raise ValueError(f"type={type(y)}")
-
     def secure_matrix_mul(self, matrix, tensor_name, cipher=None, suffix=tuple(), is_fixedpoint_table=True):
         curt_suffix = ("secure_matrix_mul",) + suffix
         dst_role = consts.GUEST if self.party.role == consts.HOST else consts.HOST
@@ -85,7 +52,6 @@ class SecureMatrix(object):
                 encrypt_mat = cipher.recursive_encrypt(de_matrix)
 
             # remote encrypted matrix;
-            LOGGER.debug(f"In_secure_matrix_mul, encrypt_mat: {encrypt_mat}")
             self.transfer_variable.encrypted_share_matrix.remote(encrypt_mat,
                                                                  role=dst_role,
                                                                  idx=0,
@@ -104,11 +70,14 @@ class SecureMatrix(object):
             share = self.transfer_variable.encrypted_share_matrix.get(role=dst_role,
                                                                       idx=0,
                                                                       suffix=curt_suffix)
-            LOGGER.debug(f"Make share tensor")
-            if isinstance(matrix, (fixedpoint_table.FixedPointTensor,
-                                   fixedpoint_table.PaillierFixedPointTensor)):
-                matrix = matrix.value
-            ret = SecureMatrix.dot(matrix, share)
+
+            if is_table(share):
+                share = fixedpoint_table.PaillierFixedPointTensor(share)
+                ret = share.dot(matrix)
+            else:
+                share = fixedpoint_numpy.PaillierFixedPointTensor(share)
+                ret = share.dot(matrix)
+
             LOGGER.debug(f"tmc, ret: {ret}")
             share_tensor = SecureMatrix.from_source(tensor_name,
                                                     ret,

python/federatedml/secureprotol/spdz/tensor/fixedpoint_numpy.py
@@ -18,7 +18,7 @@ import numpy as np
 from fate_arch.common import Party
 from federatedml.secureprotol.spdz.beaver_triples import beaver_triplets
 from federatedml.secureprotol.spdz.tensor.base import TensorBase
-from federatedml.secureprotol.spdz.utils.random_utils import urand_tensor
+from federatedml.secureprotol.spdz.utils.random_utils import urand_tensor, urand_tensor2
 from federatedml.secureprotol.spdz.tensor.fixedpoint_endec import FixedPointEndec
 from federatedml.util import LOGGER
 
@@ -84,7 +84,7 @@ class FixedPointTensor(TensorBase):
             encoder = FixedPointEndec(q_field, base, frac)
         if isinstance(source, np.ndarray):
             source = encoder.encode(source)
-            _pre = urand_tensor(q_field, source)
+            _pre = urand_tensor2(q_field, source)
             spdz.communicator.remote_share(share=_pre, tensor_name=tensor_name, party=spdz.other_parties[0])
             for _party in spdz.other_parties[1:]:
                 r = urand_tensor(q_field, source)
@@ -162,7 +162,7 @@ class FixedPointTensor(TensorBase):
         return FixedPointTensor(value=value, q_field=self.q_field, endec=self.endec, tensor_name=tensor_name)
 
     def __str__(self):
-        return f"{self.tensor_name}: {self.value}"
+        return f"tensor_name={self.tensor_name}"
 
     def __repr__(self):
         return self.__str__()
@@ -233,7 +233,7 @@ class PaillierFixedPointTensor(TensorBase):
         return self._boxed(ret, target_name)
 
     def __str__(self):
-        return f"{self.tensor_name}: {self.value}"
+        return f"tensor_name={self.tensor_name}"
 
     def __repr__(self):
         return self.__str__()
@@ -289,15 +289,18 @@ class PaillierFixedPointTensor(TensorBase):
             encoder = FixedPointEndec(q_field, base, frac)
 
         if isinstance(source, np.ndarray):
-            _pre = urand_tensor(q_field, source)
+            _pre = urand_tensor2(q_field, source)
+            # todo: dylan
             share = _pre
-            for _party in spdz.other_parties[:-1]:
-                r = urand_tensor(q_field, source)
-                spdz.communicator.remote_share(share=r - _pre, tensor_name=tensor_name, party=_party)
-                _pre = r
+
+            # for _party in spdz.other_parties[:-1]:
+            #     r = urand_tensor(q_field, source)
+            #     spdz.communicator.remote_share(share=r - _pre, tensor_name=tensor_name, party=_party)
+            #     _pre = r
             spdz.communicator.remote_share(share=source - encoder.decode(_pre),
                                            tensor_name=tensor_name,
                                            party=spdz.other_parties[-1])
+
             return FixedPointTensor(value=share,
                                     q_field=q_field,
                                     endec=encoder,

python/federatedml/secureprotol/spdz/tensor/fixedpoint_table.py
@@ -25,7 +25,7 @@ from federatedml.secureprotol.spdz.beaver_triples import beaver_triplets
 from federatedml.secureprotol.spdz.tensor import fixedpoint_numpy
 from federatedml.secureprotol.spdz.tensor.base import TensorBase
 from federatedml.secureprotol.spdz.utils import NamingService
-from federatedml.secureprotol.spdz.utils.random_utils import urand_tensor
+from federatedml.secureprotol.spdz.utils.random_utils import urand_tensor, urand_tensor2
 from federatedml.secureprotol.spdz.tensor.fixedpoint_endec import FixedPointEndec
 
 
@@ -109,7 +109,6 @@ class FixedPointTensor(TensorBase):
         return share
 
     def dot_local(self, other, target_name=None):
-
         def _vec_dot(x, y, party_idx, q_field, endec):
             ret = np.dot(x, y) % q_field
             ret = endec.truncate(ret, party_idx)
@@ -130,7 +129,8 @@ class FixedPointTensor(TensorBase):
             return self._boxed(ret, target_name)
 
         elif is_table(other):
-            ret = table_dot_mod(self.value, other, self.q_field)
+            # todo: dylan[0]
+            ret = table_dot_mod(self.value, other, self.q_field)[0]
             ret = self.endec.truncate(ret, self.get_spdz().party_idx)
             return fixedpoint_numpy.FixedPointTensor(ret,
                                                      self.q_field,
@@ -155,7 +155,7 @@ class FixedPointTensor(TensorBase):
             encoder = fixedpoint_numpy.FixedPointEndec(q_field, base, frac)
         if is_table(source):
             source = encoder.encode(source)
-            _pre = urand_tensor(q_field, source, use_mix=spdz.use_mix_rand)
+            _pre = urand_tensor2(q_field, source, use_mix=spdz.use_mix_rand)
             spdz.communicator.remote_share(share=_pre, tensor_name=tensor_name, party=spdz.other_parties[0])
             for _party in spdz.other_parties[1:]:
                 r = urand_tensor(q_field, source, use_mix=spdz.use_mix_rand)
@@ -190,11 +190,22 @@ class FixedPointTensor(TensorBase):
             share_val = _table_binary_mod_op(share_val, other_share, self.q_field, operator.add)
         return share_val
 
-    # def __str__(self):
-    #     return f"{self.tensor_name}: {self.value}"
-    #
-    # def __repr__(self):
-    #     return self.__str__()
+    def broadcast_reconstruct_share(self, tensor_name=None):
+        from federatedml.secureprotol.spdz import SPDZ
+        spdz = SPDZ.get_instance()
+        share_val = self.value
+        name = tensor_name or self.tensor_name
+        if name is None:
+            raise ValueError("name not specified")
+        # remote share to other parties
+        spdz.communicator.broadcast_rescontruct_share(share_val, name)
+        return share_val
+
+    def __str__(self):
+        return f"tensor_name={self.tensor_name}"
+
+    def __repr__(self):
+        return self.__str__()
 
     def as_name(self, tensor_name):
         return self._boxed(value=self.value, tensor_name=tensor_name)
@@ -202,25 +213,38 @@ class FixedPointTensor(TensorBase):
     def __add__(self, other):
         if isinstance(other, PaillierFixedPointTensor):
             return other + self
-
         if isinstance(other, FixedPointTensor):
-            other = other.value
-        z_value = _table_binary_mod_op(self.value, other, self.q_field, operator.add)
+            z_value = _table_binary_mod_op(self.value, other.value, self.q_field, operator.add)
+        elif is_table(other):
+            z_value = _table_binary_mod_op(self.value, other, self.q_field, operator.add)
+        else:
+            z_value = _table_scalar_mod_op(self.value, other, self.q_field, operator.add)
         return self._boxed(z_value)
 
     def __radd__(self, other):
         return self.__add__(other)
 
     def __sub__(self, other):
+        if isinstance(other, PaillierFixedPointTensor):
+            return other + self
         if isinstance(other, FixedPointTensor):
-            other = other.value
-        z_value = _table_binary_mod_op(self.value, other, self.q_field, operator.sub)
+            z_value = _table_binary_mod_op(self.value, other.value, self.q_field, operator.sub)
+        elif is_table(other):
+            z_value = _table_binary_mod_op(self.value, other, self.q_field, operator.sub)
+        else:
+            z_value = _table_scalar_mod_op(self.value, other, self.q_field, operator.sub)
+
         return self._boxed(z_value)
 
     def __rsub__(self, other):
+        if isinstance(other, PaillierFixedPointTensor):
+            return other - self
         if isinstance(other, FixedPointTensor):
             return other - self
-        z_value = _table_binary_mod_op(other, self.value, self.q_field, operator.sub)
+        elif is_table(other):
+            z_value = _table_binary_mod_op(other, self.value, self.q_field, operator.sub)
+        else:
+            z_value = _table_scalar_mod_op(self.value, other,  self.q_field, -1 * operator.sub)
         return self._boxed(z_value)
 
     def __mul__(self, other):
@@ -269,16 +293,21 @@ class PaillierFixedPointTensor(TensorBase):
             return self._boxed(ret, target_name)
 
         elif is_table(other):
-            ret = table_dot(self.value, other)
+            # todo: dylan [0]
+            ret = table_dot(self.value, other)[0]
             return fixedpoint_numpy.PaillierFixedPointTensor(ret, target_name)
         else:
             raise ValueError(f"type={type(other)}")
 
-    # def __str__(self):
-    #     return f"{self.tensor_name}: {self.value}"
-    #
-    # def __repr__(self):
-    #     return self.__str__()
+    def reduce(self, func, **kwargs):
+        ret = self.value.reduce(func)
+        return fixedpoint_numpy.PaillierFixedPointTensor(ret)
+
+    def __str__(self):
+        return f"tensor_name={self.tensor_name}"
+
+    def __repr__(self):
+        return self.__str__()
 
     def __add__(self, other):
         if isinstance(other, (PaillierFixedPointTensor, FixedPointTensor)):
@@ -292,7 +321,7 @@ class PaillierFixedPointTensor(TensorBase):
         return self.__add__(other)
 
     def __sub__(self, other):
-        if isinstance(other, PaillierFixedPointTensor):
+        if isinstance(other, (PaillierFixedPointTensor, FixedPointTensor)):
             return self._boxed(_table_binary_op(self.value, other.value, operator.sub))
         elif is_table(other):
             return self._boxed(_table_binary_op(self.value, other, operator.sub))
@@ -300,7 +329,7 @@ class PaillierFixedPointTensor(TensorBase):
             return self._boxed(_table_scalar_op(self.value, other, operator.sub))
 
     def __rsub__(self, other):
-        if isinstance(other, PaillierFixedPointTensor):
+        if isinstance(other, (PaillierFixedPointTensor, FixedPointTensor)):
             return self._boxed(_table_binary_op(other.value, self.value, operator.sub))
         elif is_table(other):
             return self._boxed(_table_binary_op(other, self.value, operator.sub))
@@ -334,14 +363,14 @@ class PaillierFixedPointTensor(TensorBase):
             encoder = FixedPointEndec(q_field, base, frac)
 
         if is_table(source):
-            _pre = urand_tensor(q_field, source, use_mix=spdz.use_mix_rand)
+            _pre = urand_tensor2(q_field, source, use_mix=spdz.use_mix_rand)
+            # todo: dylan
             share = _pre
-
-            for _party in spdz.other_parties[:-1]:
-                r = urand_tensor(q_field, source, use_mix=spdz.use_mix_rand)
-                spdz.communicator.remote_share(share=_table_binary_mod_op(r, _pre, q_field, operator.sub),
-                                               tensor_name=tensor_name, party=_party)
-                _pre = r
+            # for _party in spdz.other_parties[:-1]:
+            #     r = urand_tensor(q_field, source, use_mix=spdz.use_mix_rand)
+            #     spdz.communicator.remote_share(share=_table_binary_mod_op(r, _pre, q_field, operator.sub),
+            #                                    tensor_name=tensor_name, party=_party)
+            #     _pre = r
             spdz.communicator.remote_share(share=_table_binary_op(source, encoder.decode(_pre), operator.sub),
                                            tensor_name=tensor_name, party=spdz.other_parties[-1])
             return FixedPointTensor(value=share,