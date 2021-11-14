8f8839adbd044ecd6d84205b54eb089b6b241520
dylanfan <289765648@qq.com>
2021-11-14 16:42:07 +0800

    fixed spdz/tensor design and bugs

--- python/federatedml/secureprotol/spdz/tensor/fixedpoint_table.py
+++ python/federatedml/secureprotol/spdz/tensor/fixedpoint_table.py
@@ -220,10 +220,9 @@ class FixedPointTensor(TensorBase):
 
     def __add__(self, other):
         if isinstance(other, PaillierFixedPointTensor):
-            # PaillierFixedPointTensor
             z_value = _table_binary_op(self.value, other.value, operator.add)
             return PaillierFixedPointTensor(z_value)
-        if isinstance(other, FixedPointTensor):
+        elif isinstance(other, FixedPointTensor):
             z_value = _table_binary_mod_op(self.value, other.value, self.q_field, operator.add)
         elif is_table(other):
             z_value = _table_binary_mod_op(self.value, other, self.q_field, operator.add)
@@ -236,9 +235,9 @@ class FixedPointTensor(TensorBase):
 
     def __sub__(self, other):
         if isinstance(other, PaillierFixedPointTensor):
-            z_value = _table_binary_op(self.value, other.value, operator)
+            z_value = _table_binary_op(self.value, other.value, operator.sub)
             return PaillierFixedPointTensor(z_value)
-        if isinstance(other, FixedPointTensor):
+        elif isinstance(other, FixedPointTensor):
             z_value = _table_binary_mod_op(self.value, other.value, self.q_field, operator.sub)
         elif is_table(other):
             z_value = _table_binary_mod_op(self.value, other, self.q_field, operator.sub)
@@ -248,9 +247,7 @@ class FixedPointTensor(TensorBase):
         return self._boxed(z_value)
 
     def __rsub__(self, other):
-        if isinstance(other, PaillierFixedPointTensor):
-            return other - self
-        if isinstance(other, FixedPointTensor):
+        if isinstance(other, (PaillierFixedPointTensor, FixedPointTensor)):
             return other - self
         elif is_table(other):
             z_value = _table_binary_mod_op(other, self.value, self.q_field, operator.sub)
@@ -260,10 +257,9 @@ class FixedPointTensor(TensorBase):
 
     def __mul__(self, other):
         if isinstance(other, FixedPointTensor):
-            # raise NotImplementedError("__mul__ support scalar only")
             z_value = _table_binary_mod_op(self.value, other.value, self.q_field, operator.mul)
         elif isinstance(other, PaillierFixedPointTensor):
-            return other * self
+            z_value = _table_binary_op(self.value, other.value, operator.mul)
         else:
             z_value = _table_scalar_mod_op(self.value, other, self.q_field, operator.mul)
         z_value = self.endec.truncate(z_value, self.get_spdz().party_idx)
