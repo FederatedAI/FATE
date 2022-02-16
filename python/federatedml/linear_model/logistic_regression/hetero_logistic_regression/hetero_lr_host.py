5b4b4707262a495ce24c77fb664084368d2ae1b5
mgqa34 <mgq3374541@163.com>
2022-02-17 01:02:52 +0800

    optimize mini batch generator logic and hetero-lr batch logic
    Signed-off-by: mgqa34 <mgq3374541@163.com>

--- python/federatedml/linear_model/logistic_regression/hetero_logistic_regression/hetero_lr_host.py
+++ python/federatedml/linear_model/logistic_regression/hetero_logistic_regression/hetero_lr_host.py
@@ -101,13 +101,11 @@ class HeteroLRHost(HeteroLRBase):
         self.header = self.get_header(data_instances)
         self.cipher_operator = self.cipher.gen_paillier_cipher_operator()
 
+        self.batch_generator.initialize_batch_generator(data_instances, shuffle=self.shuffle)
+
         if self.transfer_variable.use_async.get(idx=0):
             LOGGER.debug(f"set_use_async")
             self.gradient_loss_operator.set_use_async()
-
-        self.batch_generator.initialize_batch_generator(data_instances, shuffle=self.shuffle)
-        if self.batch_generator.batch_masked:
-            self.gradient_loss_operator.set_use_sync()
 
         self.gradient_loss_operator.set_total_batch_nums(self.batch_generator.batch_nums)
 
