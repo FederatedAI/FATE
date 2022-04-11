a02e1da537d105627796347bd36bff0a9be0e83d
nemirorox <yolandawu131@gmail.com>
2022-04-11 17:59:42 +0800

    edit fate-test config (#3766)
    Signed-off-by: nemirorox <yolandawu131@gmail.com>

--- python/fate_test/fate_test/_config.py
+++ python/fate_test/fate_test/_config.py
@@ -39,7 +39,7 @@ flow_test_config_directory: examples/flow_test_template/hetero_lr/flow_test_conf
 min_test_data_config: examples/data/upload_config/min_test_data_testsuite.json
 # directory stores testsuite file with all example data sets to upload, 
 # default location={FATE}/examples/data/upload_config/all_test_data_testsuite.json
-all_example_data_config: examples/data/upload_config/all_test_data_testsuite.json
+all_examples_data_config: examples/data/upload_config/all_test_data_testsuite.json
 
 # directory where FATE code locates, default installation location={FATE}/fate
 # python/federatedml -> $fate_base/python/federatedml
