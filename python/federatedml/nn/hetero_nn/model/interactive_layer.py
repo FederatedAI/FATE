9c663f256632922407465e1ea442990a8553179e
weijing <talkingwallace@sohu.com>
2022-07-18 18:11:02 +0800

    reformat
    Signed-off-by: weijingchen <talkingwallace@sohu.com>

--- python/federatedml/nn/hetero_nn/model/interactive_layer.py
+++ python/federatedml/nn/hetero_nn/model/interactive_layer.py
@@ -505,7 +505,8 @@ class InteractiveHostDenseLayer(object):
     def get_guest_encrypted_weight_gradient_from_guest(self, epoch, batch):
         encrypted_guest_weight_gradient = self.transfer_variable.encrypted_guest_weight_gradient.get(idx=0,
                                                                                                      suffix=(
-                                                                                                      epoch, batch,))
+                                                                                                     epoch, batch,))
+                                                                                                                                                                                                 
         return encrypted_guest_weight_gradient
 
     def get_interactive_layer_drop_out_table(self, epoch, batch):
