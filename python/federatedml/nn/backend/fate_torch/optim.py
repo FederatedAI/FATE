6b6b775ee671692970fae35330d7d8f8f90ead67
weijingchen <talkingwallace@sohu.com>
2022-07-19 17:24:19 +0800

    reformat code
    Signed-off-by: weijingchen <talkingwallace@sohu.com>

--- python/federatedml/nn/backend/fate_torch/optim.py
+++ python/federatedml/nn/backend/fate_torch/optim.py
@@ -1,11 +1,12 @@
 import inspect
 from torch import optim
 from federatedml.nn.backend.fate_torch.base import FateTorchOptimizer
+
 TORCH_DICT = dict(inspect.getmembers(optim))
 
 
 class ASGD(FateTorchOptimizer):
-        
+
     def __init__(self, fate_torch_component=None, lr=0.01, lambd=0.0001, alpha=0.75, t0=1000000.0, weight_decay=0, ):
         FateTorchOptimizer.__init__(self)
         self.param_dict['lr'] = lr
@@ -16,10 +17,10 @@ class ASGD(FateTorchOptimizer):
         self.register_optimizer(fate_torch_component)
         # optim.ASGD.__init__(self, **self.param_dict)
         self.torch_class = TORCH_DICT[type(self).__name__]
-    
-    
+
+
 class Adadelta(FateTorchOptimizer):
-        
+
     def __init__(self, fate_torch_component=None, lr=1.0, rho=0.9, eps=1e-06, weight_decay=0, ):
         FateTorchOptimizer.__init__(self)
         self.param_dict['lr'] = lr
@@ -29,11 +30,12 @@ class Adadelta(FateTorchOptimizer):
         self.register_optimizer(fate_torch_component)
         # optim.Adadelta.__init__(self, **self.param_dict)
         self.torch_class = TORCH_DICT[type(self).__name__]
-    
-    
+
+
 class Adagrad(FateTorchOptimizer):
-        
-    def __init__(self, fate_torch_component=None, lr=0.01, lr_decay=0, weight_decay=0, initial_accumulator_value=0, eps=1e-10, ):
+
+    def __init__(self, fate_torch_component=None, lr=0.01, lr_decay=0, weight_decay=0, initial_accumulator_value=0,
+                 eps=1e-10, ):
         FateTorchOptimizer.__init__(self)
         self.param_dict['lr'] = lr
         self.param_dict['lr_decay'] = lr_decay
@@ -43,11 +45,12 @@ class Adagrad(FateTorchOptimizer):
         self.register_optimizer(fate_torch_component)
         # optim.Adagrad.__init__(self, **self.param_dict)
         self.torch_class = TORCH_DICT[type(self).__name__]
-    
-    
+
+
 class Adam(FateTorchOptimizer):
-        
-    def __init__(self, fate_torch_component=None, lr=0.001, betas=(0.9, 0.999), eps=1e-08, weight_decay=0, amsgrad=False, ):
+
+    def __init__(self, fate_torch_component=None, lr=0.001, betas=(0.9, 0.999), eps=1e-08, weight_decay=0,
+                 amsgrad=False, ):
         FateTorchOptimizer.__init__(self)
         self.param_dict['lr'] = lr
         self.param_dict['betas'] = betas
@@ -57,11 +60,12 @@ class Adam(FateTorchOptimizer):
         self.register_optimizer(fate_torch_component)
         # optim.Adam.__init__(self, **self.param_dict)
         self.torch_class = TORCH_DICT[type(self).__name__]
-    
-    
+
+
 class AdamW(FateTorchOptimizer):
-        
-    def __init__(self, fate_torch_component=None, lr=0.001, betas=(0.9, 0.999), eps=1e-08, weight_decay=0.01, amsgrad=False, ):
+
+    def __init__(self, fate_torch_component=None, lr=0.001, betas=(0.9, 0.999), eps=1e-08, weight_decay=0.01,
+                 amsgrad=False, ):
         FateTorchOptimizer.__init__(self)
         self.param_dict['lr'] = lr
         self.param_dict['betas'] = betas
@@ -71,10 +75,10 @@ class AdamW(FateTorchOptimizer):
         self.register_optimizer(fate_torch_component)
         # optim.AdamW.__init__(self, **self.param_dict)
         self.torch_class = TORCH_DICT[type(self).__name__]
-    
-    
+
+
 class Adamax(FateTorchOptimizer):
-        
+
     def __init__(self, fate_torch_component=None, lr=0.002, betas=(0.9, 0.999), eps=1e-08, weight_decay=0, ):
         FateTorchOptimizer.__init__(self)
         self.param_dict['lr'] = lr
@@ -84,11 +88,12 @@ class Adamax(FateTorchOptimizer):
         self.register_optimizer(fate_torch_component)
         # optim.Adamax.__init__(self, **self.param_dict)
         self.torch_class = TORCH_DICT[type(self).__name__]
-    
-    
+
+
 class LBFGS(FateTorchOptimizer):
-        
-    def __init__(self, fate_torch_component=None, lr=1, max_iter=20, max_eval=None, tolerance_grad=1e-07, tolerance_change=1e-09, history_size=100, line_search_fn=None, ):
+
+    def __init__(self, fate_torch_component=None, lr=1, max_iter=20, max_eval=None, tolerance_grad=1e-07,
+                 tolerance_change=1e-09, history_size=100, line_search_fn=None, ):
         FateTorchOptimizer.__init__(self)
         self.param_dict['lr'] = lr
         self.param_dict['max_iter'] = max_iter
@@ -100,11 +105,12 @@ class LBFGS(FateTorchOptimizer):
         self.register_optimizer(fate_torch_component)
         # optim.LBFGS.__init__(self, **self.param_dict)
         self.torch_class = TORCH_DICT[type(self).__name__]
-    
-    
+
+
 class RMSprop(FateTorchOptimizer):
-        
-    def __init__(self, fate_torch_component=None, lr=0.01, alpha=0.99, eps=1e-08, weight_decay=0, momentum=0, centered=False, ):
+
+    def __init__(self, fate_torch_component=None, lr=0.01, alpha=0.99, eps=1e-08, weight_decay=0, momentum=0,
+                 centered=False, ):
         FateTorchOptimizer.__init__(self)
         self.param_dict['lr'] = lr
         self.param_dict['alpha'] = alpha
@@ -115,10 +121,10 @@ class RMSprop(FateTorchOptimizer):
         self.register_optimizer(fate_torch_component)
         # optim.RMSprop.__init__(self, **self.param_dict)
         self.torch_class = TORCH_DICT[type(self).__name__]
-    
-    
+
+
 class Rprop(FateTorchOptimizer):
-        
+
     def __init__(self, fate_torch_component=None, lr=0.01, etas=(0.5, 1.2), step_sizes=(1e-06, 50), ):
         FateTorchOptimizer.__init__(self)
         self.param_dict['lr'] = lr
@@ -127,10 +133,10 @@ class Rprop(FateTorchOptimizer):
         self.register_optimizer(fate_torch_component)
         # optim.Rprop.__init__(self, **self.param_dict)
         self.torch_class = TORCH_DICT[type(self).__name__]
-    
-    
+
+
 class SGD(FateTorchOptimizer):
-        
+
     def __init__(self, lr, fate_torch_component=None, momentum=0, dampening=0, weight_decay=0, nesterov=False, ):
         FateTorchOptimizer.__init__(self)
         self.param_dict['lr'] = lr
@@ -141,10 +147,10 @@ class SGD(FateTorchOptimizer):
         self.register_optimizer(fate_torch_component)
         # optim.SGD.__init__(self, **self.param_dict)
         self.torch_class = TORCH_DICT[type(self).__name__]
-    
-    
+
+
 class SparseAdam(FateTorchOptimizer):
-        
+
     def __init__(self, fate_torch_component=None, lr=0.001, betas=(0.9, 0.999), eps=1e-08, ):
         FateTorchOptimizer.__init__(self)
         self.param_dict['lr'] = lr
@@ -153,5 +159,3 @@ class SparseAdam(FateTorchOptimizer):
         self.register_optimizer(fate_torch_component)
         # optim.SparseAdam.__init__(self, **self.param_dict)
         self.torch_class = TORCH_DICT[type(self).__name__]
-    
-    
