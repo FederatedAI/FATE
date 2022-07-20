ccd6d855c74dc7483ba232a3a6e56fdddb182cae
weijingchen <talkingwallace@sohu.com>
2022-07-20 17:02:14 +0800

    fix fate client import
    Signed-off-by: weijingchen <talkingwallace@sohu.com>

--- python/fate_client/pipeline/__init__.py
+++ python/fate_client/pipeline/__init__.py
@@ -1,4 +1,7 @@
-from pipeline.component.nn.backend.fate_torch.import_hook import fate_torch_hook
-from pipeline.component.nn.backend import fate_torch
+try:
+    from pipeline.component.nn.backend.fate_torch.import_hook import fate_torch_hook
+    from pipeline.component.nn.backend import fate_torch
+except:
+    fate_torch_hook, fate_torch = None, None
 
 __all__ = ['fate_torch_hook', 'fate_torch']
