60b6c20ea45b78515a52ec5e55c9d32eecc44889
zhihuiwan <15779896112@163.com>
2022-07-27 21:26:11 +0800

    update cleanup
    Signed-off-by: zhihuiwan <15779896112@163.com>

--- python/fate_arch/session/_session.py
+++ python/fate_arch/session/_session.py
@@ -510,7 +510,6 @@ class Session(object):
                 storage = self.storage()
                 if self.is_computing_valid:
                     try:
-                        storage = self.storage()
                         self._logger.info('clean table by namespace {}'.format(self._computing_session.session_id))
                         storage.cleanup(namespace=self._computing_session.session_id, name="*")
                         self._logger.info(f'clean table namespace {self._computing_session.session_id} done')
