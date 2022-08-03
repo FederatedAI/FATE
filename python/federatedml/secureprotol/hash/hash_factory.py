11e1f990bbaaae3610a5de9ddd00abe99d768ef3
nemirorox <yolandawu131@gmail.com>
2022-08-03 10:57:20 +0800

    fix sm3 base64(#4180)
    Signed-off-by: nemirorox <yolandawu131@gmail.com>

--- python/federatedml/secureprotol/hash/hash_factory.py
+++ python/federatedml/secureprotol/hash/hash_factory.py
@@ -61,7 +61,7 @@ def compute_sm3(value):
 
 
 def compute_sm3_base64(value):
-    return str(base64.b64encode(libsm3py.hash(bytes(value, encoding='utf-8')).encode('utf-8')), "utf-8")
+    return str(base64.b64encode(libsm3py.hash(bytes(value, encoding='utf-8'))), "utf-8")
 
 
 def compute_no_hash(value):
