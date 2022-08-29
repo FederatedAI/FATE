00f8da4154b460f879a31fee5a3109564ab746d6
weijingchen <talkingwallace@sohu.com>
2022-08-29 14:40:02 +0800

    pep8
    Signed-off-by: cwj <talkingwallace@sohu.com>

--- examples/benchmark_quality/hetero_nn_pytorch/local-hetero_nn.py
+++ examples/benchmark_quality/hetero_nn_pytorch/local-hetero_nn.py
@@ -139,6 +139,7 @@ def main(config="./config.yaml", param="./hetero_nn_breast_config.yaml"):
 
 
 if __name__ == "__main__":
+
     parser = argparse.ArgumentParser("BENCHMARK-QUALITY SKLEARN JOB")
     parser.add_argument("-config", type=str,
                         help="config file")
