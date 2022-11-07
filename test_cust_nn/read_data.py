de081b38a26895a54b5af92a198a702738def82d
weijingchen <talkingwallace@sohu.com>
2022-11-07 10:18:10 +0800

    remove old codes
    Signed-off-by: weijingchen <talkingwallace@sohu.com>

--- test_cust_nn/read_data.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import torch as t
-import pandas as pd
-from torch.nn.utils.rnn import pad_sequence
-
-df_guest = pd.read_csv('../examples/data/anime_rec_homo_guest.csv')
-df_host = pd.read_csv('../examples/data/anime_rec_homo_host.csv')
-df_anime = pd.read_csv('../examples/data/anime.csv')
