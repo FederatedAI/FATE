6ce4d7229cab866ba677d6c88da431ac43723181
weiwee <wbwmat@gmail.com>
2023-01-09 05:10:40 -0800

    chore: purge conf/ deploy/
    Signed-off-by: weiwee <wbwmat@gmail.com>

--- deploy/upgrade/sql/1.7.0-1.7.1.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE t_job ADD f_inheritance_info LONGTEXT;
-ALTER TABLE t_job ADD f_inheritance_status VARCHAR(50);
-
-ALTER TABLE t_storage_table_meta MODIFY f_count BIGINT;
--- deploy/upgrade/sql/1.8.0-1.9.0.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE t_machine_learning_model_info ADD f_archive_sha256 VARCHAR(100);
-ALTER TABLE t_machine_learning_model_info ADD f_archive_from_ip VARCHAR(100);
