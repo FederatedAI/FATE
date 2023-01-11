4c88f1317212937eaf4a0fc609f09f6f8d4eaa9f
weiwee <wbwmat@gmail.com>
2023-01-10 22:03:54 -0800

    fix(interface): fix import
    Signed-off-by: weiwee <wbwmat@gmail.com>

--- python/fate/interface/__init__.py
+++ python/fate/interface/__init__.py
@@ -19,24 +19,19 @@ from ._context import Context
 from ._data_io import Dataframe
 from ._federation import FederationEngine, FederationWrapper
 from ._gc import GarbageCollector
-from ._log import LOGMSG, Logger
 from ._metric import InCompleteMetrics, Metric, Metrics, MetricsHandler, MetricsWrap
 from ._module import Module
-from ._param import Params
 from ._party import Parties, Party, PartyMeta
 
 __all__ = [
     "Module",
     "Context",
     "Dataframe",
-    "Params",
     "MetricsHandler",
     "MetricsWrap",
     "Metrics",
     "InCompleteMetrics",
     "Metric",
-    "Logger",
-    "LOGMSG",
     "Party",
     "Parties",
     "PartyMeta",
