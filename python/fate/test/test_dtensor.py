c5252bc731f971593e4d1f432ed01c146dedcab3
weiwee <wbwmat@gmail.com>
2023-03-12 19:32:38 -0800

    test(tensor): add binary ops for dtensor
    Signed-off-by: weiwee <wbwmat@gmail.com>

--- python/fate/test/test_dtensor.py
+++ python/fate/test/test_dtensor.py
@@ -19,7 +19,7 @@ def ctx():
 
 
 @fixture
-def t1_sharding():
+def t1_i32_sharding():
     return [
         torch.tensor([[1, 2, 3], [4, 5, 6]]),
         torch.tensor([[1, 2, 3], [4, 5, 6]]),
@@ -28,10 +28,46 @@ def t1_sharding():
 
 
 @fixture
-def t1(ctx, t1_sharding):
+def t1_f32_sharding():
+    return [
+        torch.tensor([[1, 2, 3], [4, 5, 6]], dtype=torch.float32),
+        torch.tensor([[1, 2, 3], [4, 5, 6]], dtype=torch.float32),
+        torch.tensor([[1, 2, 3], [4, 5, 6]], dtype=torch.float32),
+    ]
+
+
+@fixture
+def t2_f32_sharding():
+    return [
+        torch.tensor([[3, 2, 1], [6, 5, 4]], dtype=torch.float32),
+        torch.tensor([[3, 2, 1], [6, 5, 4]], dtype=torch.float32),
+        torch.tensor([[3, 2, 1], [6, 5, 4]], dtype=torch.float32),
+    ]
+
+
+@fixture
+def t1_i32(ctx, t1_i32_sharding):
     return DTensor.from_sharding_list(
         ctx,
-        t1_sharding,
+        t1_i32_sharding,
+        num_partitions=3,
+    )
+
+
+@fixture
+def t1_f32(ctx, t1_f32_sharding):
+    return DTensor.from_sharding_list(
+        ctx,
+        t1_f32_sharding,
+        num_partitions=3,
+    )
+
+
+@fixture
+def t2_f32(ctx, t2_f32_sharding):
+    return DTensor.from_sharding_list(
+        ctx,
+        t2_f32_sharding,
         num_partitions=3,
     )
 
@@ -40,5 +76,15 @@ def t1(ctx, t1_sharding):
     "op",
     [torch.exp, torch.log, torch.square],
 )
-def test_unary(ctx, t1, t1_sharding, op):
-    assert op(t1) == DTensor.from_sharding_list(ctx, [op(s) for s in t1_sharding], num_partitions=3)
+def test_unary(ctx, t1_f32, t1_f32_sharding, op):
+    assert op(t1_f32) == DTensor.from_sharding_list(ctx, [op(s) for s in t1_f32_sharding], num_partitions=3)
+
+
+@pytest.mark.parametrize(
+    "op",
+    [torch.add, torch.sub, torch.mul, torch.div, torch.rsub],
+)
+def test_binary(ctx, t1_f32, t2_f32, t1_f32_sharding, t2_f32_sharding, op):
+    assert op(t1_f32, t2_f32) == DTensor.from_sharding_list(
+        ctx, [op(s1, s2) for s1, s2 in zip(t1_f32_sharding, t2_f32_sharding)], num_partitions=3
+    )
