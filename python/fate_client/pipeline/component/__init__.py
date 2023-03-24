4de3ec64b21b4d87e0b3650cd32fc33e419ba8bf
weijing <talkingwallace@sohu.com>
2023-03-24 16:21:17 +0800

    update nn pipeline component import, add notification when torch is not installed
    Signed-off-by: cwj <talkingwallace@sohu.com>

--- python/fate_client/pipeline/component/__init__.py
+++ python/fate_client/pipeline/component/__init__.py
@@ -7,16 +7,13 @@ from pipeline.component.hetero_data_split import HeteroDataSplit
 from pipeline.component.hetero_fast_secureboost import HeteroFastSecureBoost
 from pipeline.component.hetero_feature_binning import HeteroFeatureBinning
 from pipeline.component.hetero_feature_selection import HeteroFeatureSelection
-from pipeline.component.hetero_ftl import HeteroFTL
 from pipeline.component.hetero_linr import HeteroLinR
 from pipeline.component.hetero_lr import HeteroLR
-from pipeline.component.hetero_nn import HeteroNN
 from pipeline.component.hetero_pearson import HeteroPearson
 from pipeline.component.hetero_poisson import HeteroPoisson
 from pipeline.component.hetero_secureboost import HeteroSecureBoost
 from pipeline.component.homo_data_split import HomoDataSplit
 from pipeline.component.homo_lr import HomoLR
-from pipeline.component.homo_nn import HomoNN
 from pipeline.component.homo_secureboost import HomoSecureBoost
 from pipeline.component.homo_feature_binning import HomoFeatureBinning
 from pipeline.component.intersection import Intersection
@@ -40,6 +37,16 @@ from pipeline.component.hetero_kmeans import HeteroKmeans
 from pipeline.component.homo_onehot import HomoOneHotEncoder
 from pipeline.component.hetero_sshe_linr import HeteroSSHELinR
 from pipeline.component.positive_unlabeled import PositiveUnlabeled
+
+try:
+    import torch 
+    from pipeline.component.homo_nn import HomoNN
+    from pipeline.component.hetero_ftl import HeteroFTL
+    from pipeline.component.hetero_nn import HeteroNN
+except:
+    print('Import torch failed, this may casue by the situation that torch are not installed, HomoNN, HeteroNN, HeteroFTL are not available')
+    HomoNN, HeteroNN, HeteroFTL = None, None, None
+    
 
 __all__ = [
     "DataStatistics",
