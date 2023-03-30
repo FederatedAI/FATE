eeded81ea26c6ff2de5afbdf9975370d1c11fba7
weiwee <wbwmat@gmail.com>
2023-03-30 05:17:17 -0800

    refact: improve cifar demo
    Signed-off-by: weiwee <wbwmat@gmail.com>

--- python/federatedml/cifar/param.py
+++ python/federatedml/cifar/param.py
@@ -2,9 +2,10 @@ from federatedml.param.base_param import BaseParam
 
 
 class CifarParam(BaseParam):
-    def __init__(self, config: dict = {}):
+    def __init__(self, config: dict = {}, data_root=""):
         super(CifarParam, self).__init__()
         self.config = config
+        self.data_root = data_root
 
     def check(self):
         ...
--- python/federatedml/cifar/train.py
+++ python/federatedml/cifar/train.py
@@ -1,296 +1,79 @@
+from types import SimpleNamespace
 from federatedml.model_base import ModelBase
 from .param import CifarParam
-import matplotlib.pyplot as plt
-import numpy as np
 import torch.utils.data
 import torchvision
 import deepspeed
 import torchvision.transforms as transforms
 import torch.nn as nn
 import torch.nn.functional as F
-import argparse
 
 
 class CifarTrainer(ModelBase):
     def __init__(self):
         super(CifarTrainer, self).__init__()
         self.model_param = CifarParam()
+        self.args = SimpleNamespace(
+            with_cuda=False,
+            use_ema=False,
+            batch_size=32,
+            epochs=30,
+            local_rank=-1,
+            log_interval=2000,
+            moe=False,
+            ep_world_size=1,
+            num_experts=[
+                1,
+            ],
+            mlp_type="standard",
+            top_k=1,
+            min_capacity=0,
+            noisy_gate_policy=None,
+            moe_param_group=False,
+        )
 
     def _init_model(self, param: CifarParam):
-        ...
-
-    def fit(self, train_input, validate_input=None):
-        def add_argument():
-            parser = argparse.ArgumentParser(description="CIFAR")
-
-            # data
-            # cuda
-            parser.add_argument(
-                "--with_cuda",
-                default=False,
-                action="store_true",
-                help="use CPU in case there's no GPU support",
-            )
-            parser.add_argument(
-                "--use_ema",
-                default=False,
-                action="store_true",
-                help="whether use exponential moving average",
-            )
-
-            # train
-            parser.add_argument(
-                "-b",
-                "--batch_size",
-                default=32,
-                type=int,
-                help="mini-batch size (default: 32)",
-            )
-            parser.add_argument(
-                "-e",
-                "--epochs",
-                default=30,
-                type=int,
-                help="number of total epochs (default: 30)",
-            )
-            parser.add_argument(
-                "--local_rank",
-                type=int,
-                default=-1,
-                help="local rank passed from distributed launcher",
-            )
-
-            parser.add_argument(
-                "--log-interval",
-                type=int,
-                default=2000,
-                help="output logging information at a given interval",
-            )
-
-            parser.add_argument(
-                "--moe",
-                default=False,
-                action="store_true",
-                help="use deepspeed mixture of experts (moe)",
-            )
-
-            parser.add_argument(
-                "--ep-world-size",
-                default=1,
-                type=int,
-                help="(moe) expert parallel world size",
-            )
-            parser.add_argument(
-                "--num-experts",
-                type=int,
-                nargs="+",
-                default=[
-                    1,
-                ],
-                help="number of experts list, MoE related.",
-            )
-            parser.add_argument(
-                "--mlp-type",
-                type=str,
-                default="standard",
-                help="Only applicable when num-experts > 1, accepts [standard, residual]",
-            )
-            parser.add_argument(
-                "--top-k",
-                default=1,
-                type=int,
-                help="(moe) gating top 1 and 2 supported",
-            )
-            parser.add_argument(
-                "--min-capacity",
-                default=0,
-                type=int,
-                help="(moe) minimum capacity of an expert regardless of the capacity_factor",
-            )
-            parser.add_argument(
-                "--noisy-gate-policy",
-                default=None,
-                type=str,
-                help="(moe) noisy gating (only supported with top-1). Valid values are None, RSample, and Jitter",
-            )
-            parser.add_argument(
-                "--moe-param-group",
-                default=False,
-                action="store_true",
-                help="(moe) create separate moe param groups, required when using ZeRO w. MoE",
-            )
-
-            # Include DeepSpeed configuration arguments
-            parser = deepspeed.add_config_arguments(parser)
-
-            args = parser.parse_args()
-
-            return args
-
+        self.ds_config = param.config
+        self.data_root = param.data_root
         deepspeed.init_distributed()
 
-        transform = transforms.Compose(
-            [
-                transforms.ToTensor(),
-                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-            ]
+
+    def fit(self, train_input, validate_input=None):
+        trainset, testset, classes = load_data(self.data_root)
+        net = build_net(
+            self.args.moe,
+            self.args.num_experts,
+            self.args.ep_world_size,
+            self.args.mlp_type,
+            self.args.top_k,
+            self.args.min_capacity,
+            self.args.noisy_gate_policy,
         )
 
-        if torch.distributed.get_rank() != 0:
-            # might be downloading cifar data, let rank 0 download first
-            torch.distributed.barrier()
-
-        trainset = torchvision.datasets.CIFAR10(
-            root="./data", train=True, download=True, transform=transform
-        )
-
-        if torch.distributed.get_rank() == 0:
-            # cifar data is downloaded, indicate other ranks can proceed
-            torch.distributed.barrier()
-        trainloader = torch.utils.data.DataLoader(
-            trainset, batch_size=16, shuffle=True, num_workers=2
-        )
-
-        testset = torchvision.datasets.CIFAR10(
-            root="./data", train=False, download=True, transform=transform
-        )
-        testloader = torch.utils.data.DataLoader(
-            testset, batch_size=4, shuffle=False, num_workers=2
-        )
-
-        classes = (
-            "plane",
-            "car",
-            "bird",
-            "cat",
-            "deer",
-            "dog",
-            "frog",
-            "horse",
-            "ship",
-            "truck",
-        )
-
-        ########################################################################
-        # Let us show some of the training images, for fun.
-
-        # functions to show an image
-
-        def imshow(img):
-            img = img / 2 + 0.5  # unnormalize
-            npimg = img.numpy()
-            plt.imshow(np.transpose(npimg, (1, 2, 0)))
-            plt.show()
-
-        # get some random training images
-        dataiter = iter(trainloader)
-        images, labels = dataiter.next()
-
-        # show images
-        imshow(torchvision.utils.make_grid(images))
-        # print labels
-        print(" ".join("%5s" % classes[labels[j]] for j in range(4)))
-
-        ########################################################################
-        # 2. Define a Convolutional Neural Network
-        # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-        # Copy the neural network from the Neural Networks section before and modify it to
-        # take 3-channel images (instead of 1-channel images as it was defined).
-
-        args = add_argument()
-
-        class Net(nn.Module):
-            def __init__(self):
-                super(Net, self).__init__()
-                self.conv1 = nn.Conv2d(3, 6, 5)
-                self.pool = nn.MaxPool2d(2, 2)
-                self.conv2 = nn.Conv2d(6, 16, 5)
-                self.fc1 = nn.Linear(16 * 5 * 5, 120)
-                self.fc2 = nn.Linear(120, 84)
-                if args.moe:
-                    fc3 = nn.Linear(84, 84)
-                    self.moe_layer_list = []
-                    for n_e in args.num_experts:
-                        # create moe layers based on the number of experts
-                        self.moe_layer_list.append(
-                            deepspeed.moe.layer.MoE(
-                                hidden_size=84,
-                                expert=fc3,
-                                num_experts=n_e,
-                                ep_size=args.ep_world_size,
-                                use_residual=args.mlp_type == "residual",
-                                k=args.top_k,
-                                min_capacity=args.min_capacity,
-                                noisy_gate_policy=args.noisy_gate_policy,
-                            )
-                        )
-                    self.moe_layer_list = nn.ModuleList(self.moe_layer_list)
-                    self.fc4 = nn.Linear(84, 10)
-                else:
-                    self.fc3 = nn.Linear(84, 10)
-
-            def forward(self, x):
-                x = self.pool(F.relu(self.conv1(x)))
-                x = self.pool(F.relu(self.conv2(x)))
-                x = x.view(-1, 16 * 5 * 5)
-                x = F.relu(self.fc1(x))
-                x = F.relu(self.fc2(x))
-                if args.moe:
-                    for layer in self.moe_layer_list:
-                        x, _, _ = layer(x)
-                    x = self.fc4(x)
-                else:
-                    x = self.fc3(x)
-                return x
-
-        net = Net()
-
-        def create_moe_param_groups(model):
+        parameters = filter(lambda p: p.requires_grad, net.parameters())
+        if self.args.moe_param_group:
             from deepspeed.moe.utils import (
                 split_params_into_different_moe_groups_for_optimizer,
             )
 
             parameters = {
-                "params": [p for p in model.parameters()],
+                "params": [p for p in net.parameters()],
                 "name": "parameters",
             }
+            parameters = split_params_into_different_moe_groups_for_optimizer(parameters)
 
-            return split_params_into_different_moe_groups_for_optimizer(parameters)
 
-        parameters = filter(lambda p: p.requires_grad, net.parameters())
-        if args.moe_param_group:
-            parameters = create_moe_param_groups(net)
-
-        # Initialize DeepSpeed to use the following features
-        # 1) Distributed model
-        # 2) Distributed data loader
-        # 3) DeepSpeed optimizer
-        model_engine, optimizer, trainloader, __ = deepspeed.initialize(
-            args=args, model=net, model_parameters=parameters, training_data=trainset
+        model_engine, _, trainloader, _ = deepspeed.initialize(
+            model=net,
+            model_parameters=parameters,
+            training_data=trainset,
+            config=self.ds_config,
         )
 
         fp16 = model_engine.fp16_enabled()
         print(f"fp16={fp16}")
 
-        # device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-        # net.to(device)
-        ########################################################################
-        # 3. Define a Loss function and optimizer
-        # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-        # Let's use a Classification Cross-Entropy loss and SGD with momentum.
-
-        import torch.optim as optim
-
         criterion = nn.CrossEntropyLoss()
-        # optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-
-        ########################################################################
-        # 4. Train the network
-        # ^^^^^^^^^^^^^^^^^^^^
-        #
-        # This is when things start to get interesting.
-        # We simply have to loop over our data iterator, and feed the inputs to the
-        # network and optimize.
 
         for epoch in range(2):  # loop over the dataset multiple times
             running_loss = 0.0
@@ -309,57 +92,20 @@ class CifarTrainer(ModelBase):
 
                 # print statistics
                 running_loss += loss.item()
-                if i % args.log_interval == (
-                    args.log_interval - 1
+                if i % self.args.log_interval == (
+                    self.args.log_interval - 1
                 ):  # print every log_interval mini-batches
                     print(
                         "[%d, %5d] loss: %.3f"
-                        % (epoch + 1, i + 1, running_loss / args.log_interval)
+                        % (epoch + 1, i + 1, running_loss / self.args.log_interval)
                     )
                     running_loss = 0.0
 
         print("Finished Training")
 
-        ########################################################################
-        # 5. Test the network on the test data
-        # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-        #
-        # We have trained the network for 2 passes over the training dataset.
-        # But we need to check if the network has learnt anything at all.
-        #
-        # We will check this by predicting the class label that the neural network
-        # outputs, and checking it against the ground-truth. If the prediction is
-        # correct, we add the sample to the list of correct predictions.
-        #
-        # Okay, first step. Let us display an image from the test set to get familiar.
-
-        dataiter = iter(testloader)
-        images, labels = dataiter.next()
-
-        # print images
-        imshow(torchvision.utils.make_grid(images))
-        print("GroundTruth: ", " ".join("%5s" % classes[labels[j]] for j in range(4)))
-
-        ########################################################################
-        # Okay, now let us see what the neural network thinks these examples above are:
-        if fp16:
-            images = images.half()
-        outputs = net(images.to(model_engine.local_rank))
-
-        ########################################################################
-        # The outputs are energies for the 10 classes.
-        # The higher the energy for a class, the more the network
-        # thinks that the image is of the particular class.
-        # So, let's get the index of the highest energy:
-        _, predicted = torch.max(outputs, 1)
-
-        print("Predicted: ", " ".join("%5s" % classes[predicted[j]] for j in range(4)))
-
-        ########################################################################
-        # The results seem pretty good.
-        #
-        # Let us look at how the network performs on the whole dataset.
-
+        testloader = torch.utils.data.DataLoader(
+            testset, batch_size=4, shuffle=False, num_workers=2
+        )
         correct = 0
         total = 0
         with torch.no_grad():
@@ -407,3 +153,88 @@ class CifarTrainer(ModelBase):
                 "Accuracy of %5s : %2d %%"
                 % (classes[i], 100 * class_correct[i] / class_total[i])
             )
+
+def load_data(root):
+    transform = transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+        ]
+    )
+
+    if torch.distributed.get_rank() != 0:
+        # might be downloading cifar data, let rank 0 download first
+        torch.distributed.barrier()
+
+    trainset = torchvision.datasets.CIFAR10(
+        root=root, train=True, download=True, transform=transform
+    )
+
+    if torch.distributed.get_rank() == 0:
+        # cifar data is downloaded, indicate other ranks can proceed
+        torch.distributed.barrier()
+    testset = torchvision.datasets.CIFAR10(
+        root=root, train=False, download=True, transform=transform
+    )
+    classes = (
+        "plane",
+        "car",
+        "bird",
+        "cat",
+        "deer",
+        "dog",
+        "frog",
+        "horse",
+        "ship",
+        "truck",
+    )
+    return trainset, testset, classes
+
+def build_net(
+    moe, num_experts, ep_world_size, mlp_type, top_k, min_capacity, noisy_gate_policy
+):
+    class Net(nn.Module):
+        def __init__(self):
+            super(Net, self).__init__()
+            self.conv1 = nn.Conv2d(3, 6, 5)
+            self.pool = nn.MaxPool2d(2, 2)
+            self.conv2 = nn.Conv2d(6, 16, 5)
+            self.fc1 = nn.Linear(16 * 5 * 5, 120)
+            self.fc2 = nn.Linear(120, 84)
+            if moe:
+                fc3 = nn.Linear(84, 84)
+                self.moe_layer_list = []
+                for n_e in num_experts:
+                    # create moe layers based on the number of experts
+                    self.moe_layer_list.append(
+                        deepspeed.moe.layer.MoE(
+                            hidden_size=84,
+                            expert=fc3,
+                            num_experts=n_e,
+                            ep_size=ep_world_size,
+                            use_residual=mlp_type == "residual",
+                            k=top_k,
+                            min_capacity=min_capacity,
+                            noisy_gate_policy=noisy_gate_policy,
+                        )
+                    )
+                self.moe_layer_list = nn.ModuleList(self.moe_layer_list)
+                self.fc4 = nn.Linear(84, 10)
+            else:
+                self.fc3 = nn.Linear(84, 10)
+
+        def forward(self, x):
+            x = self.pool(F.relu(self.conv1(x)))
+            x = self.pool(F.relu(self.conv2(x)))
+            x = x.view(-1, 16 * 5 * 5)
+            x = F.relu(self.fc1(x))
+            x = F.relu(self.fc2(x))
+            if moe:
+                for layer in self.moe_layer_list:
+                    x, _, _ = layer(x)
+                x = self.fc4(x)
+            else:
+                x = self.fc3(x)
+            return x
+
+    return Net()
