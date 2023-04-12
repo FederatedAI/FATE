4cf222c2c2afd20fc2a35b20562f854623311623
zhihuiwan <15779896112@163.com>
2023-04-12 21:53:27 +0800

    fix status report bug
    Signed-off-by: zhihuiwan <15779896112@163.com>

--- deploy/upgrade/sql/1.9.0-1.12.0.sql
+++ deploy/upgrade/sql/1.9.0-1.12.0.sql
@@ -1,5 +1,6 @@
 ALTER TABLE t_task ADD f_world_info LONGTEXT default null;
 ALTER TABLE t_task ADD f_launcher VARCHAR(20) default null;
 ALTER TABLE t_task ADD f_master_addr VARCHAR(30) default null;
+ALTER TABLE t_task ADD f_report_worker_count INTEGER default 0;
 ALTER TABLE t_engine_registry ADD f_devices INTEGER default 0;
 ALTER TABLE t_engine_registry ADD f_remaining_devices INTEGER default NULL;
