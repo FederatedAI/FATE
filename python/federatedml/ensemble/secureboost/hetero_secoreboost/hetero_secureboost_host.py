d072bd6352dd0dd7f4c20c6e80167caed6714c64
cwj <talkingwallace@sohu.com>
2023-05-22 15:38:14 +0800

    update complete secure strategy
    Signed-off-by: cwj <talkingwallace@sohu.com>

--- python/federatedml/ensemble/secureboost/hetero_secoreboost/hetero_secureboost_host.py
+++ python/federatedml/ensemble/secureboost/hetero_secoreboost/hetero_secureboost_host.py
@@ -28,7 +28,8 @@ class HeteroSecureBoostingTreeHost(HeteroBoostingHost):
         self.grad_and_hess = None
         self.tree_param = DecisionTreeParam()  # decision tree param
         self.model_param = HeteroSecureBoostParam()
-        self.complete_secure = False
+        self.complete_secure = 0
+        self.complete_secure_count = 0
         self.model_name = 'HeteroSecureBoost'
         self.enable_goss = False
         self.cipher_compressing = False
@@ -64,10 +65,10 @@ class HeteroSecureBoostingTreeHost(HeteroBoostingHost):
         self.enable_goss = param.run_goss
         self.zero_as_missing = param.zero_as_missing
         self.complete_secure = param.complete_secure
+        self.complete_secure_count = self.complete_secure
         self.sparse_opt_para = param.sparse_optimization
         self.cipher_compressing = param.cipher_compress
         self.new_ver = param.new_ver
-
         self.tree_num_per_party = param.tree_num_per_party
         self.boosting_strategy = param.boosting_strategy
         self.guest_depth = param.guest_depth
@@ -139,7 +140,8 @@ class HeteroSecureBoostingTreeHost(HeteroBoostingHost):
     def fit_a_learner(self, epoch_idx: int, booster_dim: int):
 
         flow_id = self.generate_flowid(epoch_idx, booster_dim)
-        complete_secure = True if (epoch_idx == 0 and self.complete_secure) else False
+        complete_secure = True if (self.complete_secure_count > 0) else False
+        self.complete_secure_count -= 1  # complete secure counting, if complete secure > 0, this tree is complete secure tree
         fast_sbt = (self.boosting_strategy == consts.MIX_TREE or self.boosting_strategy == consts.LAYERED_TREE)
 
         tree_type, target_host_id = None, None
