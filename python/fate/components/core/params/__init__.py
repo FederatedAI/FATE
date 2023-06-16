79d771f01e95766ad09d79cf25c7bf8ef2f573e6
Yu Wu <yolandawu131@gmail.com>
2023-06-16 17:16:11 +0800

    add hetero feature selection(#4661)
    Signed-off-by: Yu Wu <yolandawu131@gmail.com>

--- python/fate/components/core/params/__init__.py
+++ python/fate/components/core/params/__init__.py
@@ -1,5 +1,5 @@
 #
-#  Copyright 2019 The FATE Authors. All Rights Reserved.
+#  Copyright 2023 The FATE Authors. All Rights Reserved.
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
@@ -18,8 +18,11 @@ from ._cipher import CipherParamType, PaillierCipherParam
 from ._cv_param import cv_param, CVParam
 from ._fields import confloat, conint, jsonschema, parse, string_choice, Parameter
 from ._init_param import InitParam, init_param
-from ._init_param import InitParam, init_param
 from ._learning_rate import LRSchedulerParam, lr_scheduler_param
+from ._fields import Parameter, confloat, conint, jsonschema, parse, string_choice
+from ._filter_param import StatisticFilterParam, IVFilterParam, ManualFilterParam, \
+    statistic_filter_param, iv_filter_param, manual_filter_param
+from ._learning_rate import learning_rate_param
 from ._metrics import metrics_param, statistic_metrics_param
 from ._optimizer import OptimizerParam, optimizer_param
 from ._penalty import penalty_param
