faf0d206d9ae69310514318739f8d2d14c56e50e
weiwee <wbwmat@gmail.com>
2023-06-20 20:05:48 +0800

    add writer logs
    Signed-off-by: weiwee <wbwmat@gmail.com>

--- python/fate/components/core/component_desc/artifacts/data/_dataframe.py
+++ python/fate/components/core/component_desc/artifacts/data/_dataframe.py
@@ -1,3 +1,4 @@
+import logging
 import typing
 
 from fate.components.core.essential import DataframeArtifactType
@@ -8,9 +9,12 @@ if typing.TYPE_CHECKING:
     from fate.arch import URI, Context
     from fate.arch.dataframe import DataFrame
 
+logger = logging.getLogger(__name__)
+
 
 class DataframeWriter(_ArtifactTypeWriter["_DataframeArtifactType"]):
     def write(self, ctx, df: "DataFrame", name=None, namespace=None):
+        logger.debug(f"start writing dataframe to artifact: {self.artifact}, name={name}, namespace={namespace}")
         from fate.arch import dataframe
 
         if name is not None:
@@ -26,6 +30,7 @@ class DataframeWriter(_ArtifactTypeWriter["_DataframeArtifactType"]):
             schema=self.artifact.metadata.metadata["schema"],
             options=self.artifact.metadata.metadata.get("options", None),
         )
+        logger.debug(f"write dataframe to artifact: {self.artifact}")
 
 
 class DataframeArtifactDescribe(ArtifactDescribe):
