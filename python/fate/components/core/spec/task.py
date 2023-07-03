5cbee9f7cdfc48f24c68e6a2f03019232782b1b1
weiwee <wbwmat@gmail.com>
2023-07-03 19:26:43 +0800

    refactor entrypoint
    Signed-off-by: weiwee <wbwmat@gmail.com>

--- python/fate/components/core/spec/task.py
+++ python/fate/components/core/spec/task.py
@@ -13,7 +13,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 import os
-from typing import Any, Dict, List, Optional, Union
+from typing import Any, Dict, List, Union
 
 import pydantic
 
