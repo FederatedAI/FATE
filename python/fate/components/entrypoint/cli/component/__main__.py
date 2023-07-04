e9448b48015e58881f6723b1fc609cee3e6b827d
weiwee <wbwmat@gmail.com>
2023-07-04 17:25:29 +0800

    fix bug
    Signed-off-by: weiwee <wbwmat@gmail.com>

--- python/fate/components/entrypoint/cli/component/__main__.py
+++ python/fate/components/entrypoint/cli/component/__main__.py
@@ -1,5 +1,6 @@
 import click
 from fate.components.entrypoint.cli.component import (
+    artifact_type_cli,
     cleanup_cli,
     desc_cli,
     execute_cli,
@@ -12,6 +13,7 @@ component.add_command(execute_cli.execute)
 component.add_command(cleanup_cli.cleanup)
 component.add_command(desc_cli.desc)
 component.add_command(list_cli.list)
+component.add_command(artifact_type_cli.artifact_type)
 component.add_command(task_schema_cli.task_schema)
 
 if __name__ == "__main__":
