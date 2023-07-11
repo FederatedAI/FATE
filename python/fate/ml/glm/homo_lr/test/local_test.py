772223e5b9a9f1e0833315e0c85e83a16433e6ce
cwj <talkingwallace@sohu.com>
2023-07-11 17:08:02 +0800

    1. Update HomoLR 2. Refactor HomoNN parameter & add new callback 3. ModelO class
    Signed-off-by: cwj <talkingwallace@sohu.com>
    Signed-off-by: sagewe <wbwmat@gmail.com>

--- python/fate/ml/glm/homo_lr/test/local_test.py
+++ python/fate/ml/glm/homo_lr/test/local_test.py
@@ -5,7 +5,18 @@ from fate.arch.federation.standalone import StandaloneFederation
 import pandas as pd
 from fate.arch.dataframe import PandasReader
 from fate.ml.glm.homo_lr.client import HomoLRClient, HomoLRModel
+import logging
 
+import logging
+
+# Get the root logger
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
 
 
 computing = CSession()
@@ -15,13 +26,24 @@ ctx = Context(
     federation=StandaloneFederation(computing, "fed", ("guest", 10000), [("host", 9999)]),
 )
 
-df = pd.read_csv('./examples/data/vehicle_scale_homo_guest.csv')
+df = pd.read_csv('./../../../../../../examples/data/breast_homo_guest.csv')
 df['sample_id'] = [i for i in range(len(df))]
 
 reader = PandasReader(sample_id_name='sample_id', match_id_name="id", label_name="y", dtype="object")
 data = reader.to_frame(ctx, df)
 df = data.as_pd_df()
 
-client = HomoLRClient(50, 32, learning_rate_param=0.01)
-a = client.fit(ctx, data)
+client = HomoLRClient(50, 800, learning_rate_param=0.01)
+client.l2 = 0.01
+client.l1 = 0.01
+client.fit(ctx, data)
+export_model = client.get_model()
+pred = client.predict(ctx, data)
 
+# print('load model and warm-starting')
+# client_2 = HomoLRClient(1, batch_size=800, learning_rate_param=0.001)
+# client_2.from_model(export_model)
+# client_2.fit(ctx, data)
+
+# from fate.components.core.params._learning_rate import LRSchedulerParam
+# from fate.components.core.params._optimizer import OptimizerParam
--- python/fate/ml/utils/model_io.py
+++ python/fate/ml/utils/model_io.py
@@ -1,7 +1,7 @@
 from typing import Optional
 
 
-class ModelExporter:
+class ModelIO:
     _META = "meta"
     _DATA = "data"
 
