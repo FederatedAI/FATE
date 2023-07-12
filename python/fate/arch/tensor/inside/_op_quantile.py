0132d44b418ead4c35127574528c7162e282fee6
weiwee <wbwmat@gmail.com>
2023-07-12 12:58:26 +0800

    update quantile
    Signed-off-by: weiwee <wbwmat@gmail.com>

--- python/fate/arch/tensor/inside/_op_quantile.py
+++ python/fate/arch/tensor/inside/_op_quantile.py
@@ -1,3 +1,6 @@
+from typing import List, Union
+
+import numpy
 import torch
 from fate_utils import quantile
 
@@ -12,18 +15,43 @@ def quantile_fi(input: torch.Tensor, q, epsilon):
 
 
 class GKSummary:
-    def __init__(self, summary=None) -> None:
-        if summary is None:
-            summary = quantile.QuantileSummaryStream()
-        self._summary = summary
+    """
+    GKSummary is a summary of a stream of numbers, which can be used to estimate quantiles.
+
+    Examples:
+        >>> summary = GKSummary(0.01)
+        >>> summary += torch.tensor([1.0, 2.0, 3.0])
+        >>> summary += torch.tensor([4.0, 5.0, 6.0])
+        >>> summary += torch.tensor([7.0, 8.0, 9.0])
+        >>> summary.queries([0.1, 0.2])
+        [2.0, 3.0]
+    """
+
+    def __init__(self, epsilon: float) -> None:
+        self._summary = quantile.QuantileSummaryStream(epsilon)
+
+    def merge(self, other: "GKSummary"):
+        """merge other summary into self."""
+        self._summary.merge(other._summary)
+        return self
+
+    def push(self, array: Union[torch.Tensor, numpy.ndarray]):
+        """push elements in array into summary."""
+        if isinstance(array, torch.Tensor):
+            array = array.numpy()
+        self._summary.insert_array(array)
+        return self
 
     def __add__(self, other: "GKSummary"):
         if isinstance(other, GKSummary):
-            return GKSummary(self._summary.merge(other._summary))
+            return self.merge(other)
         return NotImplemented
 
-    def __iadd__(self, other: torch.Tensor):
-        if isinstance(other, torch.Tensor):
-            self._summary.insert_array(other.numpy())
-            return self
+    def __iadd__(self, other: Union[torch.Tensor, numpy.ndarray]):
+        if isinstance(other, torch.Tensor) or isinstance(other, numpy.ndarray):
+            return self.push(other)
         return NotImplemented
+
+    def queries(self, q: List[float]):
+        """return quantile values of q."""
+        return torch.tensor(self._summary.queries(q))
