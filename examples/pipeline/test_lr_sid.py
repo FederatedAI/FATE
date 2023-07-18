4fad5b832798a903874f8ff2fd582119e707388a
Yu Wu <yolandawu131@gmail.com>
2023-07-18 16:50:42 +0800

    edit examples (#4659)
    Signed-off-by: Yu Wu <yolandawu131@gmail.com>

--- examples/pipeline/test_lr_sid.py
+++ examples/pipeline/test_lr_sid.py
@@ -20,15 +20,15 @@ from fate_client.pipeline.interface import DataWarehouseChannel
 pipeline = FateFlowPipeline().set_roles(guest="9999", host="9998", arbiter="9998")
 
 intersect_0 = Intersection("intersect_0", method="raw")
-intersect_0.guest.component_setting(input_data=DataWarehouseChannel(name="breast_hetero_guest",
+intersect_0.guest.component_setting(input_data=DataWarehouseChannel(name="breast_hetero_guest_multi",
                                                                     namespace="experiment_sid"))
 intersect_0.hosts[0].component_setting(input_data=DataWarehouseChannel(name="breast_hetero_host",
                                                                        namespace="experiment_sid"))
 lr_0 = CoordinatedLR("lr_0",
-                     epochs=2,
-                     batch_size=100,
-                     optimizer={"method": "sgd", "optimizer_params": {"lr": 0.01}},
-                     init_param={"fit_intercept": True},
+                     epochs=4,
+                     batch_size=None,
+                     optimizer={"method": "rprop", "optimizer_params": {"lr": 0.01}},
+                     init_param={"fit_intercept": True, "method": "zeros"},
                      train_data=intersect_0.outputs["output_data"])
 lr_1 = CoordinatedLR("lr_1", test_data=intersect_0.outputs["output_data"],
                      input_model=lr_0.outputs["output_model"])
@@ -39,31 +39,33 @@ lr_0.hosts[0].component_setting(train_data=DataWarehouseChannel(name="breast_het
                                                                 namespace="experiment"))"""
 
 evaluation_0 = Evaluation("evaluation_0",
+                          label_column_name="y",
                           runtime_roles=["guest"],
+                          default_eval_setting="multi",
                           input_data=lr_0.outputs["train_output_data"])
 
 # pipeline.add_task(feature_scale_0)
 # pipeline.add_task(feature_scale_1)
 pipeline.add_task(intersect_0)
 pipeline.add_task(lr_0)
-pipeline.add_task(evaluation_0)
+# pipeline.add_task(evaluation_0)
 # pipeline.add_task(hetero_feature_binning_0)
 pipeline.compile()
 print(pipeline.get_dag())
 pipeline.fit()
+print(f"lr_0 model: {pipeline.get_task_info('lr_0').get_output_model()}")
+print(f"train lr_0 data: {pipeline.get_task_info('lr_0').get_output_data()}")
 
 # print(pipeline.get_task_info("statistics_0").get_output_model())
-print(pipeline.get_task_info("lr_0").get_output_model())
-print(pipeline.get_task_info("lr_0").get_output_metrics())
-print(f"evaluation metrics: ")
-print(pipeline.get_task_info("evaluation_0").get_output_metrics())
+# print(f"evaluation metrics: ")
+# print(pipeline.get_task_info("evaluation_0").get_output_metric())
 
 pipeline.deploy([intersect_0, lr_0])
 
 predict_pipeline = FateFlowPipeline()
 
 deployed_pipeline = pipeline.get_deployed_pipeline()
-deployed_pipeline.intersect_0.guest.component_setting(input_data=DataWarehouseChannel(name="breast_hetero_guest",
+deployed_pipeline.intersect_0.guest.component_setting(input_data=DataWarehouseChannel(name="breast_hetero_guest_multi",
                                                                                       namespace="experiment_sid"))
 deployed_pipeline.intersect_0.hosts[0].component_setting(input_data=DataWarehouseChannel(name="breast_hetero_host",
                                                                                          namespace="experiment_sid"))
@@ -73,3 +75,4 @@ predict_pipeline.compile()
 # print("\n\n\n")
 # print(predict_pipeline.compile().get_dag())
 predict_pipeline.predict()
+print(f"predict lr_0 data: {pipeline.get_task_info('lr_0').get_output_data()}")
