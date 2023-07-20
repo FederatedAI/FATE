3273e0665219eb950c85d485e9942c12c34ec8ed
Yu Wu <yolandawu131@gmail.com>
2023-07-20 19:06:41 +0800

    edit selection with binning example(#4660)
    Signed-off-by: Yu Wu <yolandawu131@gmail.com>
    Signed-off-by: weiwee <wbwmat@gmail.com>

--- examples/pipeline/test_selection.py
+++ examples/pipeline/test_selection.py
@@ -40,7 +40,7 @@ intersection_1.hosts[0].component_setting(input_data=DataWarehouseChannel(name="
 statistics_0 = Statistics("statistics_0", input_data=intersection_0.outputs["output_data"],
                           metrics=["mean", "max", "std", "var", "kurtosis", "skewness"])
 binning_0 = HeteroFeatureBinning("binning_0", train_data=intersection_0.outputs["output_data"],
-                                 method="quantile", n_bins=5, bin_col=["x0"], category_col=None,
+                                 method="quantile", n_bins=5, bin_col=None, category_col=None,
                                  skip_metrics=False, transform_method="woe")
 
 selection_0 = HeteroFeatureSelection("selection_0",
@@ -56,23 +56,24 @@ selection_0 = HeteroFeatureSelection("selection_0",
 pipeline.add_task(intersection_0)
 pipeline.add_task(binning_0)
 pipeline.add_task(statistics_0)
-pipeline.add_task(selection_0)
+# pipeline.add_task(selection_0)
 pipeline.compile()
 print(pipeline.get_dag())
 pipeline.fit()
-print(json.dumps(pipeline.get_task_info("statistics_0").get_output_model(), indent=4))
-print(json.dumps(pipeline.get_task_info("selection_0").get_output_model(), indent=4))
+# print(json.dumps(pipeline.get_task_info("statistics_0").get_output_model(), indent=4))
+print(json.dumps(pipeline.get_task_info("binning_0").get_output_model(), indent=4))
+print(pipeline.get_task_info("binning_0").get_output_data())
 
 predict_pipeline = FateFlowPipeline().set_roles(guest="9999", host="9998", arbiter="9998")
-pipeline.deploy([binning_0, selection_0])
+pipeline.deploy([intersection_0, binning_0, selection_0])
 
 deployed_pipeline = pipeline.get_deployed_pipeline()
 
-deployed_pipeline.feature_scale_0.guest.component_setting(test_data=DataWarehouseChannel(name="breast_hetero_guest_sid",
+deployed_pipeline.interesction_0.guest.component_setting(input_data=DataWarehouseChannel(name="breast_hetero_guest_sid",
                                                                                          namespace="experiment"))
-deployed_pipeline.feature_scale_0.hosts[0].component_setting(
-    test_data=DataWarehouseChannel(name="breast_hetero_host_sid",
-                                   namespace="experiment"))
+deployed_pipeline.intersection_0.hosts[0].component_setting(
+    input_data=DataWarehouseChannel(name="breast_hetero_host_sid",
+                                    namespace="experiment"))
 
 predict_pipeline.add_task(deployed_pipeline)
 
