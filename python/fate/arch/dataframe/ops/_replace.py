a5338e02d6f1bb5a90a6bbc1414650e6f9ec857d
mgqa34 <mgq3374541@163.com>
2023-07-20 18:19:40 +0800

    dataframe: fix partial columns bucketize
    Signed-off-by: mgqa34 <mgq3374541@163.com>
    Signed-off-by: weiwee <wbwmat@gmail.com>

--- python/fate/arch/dataframe/ops/_replace.py
+++ python/fate/arch/dataframe/ops/_replace.py
@@ -30,7 +30,7 @@ def replace(df: "DataFrame", to_replace: dict):
     _to_replace_list = []
     for name, (_bid, _) in zip(field_names, blocks_loc):
         block_type = data_manager.get_block(_bid).block_type
-        for _, v in to_replace[name].items():
+        for k, v in to_replace[name].items():
             v_type = BlockType.get_block_type(type(v))
 
             if block_type < v_type:
