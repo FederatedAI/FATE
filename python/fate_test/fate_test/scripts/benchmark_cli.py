444a3f56be23edbb645ad9a52d0e022925e96d84
Yu Wu <yolandawu131@gmail.com>
2023-08-01 16:23:01 +0800

    edit fate-test bq & examples(#5008)
    Signed-off-by: Yu Wu <yolandawu131@gmail.com>

--- python/fate_test/fate_test/scripts/benchmark_cli.py
+++ python/fate_test/fate_test/scripts/benchmark_cli.py
@@ -68,9 +68,7 @@ def run_benchmark(ctx, include, exclude, glob, skip_data, tol, clean_data, stora
     if not yes and not click.confirm("running?"):
         return
     client = Clients(config_inst)
-    # @todo: get version
-    # fate_version = client["guest_0"].get_version()
-    fate_version = "2.0.0-beta"
+    fate_version = client["guest_0"].get_version()
     for i, suite in enumerate(suites):
         # noinspection PyBroadException
         try:
