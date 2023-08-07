71d11d63d8ebd820c47862f05b68db165340006a
weiwee <wbwmat@gmail.com>
2023-08-07 18:09:14 +0800

    update elem type
    Signed-off-by: weiwee <wbwmat@gmail.com>

--- rust/fate_utils/crates/fate_utils/src/histogram/indexer.rs
+++ rust/fate_utils/crates/fate_utils/src/histogram/indexer.rs
@@ -1,25 +1,25 @@
 use std::collections::HashMap;
-use ndarray::{Array2, Axis, Ix2};
-use numpy::{PyArray2, PyReadonlyArray1, PyReadonlyArray2};
 use pyo3::prelude::*;
 use rand::rngs::StdRng;
 use rand::SeedableRng;
 use rand::prelude::SliceRandom;
 
+type S = usize;
+
 #[pyclass]
 pub struct HistogramIndexer {
-    node_size: usize,
-    feature_bin_sizes: Vec<usize>,
-    feature_size: usize,
-    feature_axis_stride: Vec<usize>,
-    node_axis_stride: usize,
+    node_size: S,
+    feature_bin_sizes: Vec<S>,
+    feature_size: S,
+    feature_axis_stride: Vec<S>,
+    node_axis_stride: S,
 }
 
 
 #[pymethods]
 impl HistogramIndexer {
     #[new]
-    fn new(node_size: usize, feature_bin_sizes: Vec<usize>) -> HistogramIndexer {
+    fn new(node_size: S, feature_bin_sizes: Vec<S>) -> HistogramIndexer {
         let feature_size = feature_bin_sizes.len();
         let mut feature_axis_stride = vec![0];
         feature_axis_stride.extend(feature_bin_sizes.iter().scan(0, |acc, &x| {
@@ -38,17 +38,17 @@ impl HistogramIndexer {
     }
 
     #[inline]
-    fn get_position(&self, nid: usize, fid: usize, bid: usize) -> usize {
+    fn get_position(&self, nid: S, fid: S, bid: S) -> S {
         nid * self.node_axis_stride + self.feature_axis_stride[fid] + bid
     }
 
-    fn get_positions(&self, nids: Vec<usize>, bid_vec: Vec<Vec<usize>>, py: Python) -> Vec<Vec<usize>> {
+    fn get_positions(&self, nids: Vec<S>, bid_vec: Vec<Vec<S>>, py: Python) -> Vec<Vec<S>> {
         bid_vec.iter().zip(nids.iter()).map(|(bids, &nid)| {
             bids.iter().enumerate().map(|(fid, &bid)| self.get_position(nid, fid, bid)).collect()
         }).collect()
     }
 
-    fn get_reverse_position(&self, position: usize) -> (usize, usize, usize) {
+    fn get_reverse_position(&self, position: S) -> (S, S, S) {
         let nid = position / self.node_axis_stride;
         let bid = position % self.node_axis_stride;
         for fid in 0..self.feature_size {
@@ -59,22 +59,22 @@ impl HistogramIndexer {
         panic!("invalid position: {}", position);
     }
 
-    fn get_bin_num(&self, fid: usize) -> usize {
+    fn get_bin_num(&self, fid: S) -> S {
         self.feature_bin_sizes[fid]
     }
 
-    fn get_bin_interval(&self, nid: usize, fid: usize) -> (usize, usize) {
+    fn get_bin_interval(&self, nid: S, fid: S) -> (S, S) {
         let node_stride = nid * self.node_axis_stride;
         (node_stride + self.feature_axis_stride[fid], node_stride + self.feature_axis_stride[fid + 1])
     }
 
-    fn get_node_intervals(&self) -> Vec<(usize, usize)> {
+    fn get_node_intervals(&self) -> Vec<(S, S)> {
         (0..self.node_size).map(|nid| {
             (nid * self.node_axis_stride, (nid + 1) * self.node_axis_stride)
         }).collect()
     }
 
-    fn get_feature_position_ranges(&self) -> Vec<(usize, usize)> {
+    fn get_feature_position_ranges(&self) -> Vec<(S, S)> {
         (0..self.node_size).flat_map(|nid| {
             let node_stride = nid * self.node_axis_stride;
             (0..self.feature_size).map(move |fid| {
@@ -83,7 +83,7 @@ impl HistogramIndexer {
         }).collect()
     }
 
-    fn splits_into_k(&self, k: usize) -> Vec<(usize, (usize, usize), Vec<(usize, usize)>)> {
+    fn splits_into_k(&self, k: S) -> Vec<(S, (S, S), Vec<(S, S)>)> {
         let n = self.node_axis_stride;
         let mut split_sizes = vec![n / k; k];
         for i in 0..n % k {
@@ -104,15 +104,15 @@ impl HistogramIndexer {
         splits
     }
 
-    fn total_data_size(&self) -> usize {
+    fn total_data_size(&self) -> S {
         self.node_size * self.node_axis_stride
     }
 
-    fn one_node_data_size(&self) -> usize {
+    fn one_node_data_size(&self) -> S {
         self.node_axis_stride
     }
 
-    fn global_flatten_bin_sizes(&self) -> Vec<usize> {
+    fn global_flatten_bin_sizes(&self) -> Vec<S> {
         // repeat self.feature_bin_sizes for self.node_size times
         let mut feature_bin_sizes = Vec::with_capacity(self.node_size * self.feature_size);
         for _ in 0..self.node_size {
@@ -128,37 +128,37 @@ impl HistogramIndexer {
     fn squeeze_bins(&self) -> HistogramIndexer {
         HistogramIndexer::new(self.node_size, vec![1; self.feature_size])
     }
-    fn reshape(&self, feature_bin_sizes: Vec<usize>) -> HistogramIndexer {
+    fn reshape(&self, feature_bin_sizes: Vec<S>) -> HistogramIndexer {
         HistogramIndexer::new(self.node_size, feature_bin_sizes)
     }
     fn get_shuffler(&self, seed: u64) -> Shuffler {
         Shuffler::new(self.node_size, self.node_axis_stride, seed)
     }
     #[getter]
-    fn get_node_size(&self) -> usize {
+    fn get_node_size(&self) -> S {
         self.node_size
     }
     #[getter]
-    fn get_node_axis_stride(&self) -> usize {
+    fn get_node_axis_stride(&self) -> S {
         self.node_axis_stride
     }
     #[getter]
-    fn get_feature_size(&self) -> usize {
+    fn get_feature_size(&self) -> S {
         self.feature_size
     }
     #[getter]
-    fn get_feature_axis_stride(&self) -> Vec<usize> {
+    fn get_feature_axis_stride(&self) -> Vec<S> {
         self.feature_axis_stride.clone()
     }
     #[getter]
-    fn get_feature_bin_sizes(&self) -> Vec<usize> {
+    fn get_feature_bin_sizes(&self) -> Vec<S> {
         self.feature_bin_sizes.clone()
     }
     #[getter]
-    fn get_num_nodes(&self) -> usize {
+    fn get_num_nodes(&self) -> S {
         self.node_size
     }
-    fn unflatten_indexes(&self) -> HashMap<usize, HashMap<usize, Vec<usize>>> {
+    fn unflatten_indexes(&self) -> HashMap<S, HashMap<S, Vec<S>>> {
         let mut indexes = HashMap::new();
         for nid in 0..self.node_size {
             let mut feature_indexes = HashMap::new();
@@ -174,19 +174,19 @@ impl HistogramIndexer {
 
 #[pyclass]
 struct Shuffler {
-    num_node: usize,
-    node_size: usize,
-    perm_indexes: Vec<Vec<usize>>,
+    num_node: S,
+    node_size: S,
+    perm_indexes: Vec<Vec<S>>,
 }
 
 #[pymethods]
 impl Shuffler {
     #[new]
-    fn new(num_node: usize, node_size: usize, seed: u64) -> Shuffler {
+    fn new(num_node: S, node_size: S, seed: u64) -> Shuffler {
         let mut perm_indexes = Vec::with_capacity(num_node);
         for _ in 0..num_node {
             let mut rng = StdRng::seed_from_u64(seed);
-            let mut perm_index = (0..node_size).collect::<Vec<usize>>();
+            let mut perm_index = (0..node_size).collect::<Vec<S>>();
             perm_index.shuffle(&mut rng);
             perm_indexes.push(perm_index);
         }
@@ -197,7 +197,7 @@ impl Shuffler {
         }
     }
 
-    fn get_global_perm_index(&self) -> Vec<usize> {
+    fn get_global_perm_index(&self) -> Vec<S> {
         let mut index = Vec::with_capacity(self.num_node * self.node_size);
         for (nid, perm_index) in self.perm_indexes.iter().enumerate() {
             index.extend(perm_index.iter().map(|&x| x + nid * self.node_size));
@@ -205,7 +205,7 @@ impl Shuffler {
         index
     }
 
-    fn get_reverse_indexes(&self, step: usize, indexes: Vec<usize>) -> Vec<usize> {
+    fn get_reverse_indexes(&self, step: S, indexes: Vec<S>) -> Vec<S> {
         let mapping = self.get_shuffle_index(step, true);
         indexes.iter().map(|&x| mapping[x]).collect()
     }
@@ -218,7 +218,7 @@ impl Shuffler {
 //     #         if reverse:
 //     #             indexes = torch.argsort(indexes)
 //     #         return indexes
-    fn get_shuffle_index(&self, step: usize, reverse: bool) -> Vec<usize> {
+    fn get_shuffle_index(&self, step: S, reverse: bool) -> Vec<S> {
         let mut stepped = Vec::with_capacity(self.num_node * self.node_size * step);
         for i in 0..self.num_node * self.node_size {
             for j in 0..step {
@@ -239,7 +239,7 @@ impl Shuffler {
         indexes
     }
 
-    fn reverse_index(&self, index: usize) -> (usize, usize) {
+    fn reverse_index(&self, index: S) -> (S, S) {
         let nid = index / self.node_size;
         let bid = index % self.node_size;
         (nid, self.perm_indexes[nid][bid])
