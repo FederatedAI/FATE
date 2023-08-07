432c597016dd1e22e10b0e1569f431142d14dd5f
weiwee <wbwmat@gmail.com>
2023-08-07 12:59:21 +0800

    impl indexer with rust
    Signed-off-by: weiwee <wbwmat@gmail.com>

---
+++ rust/fate_utils/crates/fate_utils/src/histogram/mod.rs
@@ -0,0 +1,15 @@
+mod histogram;
+mod indexer;
+
+use pyo3::prelude::*;
+
+pub(crate) fn register(py: Python, m: &PyModule) -> PyResult<()> {
+    let submodule = PyModule::new(py, "histogram")?;
+    histogram::register(py, submodule)?;
+    indexer::register(py, submodule)?;
+    m.add_submodule(submodule)?;
+    py.import("sys")?
+        .getattr("modules")?
+        .set_item("fate_utils.histogram", submodule)?;
+    Ok(())
+}
