coordinated lr & linr add key length & floating point precision param(#4659) binning add key length param(#4660) edit fate test config(#5008) fix pipeline examples
Signed-off-by: Yu Wu <yolandawu131@gmail.com>

python/fate/arch/tensor/distributed/_ops_others.py
@@ -1,6 +1,6 @@
-from fate.arch.tensor import _custom_ops
 import torch
 
+from fate.arch.tensor import _custom_ops
 from ._tensor import DTensor, implements
 
 
@@ -11,4 +11,4 @@ def to_local_f(input: DTensor):
 
 @implements(_custom_ops.encode_as_int_f)
 def encode_as_int_f(input: DTensor, precision):
-    return input.shardings.map_shard(lambda x: (x * 2**precision).astype(torch.int64), dtype=torch.int64)
+    return DTensor(input.shardings.map_shard(lambda x: (x * 2 ** precision).type(torch.int64), dtype=torch.int64))

python/fate/ml/glm/hetero/coordinated_linr/guest.py
@@ -31,12 +31,14 @@ logger = logging.getLogger(__name__)
 
 
 class CoordinatedLinRModuleGuest(HeteroModule):
-    def __init__(self, epochs=None, batch_size=None, optimizer_param=None, learning_rate_param=None, init_param=None):
+    def __init__(self, epochs=None, batch_size=None, optimizer_param=None, learning_rate_param=None, init_param=None,
+                 floating_point_precision=23):
         self.epochs = epochs
         self.batch_size = batch_size
         self.optimizer_param = optimizer_param
         self.learning_rate_param = learning_rate_param
         self.init_param = init_param
+        self.floating_point_precision = floating_point_precision
 
         self.estimator = None
 
@@ -65,6 +67,7 @@ class CoordinatedLinRModuleGuest(HeteroModule):
                 optimizer=optimizer,
                 learning_rate_scheduler=lr_scheduler,
                 init_param=self.init_param,
+                floating_point_precision=self.floating_point_precision
             )
             self.estimator = estimator
         encryptor = ctx.arbiter("encryptor").get()
@@ -83,6 +86,7 @@ class CoordinatedLinRModuleGuest(HeteroModule):
                 "learning_rate_param": self.learning_rate_param,
                 "init_param": self.init_param,
                 "optimizer_param": self.optimizer_param,
+                "floating_point_precision": self.floating_point_precision,
             },
         }
 
@@ -93,11 +97,13 @@ class CoordinatedLinRModuleGuest(HeteroModule):
             learning_rate_param=model["meta"]["learning_rate_param"],
             batch_size=model["meta"]["batch_size"],
             init_param=model["meta"]["init_param"],
+            floating_point_precision=model["meta"]["floating_point_precision"]
         )
         estimator = CoordinatedLinREstimatorGuest(
             epochs=model["meta"]["epochs"],
             batch_size=model["meta"]["batch_size"],
             init_param=model["meta"]["init_param"],
+            floating_point_precision=model["meta"]["floating_point_precision"]
         )
         estimator.restore(model["data"]["estimator"])
         linr.estimator = estimator
@@ -106,12 +112,15 @@ class CoordinatedLinRModuleGuest(HeteroModule):
 
 
 class CoordinatedLinREstimatorGuest(HeteroModule):
-    def __init__(self, epochs=None, batch_size=None, optimizer=None, learning_rate_scheduler=None, init_param=None):
+    def __init__(self, epochs=None, batch_size=None, optimizer=None, learning_rate_scheduler=None, init_param=None,
+                 floating_point_precision=23):
         self.epochs = epochs
         self.batch_size = batch_size
         self.optimizer = optimizer
         self.lr_scheduler = learning_rate_scheduler
         self.init_param = init_param
+        self.floating_point_precision = floating_point_precision
+        self._fixpoint_precision = 2 ** floating_point_precision
 
         self.w = None
         self.start_epoch = 0
@@ -130,7 +139,11 @@ class CoordinatedLinREstimatorGuest(HeteroModule):
         Xw_h = batch_ctx.hosts.get("Xw_h")[0]
         if weight:
             Xw_h = Xw_h * weight
-        host_half_g = torch.matmul(X.T, Xw_h)
+        if self.floating_point_precision:
+            host_half_g = torch.matmul(torch.encode_as_int_f(X.T, self.floating_point_precision), Xw_h)
+            host_half_g = 1 / self._fixpoint_precision * host_half_g
+        else:
+            host_half_g = torch.matmul(X.T, Xw_h)
 
         loss = 0.5 / h * torch.matmul(half_d.T, half_d)
         if self.optimizer.l1_penalty or self.optimizer.l2_penalty:
@@ -164,7 +177,11 @@ class CoordinatedLinREstimatorGuest(HeteroModule):
         batch_ctx.hosts.put(d=d)
 
         # gradient
-        g = 1 / h * torch.matmul(X.T, d)
+        if self.floating_point_precision:
+            g = torch.matmul(torch.encode_as_int_f(X.T, self.floating_point_precision), d)
+            g = 1 / (self._fixpoint_precision * h) * g
+        else:
+            g = 1 / h * torch.matmul(X.T, d)
         return g
 
     def fit_model(self, ctx, encryptor, train_data, validate_data=None):

python/fate/ml/utils/predict_tools.py
@@ -74,7 +74,8 @@ def compute_predict_details(dataframe: DataFrame, task_type: Literal['binary', '
             neg_class, pos_class = classes[0], classes[1]
             dataframe[[PREDICT_RESULT, PREDICT_DETAIL]] = dataframe.apply_row( \
                 lambda v: [int(v[PREDICT_SCORE] > threshold),
-                           predict_detail_dict_to_str({neg_class: 1 - v[PREDICT_SCORE], pos_class: v[PREDICT_SCORE]})],
+                           predict_detail_dict_to_str({neg_class: 1 - float(v[PREDICT_SCORE]),
+                                                       pos_class: float(v[PREDICT_SCORE])})],
                 enable_type_align_checking=False)
         else:
             raise ValueError(
@@ -82,7 +83,8 @@ def compute_predict_details(dataframe: DataFrame, task_type: Literal['binary', '
         
     elif task_type == REGRESSION:
         dataframe[[PREDICT_RESULT, PREDICT_DETAIL]] = dataframe.apply_row( \
-                lambda v: [v[PREDICT_SCORE], predict_detail_dict_to_str({PREDICT_SCORE: v[PREDICT_SCORE]})], enable_type_align_checking=False)
+            lambda v: [v[PREDICT_SCORE], predict_detail_dict_to_str({PREDICT_SCORE: float(v[PREDICT_SCORE])})],
+            enable_type_align_checking=False)
 
     elif task_type == MULTI:
 
@@ -90,7 +92,7 @@ def compute_predict_details(dataframe: DataFrame, task_type: Literal['binary', '
             predict_result = np.argmax(v[PREDICT_SCORE])
             assert len(v[PREDICT_SCORE]) == len(classes), 'predict score length is not equal to classes length,\
                 predict score is {}, but classes are {}, please check the data you provided'.format(v[PREDICT_SCORE], classes)
-            predict_details = {classes[j]: v[PREDICT_SCORE][j] for j in range(len(classes))}
+            predict_details = {classes[j]: float(v[PREDICT_SCORE][j]) for j in range(len(classes))}
             return [predict_result, predict_detail_dict_to_str(predict_details)]
 
         dataframe[[PREDICT_RESULT, PREDICT_DETAIL]] = dataframe.apply_row(handle_multi, enable_type_align_checking=False)