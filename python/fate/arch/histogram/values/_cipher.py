89fcdf1ec53937eac3fe6312e57197fe087c260d
sagewe <wbwmat@gmail.com>
2023-09-06 21:14:42 +0800

    add coder
    Signed-off-by: sagewe <wbwmat@gmail.com>

--- python/fate/arch/histogram/values/_cipher.py
+++ python/fate/arch/histogram/values/_cipher.py
@@ -10,17 +10,18 @@ logger = logging.getLogger(__name__)
 
 
 class HistogramEncryptedValues(HistogramValues):
-    def __init__(self, pk, evaluator, data, stride=1):
+    def __init__(self, pk, evaluator, data, coder, stride=1):
         if stride > 1:
             raise NotImplementedError
         self.stride = stride
         self.data = data
         self.pk = pk
+        self.coder = coder
         self.evaluator = evaluator
 
     @classmethod
-    def zeros(cls, pk, evaluator, size: int, stride: int = 1):
-        return cls(pk, evaluator, evaluator.zeros(size * stride), stride)
+    def zeros(cls, pk, evaluator, coder, size: int, stride: int = 1):
+        return cls(pk, evaluator, evaluator.zeros(size * stride), coder, stride)
 
     def i_update(self, value, positions):
         from fate.arch.tensor.phe import PHETensor
@@ -44,13 +45,14 @@ class HistogramEncryptedValues(HistogramValues):
             self.pk,
             self.evaluator,
             self.evaluator.slice(self.data, start * self.stride, (end - start) * self.stride),
+            self.coder,
             self.stride,
         )
 
     def intervals_slice(self, intervals: typing.List[typing.Tuple[int, int]]) -> "HistogramEncryptedValues":
         intervals = [(start * self.stride, end * self.stride) for start, end in intervals]
         data = self.evaluator.intervals_slice(self.data, intervals)
-        return HistogramEncryptedValues(self.pk, self.evaluator, data, self.stride)
+        return HistogramEncryptedValues(self.pk, self.evaluator, data, self.coder, self.stride)
 
     def i_shuffle(self, shuffler: "Shuffler", reverse=False):
         indices = shuffler.get_shuffle_index(step=self.stride, reverse=reverse)
@@ -63,7 +65,7 @@ class HistogramEncryptedValues(HistogramValues):
         """
         intervals = [(start * self.stride, end * self.stride) for start, end in intervals]
         data = self.evaluator.intervals_sum_with_step(self.pk, self.data, intervals, self.stride)
-        return HistogramEncryptedValues(self.pk, self.evaluator, data, self.stride)
+        return HistogramEncryptedValues(self.pk, self.evaluator, data, self.coder, self.stride)
 
     def compute_child(
         self,
@@ -87,7 +89,7 @@ class HistogramEncryptedValues(HistogramValues):
             self.evaluator.i_add(self.pk, data, self.data, target_strong_child_start, parent_data_start, s)
             self.evaluator.i_sub(self.pk, data, weak_child.data, target_strong_child_start, weak_child_data_start, s)
 
-        return HistogramEncryptedValues(self.pk, self.evaluator, data, self.stride)
+        return HistogramEncryptedValues(self.pk, self.evaluator, data, self.coder, self.stride)
 
     def decrypt(self, sk):
         data = sk.decrypt_to_encoded(self.data)
@@ -95,7 +97,7 @@ class HistogramEncryptedValues(HistogramValues):
 
     def squeeze(self, pack_num, offset_bit):
         data = self.evaluator.pack_squeeze(self.data, pack_num, offset_bit, self.pk)
-        return HistogramEncryptedValues(self.pk, self.evaluator, data, self.stride)
+        return HistogramEncryptedValues(self.pk, self.evaluator, data, self.coder, self.stride)
 
     def i_chunking_cumsum(self, chunk_sizes: typing.List[int]):
         chunk_sizes = [num * self.stride for num in chunk_sizes]
