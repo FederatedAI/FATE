a2e0c85a3b325bfe7ad7eeb6dfd8fd29ad6b373c
sagewe <wbwmat@gmail.com>
2023-09-07 21:48:26 +0800

    add support for cumsum flag for histogram
    Signed-off-by: sagewe <wbwmat@gmail.com>

--- python/fate/arch/histogram/_histogram_sbt.py
+++ python/fate/arch/histogram/_histogram_sbt.py
@@ -4,7 +4,7 @@ from ._histogram_local import Histogram
 
 class HistogramBuilder:
     def __init__(
-        self, num_node, feature_bin_sizes, value_schemas, global_seed=None, seed=None, node_mapping=None, k=None
+        self, num_node, feature_bin_sizes, value_schemas, global_seed=None, seed=None, node_mapping=None, k=None, enable_cumsum=True
     ):
         self._num_node = num_node
         self._feature_bin_sizes = feature_bin_sizes
@@ -13,6 +13,7 @@ class HistogramBuilder:
         self._global_seed = global_seed
         self._seed = seed
         self._node_mapping = node_mapping
+        self._enable_cumsum = enable_cumsum
         self._k = k
 
     def __str__(self):
@@ -35,6 +36,7 @@ class HistogramBuilder:
             self._global_seed,
             self._k,
             self._node_mapping,
+            self._enable_cumsum,
         )
         table = data.mapReducePartitions(mapper, lambda x, y: x.iadd(y))
         data = DistributedHistogram(
@@ -43,13 +45,14 @@ class HistogramBuilder:
         return data
 
 
-def get_partition_hist_build_mapper(num_node, feature_bin_sizes, value_schemas, global_seed, k, node_mapping):
+def get_partition_hist_build_mapper(num_node, feature_bin_sizes, value_schemas, global_seed, k, node_mapping, enable_cumsum):
     def _partition_hist_build_mapper(part):
         hist = Histogram.create(num_node, feature_bin_sizes, value_schemas)
         for _, raw in part:
             feature_ids, node_ids, targets = raw
             hist.i_update(feature_ids, node_ids, targets, node_mapping)
-        hist.i_cumsum_bins()
+        if enable_cumsum:
+            hist.i_cumsum_bins()
         if global_seed is not None:
             hist.i_shuffle(global_seed)
         splits = hist.to_splits(k)
