fb768c76fe2854e428e07deddb2f336838b9e7f3
Yu Wu <yolandawu131@gmail.com>
2023-09-08 17:49:25 +0800

    fix manual selection (#4661)
    Signed-off-by: Yu Wu <yolandawu131@gmail.com>

--- python/fate/ml/feature_selection/hetero_feature_selection.py
+++ python/fate/ml/feature_selection/hetero_feature_selection.py
@@ -305,8 +305,8 @@ class ManualSelection(Module):
         if len(filter_out_col) >= len(header):
             raise ValueError("`filter_out_col` should not be all columns")
         filter_out_col = set(filter_out_col)
-        keep_col = set(keep_col)
-        missing_col = (filter_out_col.union(keep_col)).difference(set(self._prev_selected_mask.index))
+        # keep_col = set(keep_col)
+        missing_col = (filter_out_col.union(set(keep_col))).difference(set(self._prev_selected_mask.index))
         if missing_col:
             raise ValueError(
                 f"columns {missing_col} given in `filter_out_col` & `keep_col` " f"not found in `select_col` or header"
