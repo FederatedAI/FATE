2bd6ab7c2498d365aa7fb0a76c9397017ae06378
cwj <talkingwallace@sohu.com>
2023-09-13 10:55:22 +0800

    update key length
    Signed-off-by: cwj <talkingwallace@sohu.com>

--- examples/benchmark_quality/hetero_sbt/pipeline_hetero_sbt.py
+++ examples/benchmark_quality/hetero_sbt/pipeline_hetero_sbt.py
@@ -61,7 +61,7 @@ def main(config="../../config.yaml", param="./sbt_breast_config.yaml", namespace
         "objective": param.get("objective", "binary:bce"),
     }
     hetero_sbt_0 = HeteroSBT('sbt_0', train_data=psi_0.outputs['output_data'], num_trees=config_param["num_trees"], 
-                             max_bin=config_param["max_bin"], max_depth=config_param["max_depth"], he_param={'kind': 'ou', 'key_length': 1024},
+                             max_bin=config_param["max_bin"], max_depth=config_param["max_depth"], he_param={'kind': 'paillier', 'key_length': 1024},
                              objective=config_param["objective"])
 
     if config_param['objective'] == 'regression:l2':
