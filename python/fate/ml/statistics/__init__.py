42f45fcea2940d472f64a8c7b5b4f3b92d3ad667
Yu Wu <yolandawu131@gmail.com>
2023-11-17 17:39:11 +0800

    add feature correlation
    Signed-off-by: Yu Wu <yolandawu131@gmail.com>

--- python/fate/ml/statistics/__init__.py
+++ python/fate/ml/statistics/__init__.py
@@ -13,4 +13,5 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+from .pearson_correlation import PearsonCorrelation
 from .statistics import FeatureStatistics
