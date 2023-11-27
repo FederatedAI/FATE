82b3cc3218e8155a76bbc156264a998b655598dc
forgive_dengkai <forgive_dengkai@163.com>
2023-11-27 19:59:32 +0800

    format code
    Signed-off-by: forgive_dengkai <forgive_dengkai@163.com>

--- java/osx/osx-broker/src/main/java/org/fedai/osx/broker/pojo/RouterTableGetResponse.java
+++ java/osx/osx-broker/src/main/java/org/fedai/osx/broker/pojo/RouterTableGetResponse.java
@@ -3,7 +3,8 @@ package org.fedai.osx.broker.pojo;
 import lombok.Data;
 
 @Data
-public class RouterTableSetResponse {
+public class RouterTableGetResponse {
+    String content;
     String code;
     String msg;
 }
--- java/osx/osx-core/src/main/java/org/fedai/osx/core/exceptions/InvalidRequestException.java
+++ java/osx/osx-core/src/main/java/org/fedai/osx/core/exceptions/InvalidRequestException.java
@@ -1,4 +1,9 @@
 package org.fedai.osx.core.exceptions;
 
+import org.fedai.osx.core.constant.StatusCode;
+
 public class InvalidRequestException extends BaseException {
+    public InvalidRequestException(String msg){
+        super(StatusCode.PTP_INVALID_REQUEST,msg);
+    }
 }
