c4b04481b75caaf34a96f298c4f6b9d665facb09
mgqa34 <mgq3374541@163.com>
2023-12-19 11:36:41 +0800

    homo_nn: deepspeed support
    Signed-off-by: mgqa34 <mgq3374541@163.com>

--- python/fate/ml/nn/homo/fedavg.py
+++ python/fate/ml/nn/homo/fedavg.py
@@ -10,10 +10,8 @@ from transformers.trainer_callback import TrainerCallback
 from torch.nn import Module
 from torch import nn
 from torch.utils.data import DataLoader
-from fate.ml.aggregator import PlainTextAggregatorClient, SecureAggregatorClient
-from fate.ml.aggregator import PlainTextAggregatorServer, SecureAggregatorServer
 from transformers import TrainerState, TrainerControl, PreTrainedTokenizer
-from fate.ml.aggregator import AggregatorType, aggregator_map
+from fate.ml.aggregator import AggregatorClientWrapper, AggregatorServerWrapper
 import logging
 
 
@@ -66,23 +64,19 @@ class FedAVGClient(HomoTrainerClient):
         aggregate_type = "weighted_mean"
         aggregator_name = "fedavg"
         aggregator = fed_args.aggregator
-        assert aggregator in {
-            item.value for item in AggregatorType
-        }, f"aggregator should be one of {{item.value for item in AggregatorType}}, but got {aggregator}"
-        client_class = aggregator_map[aggregator][0]
-        logger.info(f"Using {aggregator} aggregator")
-        sample_num = len(self.train_dataset)
-        ctx.arbiter.put("agg_type", aggregator)
-        aggregator = client_class(
-            ctx, aggregate_type=aggregate_type, aggregator_name=aggregator_name, sample_num=sample_num
+        return AggregatorClientWrapper(
+            ctx,
+            aggregate_type,
+            aggregator_name,
+            aggregator,
+            sample_num=len(self.train_dataset),
+            args=self._args
         )
-
-        return aggregator
 
     def on_federation(
         self,
         ctx: Context,
-        aggregator: Union[PlainTextAggregatorClient, SecureAggregatorClient],
+        aggregator: AggregatorClientWrapper,
         fed_args: FedArguments,
         args: TrainingArguments,
         model: Optional[nn.Module] = None,
@@ -101,19 +95,9 @@ class FedAVGServer(HomoTrainerServer):
         super().__init__(ctx, local_mode)
 
     def init_aggregator(self, ctx):
-        aggregator = [ctx.guest.get("agg_type")]
-        aggregator.extend(ctx.hosts.get("agg_type"))
-        aggregator = set(aggregator)
-        if len(aggregator) > 1:
-            raise ValueError("Aggregator type should be the same between clients, but got {}".format(aggregator))
-        aggregator = aggregator.pop()
-        aggregator_name = "fedavg"
-        aggregator_server = aggregator_map[aggregator][1]
-        logger.info(f"Using {aggregator} aggregator")
-        aggregator = aggregator_server(ctx, aggregator_name=aggregator_name)
-        return aggregator
+        return AggregatorServerWrapper(ctx)
 
-    def on_federation(self, ctx: Context, aggregator: Union[SecureAggregatorServer, PlainTextAggregatorServer],
+    def on_federation(self, ctx: Context, aggregator: AggregatorServerWrapper,
                       agg_iter_idx: int):
         aggregator.model_aggregation(ctx)
 
