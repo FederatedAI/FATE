9e34eb7d23eed9665ad4e8fe3a889dbb549f4406
sagewe <wbwmat@gmail.com>
2023-12-27 11:11:32 +0800

    chore: remove unused code
    Signed-off-by: sagewe <wbwmat@gmail.com>

--- python/fate/ml/mpc/utils.py
+++
@@ -1,8 +0,0 @@
-class NoopContextManager:
-    """Context manager that does nothing."""
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, type, value, traceback):
-        pass
