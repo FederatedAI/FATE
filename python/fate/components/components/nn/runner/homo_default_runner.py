dd66419896311f6ff45044e79f2871fc254702c6
cwj <talkingwallace@sohu.com>
2024-03-05 10:49:41 +0800

    Fix agg type in component Signed-off-by: weijingchen <talkingwallace@sohu.com>
    Signed-off-by: cwj <talkingwallace@sohu.com>

--- python/fate/components/components/nn/runner/homo_default_runner.py
+++ python/fate/components/components/nn/runner/homo_default_runner.py
@@ -260,6 +260,8 @@ class DefaultRunner(NNRunner):
         saved_model_path: str = None,
     ):
         if self.is_client():
+            if self.fed_args_conf.aggregate_strategy == 'steps':
+                raise ValueError('aggregate_strategy "steps" is not supported in FATE-pipeline which will be used in production.')
             train_set = self._prepare_data(train_data, "train_data")
             validate_set = self._prepare_data(validate_data, "val_data")
             trainer = self.client_setup(
